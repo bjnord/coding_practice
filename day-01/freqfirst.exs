c2aad550a847921975e0493886e2a67a7ba2eccb
Brent J. Nordquist <brent@nordist.net>
2018-12-01 16:18:16 -0600

    Comments

--- day-01/freqfirst.exs
+++ day-01/freqfirst.exs
@@ -1,9 +1,29 @@
+# Day 1: Chronal Calibration
+#
+# Part 2: (Given a repeating set of frequency changes:) What is the first frequency your device reaches twice?
+#
+# Correct answer: 312
+
+###
+# read frequency changes from input file (one per line)
+# treat them as a repeating list (with "cycle")
+# and map them to accumulated frequencies
+#
+# i.e. input:  +1 +2 -1 +3
+#      stream:  1  3  2  5  6  8  7 10 ...
+###
+
 # "Streams are composable, lazy enumerables"
 stream = File.stream!("input.txt")
 |> Stream.map(&String.trim/1)
 |> Stream.map(&String.to_integer/1)
 |> Stream.cycle
 |> Stream.scan(fn(i, sum) -> i + sum end)
+
+###
+# find the first frequency value that repeats
+# keep a map of accumulated "seen" values
+###
 
 accum = Enum.reduce_while(stream, {%{0 => true}, 0, false}, fn(i, {seen, value, found}) ->
           case {seen[i], found} do
--- day-01/freqsum.exs
+++ day-01/freqsum.exs
@@ -1,7 +1,22 @@
+# Day 1: Chronal Calibration
+#
+# Part 1: Starting with a frequency of zero, what is the resulting frequency after all of the changes in frequency have been applied?
+#
+# Correct answer: 406
+
+###
+# read frequency changes from input file (one per line)
+# create a stream of *integers*
+###
+
 # "Streams are composable, lazy enumerables"
 stream = File.stream!("input.txt")
 |> Stream.map(&String.trim/1)
 |> Stream.map(&String.to_integer/1)
+
+###
+# reduce the stream to an accumulated sum
+###
 
 freqsum = Enum.reduce(stream, fn(i, sum) -> i + sum end)
 IO.inspect(freqsum)
