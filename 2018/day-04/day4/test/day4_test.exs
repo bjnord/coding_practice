ce10888ca5de98e6f931a00ec490a1f065375190
Brent J. Nordquist <brent@nordist.net>
2018-12-04 08:19:54 -0600

    RF: Import module we're testing
    so we don't have to qualify all the method names

--- 2018/day-04/day4/test/day4_test.exs
+++ 2018/day-04/day4/test/day4_test.exs
@@ -2,11 +2,13 @@ defmodule Day4Test do
   use ExUnit.Case
   doctest Day4
 
+  import Day4
+
   test "parses input filename from command-line arguments" do
     argv = [
       "input/input.txt",
       ]
-    assert Day4.input_file(argv) == "input/input.txt"
+    assert input_file(argv) == "input/input.txt"
   end
 
   test "sorts input lines chronologically" do
@@ -15,7 +17,7 @@ defmodule Day4Test do
       "[1518-11-02 00:40] falls asleep",
       "[1518-11-01 23:58] Guard #99 begins shift",
       ]
-    assert Day4.sort_lines(lines) == [
+    assert sort_lines(lines) == [
       "[1518-11-01 23:58] Guard #99 begins shift",
       "[1518-11-02 00:40] falls asleep",
       "[1518-11-02 00:50] wakes up",
@@ -23,10 +25,10 @@ defmodule Day4Test do
   end
 
   test "gets minute of input line (leading 0)" do
-    assert Day4.minute_of("[1518-11-01 00:05] falls asleep") == 5
+    assert minute_of("[1518-11-01 00:05] falls asleep") == 5
   end
 
   test "gets minute of input line (no leading 0)" do
-    assert Day4.minute_of("[1518-11-01 23:58] Guard #99 begins shift") == 58
+    assert minute_of("[1518-11-01 23:58] Guard #99 begins shift") == 58
   end
 end
