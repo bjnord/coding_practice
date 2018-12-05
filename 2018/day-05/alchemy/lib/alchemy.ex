5d8cd883019dd06f79775f4782eada41cfdf0b6e
Brent J. Nordquist <brent@nordist.net>
2018-12-05 06:21:46 -0600

    Day 5: Empty mix project

---
+++ 2018/day-05/alchemy/lib/alchemy.ex
@@ -0,0 +1,69 @@
+defmodule Alchemy do
+  @moduledoc """
+  Documentation for Alchemy.
+  """
+
+  defp abort(message, excode) do
+    IO.puts(:stderr, message)
+    System.halt(excode)
+  end
+
+  @doc """
+  Process input file and display part 1 solution.
+
+  ## Parameters
+
+  - argv: Command-line arguments (should be name of input file)
+
+  ## Correct Answer
+
+  - Part 1 answer is: ...
+  """
+  def part1(argv) do
+  end
+
+  @doc """
+  Process input file and display part 2 solution.
+
+  ## Parameters
+
+  - argv: Command-line arguments (should be name of input file)
+
+  ## Correct Answer
+
+  - Part 2 answer is: ...
+  """
+  def part2(argv) do
+  end
+
+  @doc """
+  Get name of input file from command-line arguments.
+
+  ## Parameters
+
+  - argv: Command-line arguments
+
+  ## Returns
+
+  Input filename (or aborts if argv invalid)
+  """
+  def input_file(argv) do
+    case argv do
+      [filename] -> filename
+      _          -> abort('Usage: alchemy filename', 64)
+    end
+  end
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Alchemy.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
