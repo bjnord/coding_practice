a13405340e5fa0422bfeaaa2cd9fd5599d04754d
Brent J. Nordquist <brent@nordist.net>
2018-12-08 17:14:15 -0600

    RF: Change anonymous functions to module

--- 2018/day-02/checksum.exs
+++ 2018/day-02/checksum.exs
@@ -1,17 +1,47 @@
-###
-# functions needed for main stream
-###
+defmodule Inventory do
+  @moduledoc """
+  Documentation for Inventory.
+  """
 
-# count occurrences in a list
-# e.g. counting ["a", "b", "a"]  yields %{"a" => 2, "b" => 1}
-# h/t <https://stackoverflow.com/a/43007740/291754>
-counter = fn (list) -> list |> Enum.reduce(%{}, fn (x, acc) -> Map.update(acc, x, 1, &(&1 + 1)) end) end
+  @doc """
+  Count occurrences in a list.
 
-# return true if countmap has any entry with given value
-detector = fn (countmap, value) -> Map.values(countmap) |> Enum.member?(value) end
+  e.g. counting ["a", "b", "a"]  yields %{"a" => 2, "b" => 1}
+  h/t <https://stackoverflow.com/a/43007740/291754>
+  """
+  def count(list) do
+    Enum.reduce(list, %{}, fn (x, acc) -> Map.update(acc, x, 1, &(&1 + 1)) end)
+  end
 
-# return tuple with binary values {has-value-twice, has-value-thrice}
-tupler = fn (countmap) -> { detector.(countmap, 2), detector.(countmap, 3) } end
+  @doc """
+  Return true if countmap has any entry with given value.
+  """
+  def has_value?(countmap, value) do
+    Map.values(countmap)
+    |> Enum.member?(value)
+  end
+
+  @doc """
+  Return tuple with binary values {has-value-twice, has-value-thrice}.
+  """
+  def two_and_three?(countmap) do
+    { has_value?(countmap, 2), has_value?(countmap, 3) }
+  end
+
+  @doc """
+  Return tuple with binary values {has-value-twice, has-value-thrice}.
+  """
+  def count_2s_3s(stream) do
+    Enum.reduce(stream, {0, 0}, fn (tuple, {acc2, acc3}) ->
+      case tuple do
+        {true, true} -> {acc2 + 1, acc3 + 1}
+        {true, false} -> {acc2 + 1, acc3}
+        {false, true} -> {acc2, acc3 + 1}
+        {_, _} -> {acc2, acc3}
+      end
+    end)
+  end
+end
 
 ###
 # read box IDs from input file (one per line)
@@ -21,8 +51,8 @@ tupler = fn (countmap) -> { detector.(countmap, 2), detector.(countmap, 3) } end
 stream = File.stream!("input/input.txt")
 |> Stream.map(&String.trim/1)
 |> Stream.map(&String.graphemes/1)
-|> Stream.map(counter)
-|> Stream.map(tupler)
+|> Stream.map(&Inventory.count/1)
+|> Stream.map(&Inventory.two_and_three?/1)
 
 ###
 # reduce the stream of binary has-2/has-3 tuples
@@ -30,14 +60,7 @@ stream = File.stream!("input/input.txt")
 # and calculate the checksum
 ###
 
-{has2, has3} = Enum.reduce(stream, {0, 0}, fn (tuple, {acc2, acc3}) ->
-                 case tuple do
-                   {true, true} -> {acc2 + 1, acc3 + 1}
-                   {true, false} -> {acc2 + 1, acc3}
-                   {false, true} -> {acc2, acc3 + 1}
-                   {_, _} -> {acc2, acc3}
-                 end
-               end)
+{has2, has3} = Inventory.count_2s_3s(stream)
 
 # Part 1 answer is: 6696
 IO.inspect(has2 * has3, label: "Part 1 checksum is")
--- 2018/day-02/similarity.exs
+++ 2018/day-02/similarity.exs
@@ -1,30 +1,55 @@
-# NOTE solution assumes all box IDs will be the same length
+defmodule Inventory do
+  @moduledoc """
+  Documentation for Inventory.
 
-###
-# functions needed for main stream
-###
+  NOTE solution assumes all box IDs will be the same length
+  """
 
-# return index of the only pair in list with different values
-# (return nil if more or less than one differing pair exists)
-pair_comparator = fn (pairs) ->
-                    diff = Enum.map(pairs, fn ({a, b}) -> if a == b, do: 0, else: 1 end)
-                           |> Enum.sum
-                    case diff do
-                      1 -> Enum.find_index(pairs, fn ({a, b}) -> a != b end)
-                      _ -> nil
-                    end
-                  end
+  @doc """
+  Return index of the only pair in list with different values.
+  (Return `nil` if more or less than one differing pair exists.)
+  """
+  def compare_pairs(pairs) do
+    diff = Enum.map(pairs, fn ({a, b}) -> if a == b, do: 0, else: 1 end)
+           |> Enum.sum
+    case diff do
+      1 -> Enum.find_index(pairs, fn ({a, b}) -> a != b end)
+      _ -> nil
+    end
+  end
 
-# return position where strings differ, if they differ in exactly one position
-# (return nil if they differ in more or less than one position)
-str_comparator = fn (a, b) -> pair_comparator.(Enum.zip(String.graphemes(a), String.graphemes(b))) end
+  @doc """
+  Return position where strings differ, if they differ in exactly one position.
+  (Return `nil` if they differ in more or less than one position.)
+  """
+  def compare_strings(a, b) do
+    Enum.zip(String.graphemes(a), String.graphemes(b))
+    |> Inventory.compare_pairs()
+  end
 
-# return string with common characters between input strings
-str_commonor = fn (a, b) ->
-  Enum.zip(String.graphemes(a), String.graphemes(b))
-  |> Enum.reduce([], fn ({l, r}, acc) -> if l == r, do: [l | acc], else: acc end)
-  |> Enum.reverse
-  |> List.to_string  # or Enum.join("")
+  @doc """
+  Return string with common characters between input strings.
+  """
+  def common_characters(a, b) do
+    Enum.zip(String.graphemes(a), String.graphemes(b))
+    |> Enum.reduce([], fn ({l, r}, acc) -> if l == r, do: [l | acc], else: acc end)
+    |> Enum.reverse
+    |> List.to_string  # or Enum.join("")
+  end
+
+  @doc """
+  Return the two box IDs that differ in only one letter (same position).
+  """
+  def comparable_box_ids(stream) do
+    Enum.reduce_while(stream, [], fn (this_s, seen) ->
+      compares = Enum.map(seen, fn (seen_s) -> Inventory.compare_strings(seen_s, this_s) end)
+      i = Enum.find_index(compares, fn (compare) -> compare != nil end)
+      case i do
+        nil -> {:cont, [this_s | seen]}
+        i   -> {:halt, {Enum.at(seen, i), this_s}}
+      end
+    end)
+  end
 end
 
 ###
@@ -36,22 +61,14 @@ stream = File.stream!("input/input.txt")
 
 ###
 # find two box IDs that differ in only one letter (same position)
-# keep a list of accumulated "seen" box IDs
 ###
 
-{id1, id2} = Enum.reduce_while(stream, [], fn (this_s, seen) ->
-               compares = Enum.map(seen, fn (seen_s) -> str_comparator.(seen_s, this_s) end)
-               i = Enum.find_index(compares, fn (compare) -> compare != nil end)
-               case i do
-                 nil -> {:cont, [this_s | seen]}
-                 i   -> {:halt, {Enum.at(seen, i), this_s}}
-               end
-             end)
+{id1, id2} = Inventory.comparable_box_ids(stream)
 
 ###
 # display the letters in common between the two box IDs
 ###
 
 # Part 2 answer is: "bvnfawcnyoeyudzrpgslimtkj"
-str_commonor.(id1, id2)
+Inventory.common_characters(id1, id2)
 |> IO.inspect(label: "Part 2 letters in common are")
