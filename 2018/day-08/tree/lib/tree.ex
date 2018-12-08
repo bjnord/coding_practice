55a55bc8dbe574043de36abedafae6739841b908
Brent J. Nordquist <brent@nordist.net>
2018-12-08 10:41:05 -0600

    Day 8: Empty mix project

---
+++ 2018/day-08/tree/lib/tree.ex
@@ -0,0 +1,75 @@
+defmodule Tree do
+  @moduledoc """
+  Documentation for Tree.
+  """
+
+  defp abort(message, excode) do
+    IO.puts(:stderr, message)
+    System.halt(excode)
+  end
+
+  @doc """
+  Process input file and display part 1 solution.
+
+  ## Parameters
+
+  - argv: Command-line arguments (should be name of input file)
+
+  ## Correct Answer
+
+  - Part 1 answer is: ...
+  """
+  def part1(argv) do
+    argv
+    |> input_file
+    |> File.stream!
+  end
+
+  @doc """
+  Process input file and display part 2 solution.
+
+  ## Parameters
+
+  - argv: Command-line arguments (should be name of input file)
+
+  ## Correct Answer
+
+  - Part 2 answer is: ...
+  """
+  def part2(argv) do
+    argv
+    |> input_file
+    |> File.stream!
+  end
+
+  @doc """
+  Get name of input file from command-line arguments.
+
+  ## Parameters
+
+  - argv: Command-line arguments
+
+  ## Returns
+
+  Input filename (or aborts if argv invalid)
+  """
+  def input_file(argv) do
+    case argv do
+      [filename] -> filename
+      _          -> abort('Usage: sleigh filename', 64)
+    end
+  end
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Tree.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
