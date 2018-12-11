d3cb33aea2f36b17e5276ba33ae6e245b31fced6
Brent J. Nordquist <brent@nordist.net>
2018-12-11 06:31:09 -0600

    Day 11 part 1: power_level() function

--- advent-2018/day-11/fuel_cell/lib/fuel_cell.ex
+++ advent-2018/day-11/fuel_cell/lib/fuel_cell.ex
@@ -63,6 +63,52 @@ defmodule FuelCell do
   end
 
   @doc """
+  Find the power level in a given fuel cell.
+
+  ## Algorithm
+
+  - Find the fuel cell's rack ID, which is its X coordinate plus 10.
+  - Begin with a power level of the rack ID times the Y coordinate.
+  - Increase the power level by the value of the grid serial number.
+  - Set the power level to itself multiplied by the rack ID.
+  - Keep only the hundreds digit of the power level (so 12345 becomes 3).
+    -- Numbers with no hundreds digit become 0.
+  - Subtract 5 from the power level.
+
+  ## Parameters
+
+  - {x, y}: Fuel cell coordinates (integers 1..300)
+  - grid_serial: Grid serial number (integer)
+
+  ## Returns
+
+  Power level (integer)
+
+  ## Examples
+
+    iex> FuelCell.power_level({3, 5}, 8)
+    4
+
+    iex> FuelCell.power_level({122, 79}, 57)
+    -5
+
+    iex> FuelCell.power_level({217, 196}, 39)
+    0
+
+    iex> FuelCell.power_level({101, 153}, 71)
+    4
+
+  """
+  def power_level({x, y}, grid_serial) do
+    rack_id = (x + 10)
+    hundreds_digit(((rack_id * y) + grid_serial) * rack_id) - 5
+  end
+
+  defp hundreds_digit(n) do
+    div(rem(n, 1000), 100)
+  end
+
+  @doc """
   Hello world.
 
   ## Examples
