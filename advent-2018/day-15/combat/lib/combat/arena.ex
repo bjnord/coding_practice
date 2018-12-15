f1ff38da5ffe044d86c13da3968eca0a6e2054dd
Brent J. Nordquist <brent@nordist.net>
2018-12-15 09:52:44 -0600

    Day 15 part 1: Find live opponents

--- advent-2018/day-15/combat/lib/combat/arena.ex
+++ advent-2018/day-15/combat/lib/combat/arena.ex
@@ -3,8 +3,10 @@ defmodule Combat.Arena do
   @type position() :: tuple()  # {y :: non_neg_integer(), x :: non_neg_integer()}
   @type square() :: atom()  # :rock | :floor | :combatant
   @type grid() :: %{required(position()) => square()}
+  @type team() :: :elf | :goblin  # {y_x :: position(), team(), power :: non_neg_integer(), hp :: integer()}
   @type combatant() :: tuple()  # {y_x :: position(), team :: atom(), power :: non_neg_integer(), hp :: integer()}
   @type roster() :: MapSet.t()  # of combatant()
+  # FIXME In Elixr, for module Foo the main type for Foo should be named Foo.t
   @type arena() :: {grid(), roster()}
 
   @doc ~S"""
@@ -59,5 +61,84 @@ defmodule Combat.Arena do
       "G" -> {:combatant, :goblin}
       " " -> {:void}
     end
+  end
+
+  @doc ~S"""
+  Return the opposing team.
+
+  ## Example
+
+      iex> Combat.Arena.opponent(:elf)
+      :goblin
+
+      iex> Combat.Arena.opponent(:goblin)
+      :elf
+  """
+  @spec opponent(team()) :: team()
+  def opponent(:elf), do: :goblin
+  def opponent(:goblin), do: :elf
+
+  @doc ~S"""
+  Find combatants for the given team.
+
+  ## Example
+
+      iex> arena = {%{}, MapSet.new([
+      ...>   {{42, 3}, :elf, 3, 200},
+      ...>   {{42, 4}, :goblin, 3, 2},
+      ...>   {{42, 7}, :elf, 3, 200},
+      ...> ])}
+      iex> Combat.Arena.find_combatants(arena, :goblin)
+      [{{42, 4}, :goblin, 3, 2}]
+
+      iex> arena = {%{}, MapSet.new([
+      ...>   {{42, 3}, :elf, 3, 200},
+      ...>   {{42, 7}, :elf, 3, 200},
+      ...> ])}
+      iex> Combat.Arena.find_combatants(arena, :goblin)
+      []
+  """
+  @spec find_combatants(arena(), team()) :: boolean()
+  def find_combatants({_grid, roster}, team) do
+    Enum.filter(roster, fn ({_pos, c_team, _pw, _hp}) -> c_team == team end)
+  end
+
+  @doc ~S"""
+  Execute a round of combat.
+
+  ## Returns
+
+  Tuple:
+  - updated arena
+  - is the battle over? (boolean)
+
+  ## Example
+
+      iex> arena = {%{
+      ...>     {0, 0} => :floor,
+      ...>     {0, 1} => :combatant,
+      ...>     {1, 0} => :combatant,
+      ...>     {1, 1} => :floor,
+      ...>   }, MapSet.new([
+      ...>     {{0, 1}, :goblin, 3, 2},
+      ...>     {{1, 0}, :elf, 3, 200},
+      ...>   ])
+      ...> }
+      iex> Combat.Arena.fight(arena) |> elem(1)
+      false
+  """
+  @spec fight(arena()) :: {arena(), boolean()}
+  def fight({grid, roster}) do
+    Enum.sort(roster, &(elem(&1, 0) <= elem(&2, 0)))
+    |> Enum.reduce_while({{grid, roster}, false}, fn ({pos, team, pw, hp}, {{grid_a, roster_a}, _done}) ->
+      IO.inspect({pos, team, pw, hp}, label: "next combatant")
+      enemies = find_combatants({grid_a, roster_a}, opponent(team))
+      if enemies != [] do
+        # TODO inspect, move, attack, etc.
+        {:cont, {{grid_a, roster_a}, false}}
+      else
+        {:halt, {{grid_a, roster_a}, true}}
+      end
+    end)
   end
 end
