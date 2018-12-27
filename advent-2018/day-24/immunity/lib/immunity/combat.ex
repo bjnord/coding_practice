551c570f82e0544f361573ab17a8e691fe629af9
Brent J. Nordquist <brent@nordist.net>
2018-12-26 21:27:44 -0600

    Day 24 part 1: Target selection

--- advent-2018/day-24/immunity/lib/immunity/combat.ex
+++ advent-2018/day-24/immunity/lib/immunity/combat.ex
@@ -9,7 +9,9 @@ defmodule Immunity.Combat do
   Fight!
   """
   def fight(army1, army2) do
-    all_groups = army1 ++ army2
+    all_groups =
+      army1 ++ army2
+      |> Enum.reduce(%{}, fn (group, acc) -> Map.put(acc, group.id, group) end)
     target_selection(all_groups)
   end
 
@@ -21,10 +23,17 @@ defmodule Immunity.Combat do
   zero or one groups."
   """
   def target_selection(groups) do
-    selecting_groups =
-      groups
+    selector_list =
+      Map.values(groups)
       |> Enum.sort_by(&(target_selector_precedence(&1)))
-      #|> IO.inspect(label: "target_selector_precedence")
+      |> IO.inspect(label: "target selector precedence")
+    targets =
+      selector_list
+      |> Enum.reduce({groups, %{}}, fn (att_group, {groups, targets}) ->
+        select_target(att_group, groups, targets)
+      end)
+      |> elem(1)
+      |> IO.inspect(label: "targets")
   end
 
   @doc """
@@ -35,5 +44,74 @@ defmodule Immunity.Combat do
   """
   def target_selector_precedence(group) do
     {-effective_power(group), -group.initiative}
+  end
+
+  @doc """
+  Select a target.
+
+  "If [an attacking group" cannot deal any defending groups damage,
+  it does not choose a target. Defending groups can only be chosen as
+  a target by one attacking group."
+  """
+  def select_target(att_group, groups, targets) do
+    best_targets =
+      Map.values(groups)
+      |> Enum.reject(fn (group) -> army_n(group) == army_n(att_group) end)
+      |> Enum.map(fn (group) -> {group, damage_from_attack(att_group, group)} end)
+      |> Enum.reject(fn ({_group, damage}) -> damage <= 0 end)
+      |> Enum.sort_by(fn ({group, damage}) -> target_choice(group, damage) end)
+      |> IO.inspect(label: "target priority for #{att_group.id}")
+    case best_targets do
+      [] ->
+        {groups, targets}
+      [{best_group, _damage} | _rest] ->
+        {Map.delete(groups, best_group.id), Map.put(targets, att_group.id, best_group.id)}
+    end
+  end
+
+  @doc """
+  Sort function for target choice.
+
+  "The attacking group chooses to target the group in the enemy army to
+  which it would deal the most damage (after accounting for weaknesses
+  and immunities, but not accounting for whether the defending group
+  has enough units to actually receive all of that damage)"
+
+  "If an attacking group is considering two defending groups to which
+  it would deal equal damage, it chooses to target the defending group
+  with the largest effective power; if there is still a tie, it chooses
+  the defending group with the highest initiative."
+  """
+  def target_choice(def_group, damage) do
+    {-damage, -effective_power(def_group), -def_group.initiative}
+  end
+
+  @doc """
+  Find damage from attack.
+
+  "By default, an attacking group would deal damage equal to its
+  effective power to the defending group." [but see "However" below]
+  """
+  def damage_from_attack(att_group, def_group) do
+    effective_power(att_group) * damage_multiplier(att_group, def_group)
+  end
+
+  @doc """
+  Find damage multiplier.
+
+  "However, if the defending group is immune to the attacking group's
+  attack type, the defending group instead takes no damage; if the
+  defending group is weak to the attacking group's attack type, the
+  defending group instead takes double damage."
+  """
+  def damage_multiplier(att_group, def_group) do
+    cond do
+      att_group.attack_type in def_group.immunity ->
+        0
+      att_group.attack_type in def_group.weakness ->
+        2
+      true ->
+        1
+    end
   end
 end
--- advent-2018/day-24/immunity/test/immunity/combat_test.exs
+++ advent-2018/day-24/immunity/test/immunity/combat_test.exs
@@ -5,7 +5,7 @@ defmodule Immunity.CombatTest do
   import Immunity.Combat
   import Immunity.Group
 
-  test "target precedence tuple" do
+  test "target selector precedence" do
     # NB "<" means "has earlier (higher) precedence than"
     group0 = new(0, 18, nil, nil, nil, 8, nil, 10)
     group1 = new(1, 100, nil, nil, nil, 16, nil, 9)
@@ -18,5 +18,27 @@ defmodule Immunity.CombatTest do
     # but higher effective_power trumps higher initiative
     group3 = new(3, 801, nil, nil, nil, 2, nil, 7)
     assert target_selector_precedence(group1) > target_selector_precedence(group3)
+  end
+
+  test "damage multiplier" do
+    group0 = new(0, nil, nil, [:fire], [], 0, :cold, 0)
+    group1 = new(1, nil, nil, [], [:radiation], 0, :fire, 0)
+    group2 = new(1, nil, nil, [], [], 0, :radiation, 0)
+
+    assert damage_multiplier(group2, group1) == 2
+    assert damage_multiplier(group2, group0) == 1
+    assert damage_multiplier(group1, group0) == 0
+    assert damage_multiplier(group1, group2) == 1
+  end
+
+  test "damage from attack" do
+    group0 = new(0, 17, nil, [:fire], [], 7, :cold, 0)
+    group1 = new(1, 19, nil, [], [:radiation], 5, :fire, 0)
+    group2 = new(1, 23, nil, [], [], 3, :radiation, 0)
+
+    assert damage_from_attack(group2, group1) == (23 * 3) * 2
+    assert damage_from_attack(group2, group0) == 23 * 3
+    assert damage_from_attack(group1, group0) == 0
+    assert damage_from_attack(group1, group2) == 19 * 5
   end
 end
