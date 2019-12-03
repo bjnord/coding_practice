cb8b8385d83d85a66319d786c0ae78ac552de41c
Brent J. Nordquist <brent@nordist.net>
2019-12-02 21:19:25 -0600

    RF: Tighten up test functions

--- advent-2019/day-01/test/module_mass2fuel_test.js
+++ advent-2019/day-01/test/module_mass2fuel_test.js
@@ -2,23 +2,19 @@ var expect = require('chai').expect;
 var module = require('../src/module');
 describe('module mass2fuel tests', function() {
   it('should return 2 for 12', function() {
-    var mass = 12;
-    var fuel = module.mass2fuel(mass);
-    expect(fuel).to.eql(2);
+    const mass = 12;
+    expect(module.mass2fuel(mass)).to.eql(2);
   });
   it('should return 2 for 14', function() {
-    var mass = 14;
-    var fuel = module.mass2fuel(mass);
-    expect(fuel).to.eql(2);
+    const mass = 14;
+    expect(module.mass2fuel(mass)).to.eql(2);
   });
   it('should return 654 for 1969', function() {
-    var mass = 1969;
-    var fuel = module.mass2fuel(mass);
-    expect(fuel).to.eql(654);
+    const mass = 1969;
+    expect(module.mass2fuel(mass)).to.eql(654);
   });
   it('should return 33583 for 100756', function() {
-    var mass = 100756;
-    var fuel = module.mass2fuel(mass);
-    expect(fuel).to.eql(33583);
+    const mass = 100756;
+    expect(module.mass2fuel(mass)).to.eql(33583);
   });
 });
--- advent-2019/day-01/test/module_mass2fullfuel_test.js
+++ advent-2019/day-01/test/module_mass2fullfuel_test.js
@@ -2,18 +2,15 @@ var expect = require('chai').expect;
 var module = require('../src/module');
 describe('module mass2fullfuel tests', function() {
   it('should return 2 for 14', function() {
-    var mass = 14;
-    var fuel = module.mass2fullfuel(mass);
-    expect(fuel).to.eql(2);
+    const mass = 14;
+    expect(module.mass2fullfuel(mass)).to.eql(2);
   });
   it('should return 966 for 1969', function() {
-    var mass = 1969;
-    var fuel = module.mass2fullfuel(mass);
-    expect(fuel).to.eql(966);
+    const mass = 1969;
+    expect(module.mass2fullfuel(mass)).to.eql(966);
   });
   it('should return 50346 for 100756', function() {
-    var mass = 100756;
-    var fuel = module.mass2fullfuel(mass);
-    expect(fuel).to.eql(50346);
+    const mass = 100756;
+    expect(module.mass2fullfuel(mass)).to.eql(50346);
   });
 });
