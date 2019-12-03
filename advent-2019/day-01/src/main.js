3a7cdd22e3b5cf026124e3a323bae014b77c9dff
Brent J. Nordquist <brent@nordist.net>
2019-12-02 20:54:33 -0600

    RF: camelCase not snake_case

--- advent-2019/day-01/src/main.js
+++ advent-2019/day-01/src/main.js
@@ -1,9 +1,9 @@
 const Module = require('../src/module');
 const fs = require('fs');
 const input = fs.readFileSync('input/input.txt', 'utf8');
-const module_masses = input.trim().split(/\s+/);
+const moduleMasses = input.trim().split(/\s+/);
 const summer = (acc, i) => acc + i;
-const total = module_masses
+const total = moduleMasses
   .map(mass => {
     var module = new Module;
     return module.mass2fuel(mass);
@@ -13,11 +13,11 @@ console.log('part 1: expected answer is:          3405721');
 console.log(`part 1: sum of fuel requirements is: ${total}`);
 console.log('');
 
-const full_total = module_masses
+const fullTotal = moduleMasses
   .map(mass => {
     var module = new Module;
     return module.mass2fullfuel(mass);
   })
   .reduce(summer);
 console.log('part 2: expected answer is:          5105716');
-console.log(`part 2: sum of fuel requirements is: ${full_total}`);
+console.log(`part 2: sum of fuel requirements is: ${fullTotal}`);
