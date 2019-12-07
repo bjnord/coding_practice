a9be3adb8343047c11121d0485989c6d22435fc2
Brent J. Nordquist <brent@nordist.net>
2019-12-07 12:49:31 -0600

    Fix lint errors

--- advent-2019/day-07/test/permutator_test.js
+++ advent-2019/day-07/test/permutator_test.js
@@ -20,9 +20,9 @@ describe('permutator tests', () => {
   it('returns Set of Arrays with size of nPicks', () => {
     const nPicks = 2;
     const result = permutator.permute([1,2,3], nPicks);
-    expect(result instanceof Set).to.be.true
+    expect(result instanceof Set).to.be.true;
     result.forEach((p) => {
-      expect(Array.isArray(p)).to.be.true
+      expect(Array.isArray(p)).to.be.true;
       expect(p.length).to.eql(nPicks);
     });
   });
@@ -45,22 +45,28 @@ describe('permutator tests', () => {
     expect(permutator.permute([1, 2], 2)).to.eql(exp);
   });
   it('returns correct permutations [3 pick 2]', () => {
-    const exp = new Set([[1, 2], [1, 3],
-                         [2, 1], [2, 3],
-                         [3, 1], [3, 2]]);
+    const exp = new Set([
+      [1, 2], [1, 3],
+      [2, 1], [2, 3],
+      [3, 1], [3, 2],
+    ]);
     expect(permutator.permute([1, 2, 3], 2)).to.eql(exp);
   });
   it('returns correct permutations [3 pick 3]', () => {
-    const exp = new Set([[1, 2, 3], [1, 3, 2],
-                         [2, 1, 3], [2, 3, 1],
-                         [3, 1, 2], [3, 2, 1]]);
+    const exp = new Set([
+      [1, 2, 3], [1, 3, 2],
+      [2, 1, 3], [2, 3, 1],
+      [3, 1, 2], [3, 2, 1],
+    ]);
     expect(permutator.permute([1, 2, 3], 3)).to.eql(exp);
   });
   it('returns correct permutations [4 pick 3]', () => {
-    const exp = new Set([[1, 2, 3], [1, 3, 2], [1, 3, 4], [1, 4, 3], [1, 2, 4], [1, 4, 2],
-                         [2, 1, 3], [2, 3, 1], [2, 1, 4], [2, 4, 1], [2, 3, 4], [2, 4, 3],
-                         [3, 1, 2], [3, 2, 1], [3, 1, 4], [3, 4, 1], [3, 2, 4], [3, 4, 2],
-                         [4, 1, 2], [4, 2, 1], [4, 1, 3], [4, 3, 1], [4, 2, 3], [4, 3, 2]]);
+    const exp = new Set([
+      [1, 2, 3], [1, 3, 2], [1, 3, 4], [1, 4, 3], [1, 2, 4], [1, 4, 2],
+      [2, 1, 3], [2, 3, 1], [2, 1, 4], [2, 4, 1], [2, 3, 4], [2, 4, 3],
+      [3, 1, 2], [3, 2, 1], [3, 1, 4], [3, 4, 1], [3, 2, 4], [3, 4, 2],
+      [4, 1, 2], [4, 2, 1], [4, 1, 3], [4, 3, 1], [4, 2, 3], [4, 3, 2],
+    ]);
     expect(permutator.permute([1, 2, 3, 4], 3)).to.eql(exp);
   });
 });
