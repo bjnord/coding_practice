6698ad7ea394c1076bd589a6cbfe457e458dcdab
Brent J. Nordquist <brent@nordist.net>
2019-12-07 14:51:36 -0600

    Day 7 part 2: Working runFeedback() method

--- advent-2019/day-07/src/amp_set.js
+++ advent-2019/day-07/src/amp_set.js
@@ -12,15 +12,54 @@ const intcode = require('../src/intcode');
 //   signal output from the last amp in the chain
 const run = (program, settings) => {
   const values = [0];
-  const popValue = () => values.shift();
-  const pushValue = (v) => values.unshift(v);
+  const getValue = () => values.shift();
+  const storeValue = (v) => values.push(v);
   for (const setting of settings) {
     // "Make sure that memory is not shared or reused between copies of the program."
     const programCopy = program.slice();
     // Intcode inputs: FIRST the amp phase setting, SECOND the signal value
     values.unshift(setting);
-    intcode.run(programCopy, false, popValue, pushValue);
+    intcode.run(programCopy, false, getValue, storeValue);
   }
   return values[0];
 };
+// Run program on a chain of amplifiers in feedback mode. Signal value "0"
+// will be sent as input to the first amp in the chain. Each amp's output
+// will be sent to the next amp's input, with the last amp's output being
+// sent back to the first amp's input again. This process continues until
+// all amps have halted.
+//
+// PARAMETERS
+//   program: program to run (will not be altered)
+//   settings: Array of amp phase settings (5..9), one per amp
+//
+// RETURNS
+//   signal output from the last amp in the chain, after all amps have halted
+const runFeedback = (program, settings) => {
+  const amps = settings.map((s) => ({
+    program: program.slice(),
+    values: [s],
+    pc: 0,
+  }));
+  let curAmp = 0;
+  const getValue = () => amps[curAmp].values.shift();
+  const storeValue = (v) => amps[(curAmp+1) % settings.length].values.push(v);
+  // Intcode inputs: FIRST the amp phase setting, SECOND the signal value
+  amps[0].values.push(0);
+  for (;;) {
+    //console.log(`< amp ${curAmp} PC: ${amps[curAmp].pc} values: ${amps[curAmp].values}`);
+    amps[curAmp].pc = intcode.run(amps[curAmp].program, false, getValue, storeValue, amps[curAmp].pc);
+    //console.log(`> amp ${curAmp} PC: ${amps[curAmp].pc} values: ${amps[curAmp].values}`);
+    if (amps.every((a) => a.pc < 0)) {
+      break;
+    }
+    curAmp = (curAmp+1) % settings.length;
+  }
+  //console.log('FINAL STATE');
+  //amps.forEach((amp, i) => {
+  //  console.log(`amp ${i} PC: ${amp.pc} values: ${amp.values}`);
+  //});
+  return amps[0].values[0];
+};
+exports.runFeedback = runFeedback;
 exports.run = run;
--- advent-2019/day-07/test/amp_set_test.js
+++ advent-2019/day-07/test/amp_set_test.js
@@ -18,3 +18,15 @@ describe('amp set run tests', () => {
     expect(ampSet.run(program, settings)).to.eql(65210);
   });
 });
+describe('amp set runFeedback tests', () => {
+  it('should return 139629729 from feedback example program 1 with settings 9,8,7,6,5', () => {
+    const program = [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5];
+    const settings = [9,8,7,6,5];
+    expect(ampSet.runFeedback(program, settings)).to.eql(139629729);
+  });
+  it('should return 18216 from feedback example program 2 with settings 9,7,8,5,6', () => {
+    const program = [3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10];
+    const settings = [9,7,8,5,6];
+    expect(ampSet.runFeedback(program, settings)).to.eql(18216);
+  });
+});
