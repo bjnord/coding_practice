107d9b6fe063fc4d315e67c1e2f5445fda74010f
Brent J. Nordquist <brent@nordist.net>
2019-12-07 14:53:56 -0600

    Day 7 part 2, first working solution

--- advent-2019/day-07/src/main.js
+++ advent-2019/day-07/src/main.js
@@ -13,3 +13,12 @@ const signal = permutations
 console.log('part 1: expected answer is:   398674');
 console.log(`part 1: highest signal value: ${signal}`);
 console.log('');
+
+// PART 2
+const fPermutations = Array.from(permutator.permute([5, 6, 7, 8, 9], 5));
+const fSignal = fPermutations
+  .map((p) => ampSet.runFeedback(program, p))
+  .reduce((max, s) => Math.max(s, max), 0);
+console.log('part 2: expected answer is:   39431233');
+console.log(`part 2: highest signal value: ${fSignal}`);
+console.log('');
