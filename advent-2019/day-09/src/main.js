78608255c54e8f34f6c53763b46ac86c9e9dbc7b
Brent J. Nordquist <brent@nordist.net>
2019-12-09 19:12:04 -0600

    Day 9 part 2, first working solution

--- advent-2019/day-09/src/main.js
+++ advent-2019/day-09/src/main.js
@@ -5,8 +5,15 @@ const input = fs.readFileSync('input/input.txt', 'utf8');
 const programStr = input.trim().split(/,/);
 
 // PART 1
-const program = programStr.map((str) => Number(str));
+let program = programStr.map((str) => Number(str));
 console.log('[Part One: Enter BOOST test mode value (1):]');
 intcode.run(program);
 console.log('[Part One: Finished (output value should be 2870072642)');
+console.log('');
+
+// PART 2
+program = programStr.map((str) => Number(str));
+console.log('[Part Two: Enter BOOST sensor boost mode value (2):]');
+intcode.run(program);
+console.log('[Part Two: Finished (output value should be 58534)');
 console.log('');
