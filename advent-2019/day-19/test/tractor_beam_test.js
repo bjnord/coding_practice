cb7e8ea45a8c1b2b638767022827b70918d10792
Brent J. Nordquist <brent@nordist.net>
2019-12-19 07:38:06 -0600

    Day 19 part 1: Test with puzzle example #1

---
+++ advent-2019/day-19/test/tractor_beam_test.js
@@ -0,0 +1,26 @@
+'use strict';
+const expect = require('chai').expect;
+const TractorBeam = require('../src/tractor_beam');
+
+// #.........
+// .#........
+// ..##......
+// ...###....
+// ....###...
+// .....####.
+// ......####
+// ......####
+// .......###
+// ........##
+const puzzleExample1 = '3,20,3,21,2,21,22,24,1,24,20,24,9,24,204,25,99,0,0,0,0,0,10,10,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,1,1';
+
+describe('tractor beam affected-squares tests [puzzle example #1]', () => {
+  let example1;
+  before(() => {
+    example1 = new TractorBeam(puzzleExample1);
+    example1.run(10);
+  });
+  it('should compute affected squares correctly', () => {
+    expect(example1.squaresAffected).to.eql(27);
+  });
+});
