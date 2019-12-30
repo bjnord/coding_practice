a5bacd3a3c138304703ce2830ede5ef41d34bb26
Brent J. Nordquist <brent@nordist.net>
2019-12-29 18:27:43 -0600

    Disable debug output

--- advent-2019/day-24/src/main.js
+++ advent-2019/day-24/src/main.js
@@ -8,8 +8,8 @@ const inputLines = input.trim().split(/\n/);
 // PART 1
 const initialState = bugs.parse(inputLines);
 const finalState = bugs.iterate(initialState);
-console.debug('part 1: repeated state:');
-bugs.format(finalState).forEach((line) => console.debug('        '+line));
+//console.debug('part 1: repeated state:');
+//bugs.format(finalState).forEach((line) => console.debug('        '+line));
 console.log('part 1: expected answer:      13500447');
 console.log(`part 1: biodiversity rating:  ${finalState}`);
 console.log('');
