9545cae4b873c81014e3391db0c50c86274c9b98
Brent J. Nordquist <brent@nordist.net>
2020-09-07 14:20:46 -0500

    A few more tests

--- synacor/spec/memory_spec.rb
+++ synacor/spec/memory_spec.rb
@@ -6,11 +6,22 @@ describe Memory do
     expect(memory.get(0)).to be == 0x0
     expect(memory.get(Memory::MAX_ADDRESS)).to be == 0x0
   end
+  it 'should retrieve a previously-stored 16-bit value' do
+    expect(memory.get(0x55)).to be == 0x0
+    memory.set(0x55, 0x9005)
+    expect(memory.get(0x55)).to be == 0x9005
+  end
 
   describe '#get' do
     let(:memory) { Memory.new }
     it 'should disallow negative addresses' do
       expect{ memory.get(-1) }.to raise_error(MemoryError, 'negative address')
+    end
+    it 'should allow minimum address' do
+      expect{ memory.get(0x0) }.not_to raise_error
+    end
+    it 'should allow maximum address' do
+      expect{ memory.get(Memory::MAX_ADDRESS) }.not_to raise_error
     end
     it 'should disallow addresses outside address space' do
       expect{ memory.get(Memory::MAX_ADDRESS+1) }.to raise_error(MemoryError, 'address outside address space')
@@ -22,11 +33,23 @@ describe Memory do
     it 'should disallow negative addresses' do
       expect{ memory.set(-1, 0x1) }.to raise_error(MemoryError, 'negative address')
     end
+    it 'should allow minimum address' do
+      expect{ memory.set(0x0, 0x5) }.not_to raise_error
+    end
+    it 'should allow maximum address' do
+      expect{ memory.set(Memory::MAX_ADDRESS, 0x6) }.not_to raise_error
+    end
     it 'should disallow addresses outside address space' do
       expect{ memory.set(Memory::MAX_ADDRESS+1, 0x2) }.to raise_error(MemoryError, 'address outside address space')
     end
     it 'should disallow negative values' do
       expect{ memory.set(0x3, -1) }.to raise_error(MemoryError, 'negative value')
+    end
+    it 'should allow minimum value' do
+      expect{ memory.set(0x7, 0x0) }.not_to raise_error
+    end
+    it 'should allow maximum value' do
+      expect{ memory.set(0x8, Memory::MAX_VALUE) }.not_to raise_error
     end
     it 'should disallow too-large values' do
       expect{ memory.set(0x4, Memory::MAX_VALUE+1) }.to raise_error(MemoryError, 'value overflow')
