741d32a6209c12edc493dca6f6bc37058d674596
Brent J. Nordquist <brent@nordist.net>
2020-09-12 21:33:36 -0500

    Fix east-west error
    I dunno; north and south are absolutely clear in my mind, but since I
    can remember, I've always had to think hard about west and east, and
    often get them backward

--- synacor/doc/maps/ruins.mm
+++ synacor/doc/maps/ruins.mm
@@ -3,12 +3,12 @@ A[Ruins]-->|north|B
 B-->|south|A
 B[Ruins foyer=red]-->|north|C
 C-->|south|B
-C[Ruins hall]-->|west|D
-D-->|east|C
+C[Ruins hall]-->|east|D
+D-->|west|C
 D[Ruins dining=concave]-->|down|E
 E[Ruins kitchen=corroded]-->|up|D
-C-->|east|F
-F-->|west|C
+C-->|west|F
+F-->|east|C
 F[Ruins living=blue]-->|up|G
 G[Ruins throne=shiny]-->|down|F
 C-->|north|H
