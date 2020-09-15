f2ff6f4deb4a43c88f1d3807a83513a104552fe6
Brent J. Nordquist <brent@nordist.net>
2020-09-15 17:48:34 -0500

    RF: Move validation to struct methods

--- hogan-57/5-func/valid_input/src/main.rs
+++ hogan-57/5-func/valid_input/src/main.rs
@@ -11,12 +11,74 @@ struct Employee {
     zip: String,
 }
 
+impl Employee {
+    fn validate(&self) {
+        // FIXME make this 4-way test more concise
+        let e_f_name = self.validate_f_name();
+        let e_l_name = self.validate_l_name();
+        let e_emp_id = self.validate_emp_id();
+        let e_zip = self.validate_zip();
+        if e_f_name && e_l_name && e_emp_id && e_zip {
+            println!("There were no errors found.");
+        }
+    }
+
+    fn validate_f_name(&self) -> bool {
+        if self.f_name.len() == 0 {
+            println!("The first name must be filled in.");
+            false
+        } else if self.f_name.len() < 2 {
+            println!("\"{}\" is not a valid first name. It is too short.", self.f_name);
+            false
+        } else {
+            true
+        }
+    }
+
+    fn validate_l_name(&self) -> bool {
+        if self.l_name.len() == 0 {
+            println!("The last name must be filled in.");
+            false
+        } else if self.l_name.len() < 2 {
+            println!("\"{}\" is not a valid last name. It is too short.", self.l_name);
+            false
+        } else {
+            true
+        }
+    }
+
+    fn validate_emp_id(&self) -> bool {
+        // FIXME use lazy_static; see:
+        //       https://docs.rs/regex/1.3.9/regex/#example-avoid-compiling-the-same-regex-in-a-loop
+        let re = Regex::new(r"^(?i)[a-z]{2}-\d{4}$").unwrap();
+        if re.is_match(&self.emp_id[..]) {
+            true
+        } else {
+            println!("{} is not a valid ID.", self.emp_id);
+            false
+        }
+    }
+
+    fn validate_zip(&self) -> bool {
+        // FIXME use lazy_static; see:
+        //       https://docs.rs/regex/1.3.9/regex/#example-avoid-compiling-the-same-regex-in-a-loop
+        let re = Regex::new(r"^\d{5}(?:-\d{4})?$").unwrap();
+        if re.is_match(&self.zip[..]) {
+            true
+        } else {
+            println!("The ZIP code must be numeric.");
+            false
+        }
+    }
+}
+
 fn main() {
     let emp = read_employee();
-    validate_employee(&emp);
+    emp.validate();
     //println!("employee data: {:#?}", emp);
 }
 
+// TODO move this to impl also
 fn read_employee() -> Employee {
     let f_name = read_input("Enter the first name: ");
     let l_name = read_input("Enter the last name: ");
@@ -43,58 +105,4 @@ fn read_response() -> String {
         .read_line(&mut input)
         .expect("Failed to read line");
     String::from(input.trim())
-}
-
-fn validate_employee(emp: &Employee) {
-    let e_f_name = validate_f_name(&emp);
-    let e_l_name = validate_l_name(&emp);
-    let e_emp_id = validate_emp_id(&emp);
-    let e_zip = validate_zip(&emp);
-    if e_f_name && e_l_name && e_emp_id && e_zip {
-        println!("There were no errors found.");
-    }
-}
-
-fn validate_f_name(emp: &Employee) -> bool {
-    if emp.f_name.len() == 0 {
-        println!("The first name must be filled in.");
-        false
-    } else if emp.f_name.len() < 2 {
-        println!("\"{}\" is not a valid first name. It is too short.", emp.f_name);
-        false
-    } else {
-        true
-    }
-}
-
-fn validate_l_name(emp: &Employee) -> bool {
-    if emp.l_name.len() == 0 {
-        println!("The last name must be filled in.");
-        false
-    } else if emp.l_name.len() < 2 {
-        println!("\"{}\" is not a valid last name. It is too short.", emp.l_name);
-        false
-    } else {
-        true
-    }
-}
-
-fn validate_emp_id(emp: &Employee) -> bool {
-    let re = Regex::new(r"^(?i)[a-z]{2}-\d{4}$").unwrap();
-    if re.is_match(&emp.emp_id[..]) {
-        true
-    } else {
-        println!("{} is not a valid ID.", emp.emp_id);
-        false
-    }
-}
-
-fn validate_zip(emp: &Employee) -> bool {
-    let re = Regex::new(r"^\d{5}(?:-\d{4})?$").unwrap();
-    if re.is_match(&emp.zip[..]) {
-        true
-    } else {
-        println!("The ZIP code must be numeric.");
-        false
-    }
 }
