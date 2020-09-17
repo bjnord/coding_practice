c1c59d7fc82eb2bd31fd3af020e1bc15e23b0867
Brent J. Nordquist <brent@nordist.net>
2020-09-17 07:13:49 -0500

    RF as EmployeeList

--- hogan-57/7-struct/emp_list/src/main.rs
+++ hogan-57/7-struct/emp_list/src/main.rs
@@ -1,15 +1,51 @@
 use std::io::{self, Write};
 
-// TODO create EmployeeList type
+struct EmployeeList {
+    employees: Vec<String>,
+}
+
+impl EmployeeList {
+    fn from_memory() -> EmployeeList {
+        let mut employees = Vec::new();
+        employees.push(String::from("Fred Flintstone"));
+        employees.push(String::from("Wilma Flintstone"));
+        employees.push(String::from("Barney Rubble"));
+        employees.push(String::from("Betty Rubble"));
+        EmployeeList {employees}
+    }
+
+    // TODO from_file(filename)
+
+    // TODO count() [and use it in dump()]
+
+    fn dump(&self) {
+        let count = self.employees.len();
+        match count {
+            1 => { println!("There is 1 employee:"); },
+            _ => { println!("There are {} employees:", count); },
+        }
+        for emp in &self.employees {
+            println!("{}", emp);
+        }
+    }
+
+    fn remove(&mut self, name: &str) {
+        match self.employees.iter().position(|n| *n == name) {
+            Some(i) => { self.employees.remove(i); },
+            None => { println!("Employee '{}' not found.", name); },
+        }
+    }
+}
 
 fn main() {
-    let mut emp_list = load_employees();
-    dump_employees(&emp_list);
+    let mut emp_list = EmployeeList::from_memory();
+    emp_list.dump();
     println!("");
+    // TODO do the following in a loop; end when empty
     let name = read_input("Enter an employee name to remove: ");
-    delete_employee(&mut emp_list, &name[..]);
+    emp_list.remove(&name[..]);
     println!("");
-    dump_employees(&emp_list);
+    emp_list.dump();
 }
 
 fn read_input(prompt: &str) -> String {
@@ -29,32 +65,4 @@ fn read_response() -> String {
         .read_line(&mut input)
         .expect("Failed to read line");
     String::from(input.trim())
-}
-
-// TODO get these from a file
-fn load_employees() -> Vec<String> {
-    let mut emp_list = Vec::new();
-    emp_list.push(String::from("Fred Flintstone"));
-    emp_list.push(String::from("Wilma Flintstone"));
-    emp_list.push(String::from("Barney Rubble"));
-    emp_list.push(String::from("Betty Rubble"));
-    emp_list
-}
-
-fn dump_employees(emp_list: &Vec<String>) {
-    let count = emp_list.len();
-    match count {
-        1 => { println!("There is 1 employee:"); },
-        _ => { println!("There are {} employees:", count); },
-    }
-    for emp in emp_list {
-        println!("{}", emp);
-    }
-}
-
-fn delete_employee(emp_list: &mut Vec<String>, name: &str) {
-    match emp_list.iter().position(|x| *x == name) {
-        Some(i) => { emp_list.remove(i); },
-        None => { println!("Employee '{}' not found.", name); },
-    }
 }
