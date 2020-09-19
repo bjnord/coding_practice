471dee70bce482cc2ece727d3890ad88b6191258
Brent J. Nordquist <brent@nordist.net>
2020-09-19 15:09:34 -0500

    Read params; generate 10 passwords to choose from

--- hogan-57/7-struct/password_gen/src/main.rs
+++ hogan-57/7-struct/password_gen/src/main.rs
@@ -1,5 +1,13 @@
+extern crate interact_io;
+use interact_io::readln;
+
 fn main() {
-    let pw = password_gen::generate(8, 2, 2);
-    println!("Your password is");
-    println!("{}", pw);
+    let min_len = readln::read_u32("What's the minimum length? ");
+    let n_specials = readln::read_u32("How many special characters? ");
+    let n_numbers = readln::read_u32("How many numbers? ");
+    println!("Your passwords are:");
+    for _ in 0..10 {
+        let pw = password_gen::generate(min_len, n_numbers, n_specials);
+        println!("{}", pw);
+    }
 }
