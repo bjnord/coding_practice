8a84aeb087e18cb3d4a72486399fc70d2a5f392d
Brent J. Nordquist <brent@nordist.net>
2020-09-20 17:30:16 -0500

    RF: Split employee and roster

--- hogan-57/7-struct/emp_roster/src/lib.rs
+++ hogan-57/7-struct/emp_roster/src/lib.rs
@@ -9,43 +9,6 @@ pub struct Employee {
 }
 
 impl Employee {
-    #[cfg(test)]
-    fn for_tests() -> Vec<Employee> {
-        Employee::from_string("\
-            Name                | Position          | Separation Date\n\
-            --------------------|-------------------|----------------\n\
-            John Johnson        | Manager           | 2016-12-31\n\
-            Tou Xiong           | Software Engineer | 2016-10-05\n\
-            Michaela Michaelson | District Manager  | 2015-12-19     \n\
-            Jake Jacobson       | Programmer        |                \n\
-            Jacquelyn Jackson   | DBA               | \n\
-            Sally Weber         | Web Developer     | 2015-12-18\n")
-    }
-
-    pub fn from_file(filename: &str) -> Vec<Employee> {
-        // TODO read file to string, pass to from_string()
-        //      change return types of both to Result for errors
-        Vec::<Employee>::new()
-    }
-
-    pub fn from_string(input: &str) -> Vec<Employee> {
-        let mut roster: Vec<Employee> = Vec::<Employee>::new();
-        let lines: Vec<&str> = input.split("\n").collect();
-        for line in lines {
-            let tokens: Vec<&str> = line.split("|").collect();
-            if tokens.len() == 1 && tokens[0].trim().is_empty() {
-                continue;  // ignore blank lines
-            } else if tokens.len() != 3 {
-                panic!("line in unknown format (tokens={}): [{}]", tokens.len(), line);
-            }
-            match Employee::from_tokens(tokens) {
-                Some(emp) => roster.push(emp),
-                None => (),
-            }
-        }
-        roster
-    }
-
     pub fn from_tokens(tokens: Vec<&str>) -> Option<Employee> {
         lazy_static! {
             static ref LINE_RE: Regex = Regex::new(r"^---*$").unwrap();
@@ -63,20 +26,73 @@ impl Employee {
     }
 }
 
+pub struct EmployeeRoster {
+    roster: Vec<Employee>,
+}
+
+impl EmployeeRoster {
+    #[cfg(test)]
+    fn for_tests() -> EmployeeRoster {
+        EmployeeRoster::from_string("\
+            Name                | Position          | Separation Date\n\
+            --------------------|-------------------|----------------\n\
+            John Johnson        | Manager           | 2016-12-31\n\
+            Tou Xiong           | Software Engineer | 2016-10-05\n\
+            Michaela Michaelson | District Manager  | 2015-12-19     \n\
+            Jake Jacobson       | Programmer        |                \n\
+            Jacquelyn Jackson   | DBA               | \n\
+            Sally Weber         | Web Developer     | 2015-12-18\n")
+    }
+
+    pub fn from_file(_filename: &str) -> EmployeeRoster {
+        // TODO read file to string, pass to from_string()
+        //      change return types of both to Result for errors
+        EmployeeRoster::from_string("")
+    }
+
+    pub fn from_string(input: &str) -> EmployeeRoster {
+        let mut roster: Vec<Employee> = Vec::<Employee>::new();
+        let lines: Vec<&str> = input.split("\n").collect();
+        for line in lines {
+            let tokens: Vec<&str> = line.split("|").collect();
+            if tokens.len() == 1 && tokens[0].trim().is_empty() {
+                // FIXME add blank lines to test input, to exercise this check
+                continue;  // ignore blank lines
+            } else if tokens.len() != 3 {
+                // FIXME return error Result
+                panic!("line in unknown format (tokens={}): [{}]", tokens.len(), line);
+            }
+            match Employee::from_tokens(tokens) {
+                Some(emp) => roster.push(emp),
+                None => (),
+            }
+        }
+        EmployeeRoster {roster}
+    }
+
+    pub fn len(&self) -> usize {
+        self.roster.len()
+    }
+
+    pub fn find(&self, f_name: &str, l_name: &str) -> Option<&Employee> {
+        self.roster.iter().find(|e| e.f_name == f_name && e.l_name == l_name)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn parsing_roster() {
-        let roster: Vec<Employee> = Employee::for_tests();
-        assert_eq!(6, roster.len());
-        let emp = &roster[0];
+        let er: EmployeeRoster = EmployeeRoster::for_tests();
+        assert_eq!(6, er.len());
+        let emp = er.find("John", "Johnson").unwrap();
         assert_eq!("John", emp.f_name);
         assert_eq!("Johnson", emp.l_name);
         assert_eq!("Manager", emp.position);
         assert_eq!("2016-12-31", emp.separation);
-        let emp = &roster[3];
+        let emp = er.find("Jake", "Jacobson").unwrap();
         assert_eq!("Jake", emp.f_name);
         assert_eq!("Jacobson", emp.l_name);
         assert_eq!("Programmer", emp.position);
