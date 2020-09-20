212eb69c071a0153a0b020b905e0ab85e5be1794
Brent J. Nordquist <brent@nordist.net>
2020-09-20 10:17:16 -0500

    RF: Let caller do the output

--- hogan-57/2-io/retirement/src/lib.rs
+++ hogan-57/2-io/retirement/src/lib.rs
@@ -1,29 +1,28 @@
 use chrono::{Datelike, Utc};
 
 pub struct RetirementYears {
-    years: i32,
-    cur_year: i32,
+    pub years: i32,
+    pub cur_year: i32,
+    pub ret_year: i32,
 }
 
 impl RetirementYears {
     pub fn from_ages(cur_age: i32, ret_age: i32) -> RetirementYears {
         let years = ret_age - cur_age;
         let cur_year = RetirementYears::get_cur_year() as i32;
-        RetirementYears {years, cur_year}
+        let ret_year = cur_year + years;
+        RetirementYears {years, cur_year, ret_year}
     }
 
-    // TODO change this to calculate() which returns an enum (move ret_year into structure)
-    //      let the caller handle the printing
+    // TODO change this to calculate() which returns an enum
     pub fn show(&self) {
         if self.years < 0 {
-            let ret_year = self.cur_year + self.years;
-            println!("It's {}, and you could have retired back in {}.", self.cur_year, ret_year);
+            println!("It's {}, and you could have retired back in {}.", self.cur_year, self.ret_year);
         } else if self.years == 0 {
             println!("It's {}, and you can retire now.", self.cur_year);
         } else {
             println!("You have {} years left until you can retire.", self.years);
-            let ret_year = self.cur_year + self.years;
-            println!("It's {}, so you can retire in {}.", self.cur_year, ret_year);
+            println!("It's {}, so you can retire in {}.", self.cur_year, self.ret_year);
         }
     }
 
@@ -34,3 +33,5 @@ impl RetirementYears {
         year
     }
 }
+
+// TODO add tests
--- hogan-57/2-io/retirement/src/main.rs
+++ hogan-57/2-io/retirement/src/main.rs
@@ -5,5 +5,17 @@ use retirement::RetirementYears;
 fn main() {
     let cur_age = readln::read_i32_range("What is your current age? ", 1, 120).unwrap();
     let ret_age = readln::read_i32_range("At what age would you like to retire? ", 1, 120).unwrap();
-    RetirementYears::from_ages(cur_age, ret_age).show();
+    let ry = RetirementYears::from_ages(cur_age, ret_age);
+    show_retirement(ry);
+}
+
+fn show_retirement(ry: RetirementYears) {
+    if ry.years < 0 {
+        println!("It's {}, and you could have retired back in {}.", ry.cur_year, ry.ret_year);
+    } else if ry.years == 0 {
+        println!("It's {}, and you can retire now.", ry.cur_year);
+    } else {
+        println!("You have {} years left until you can retire.", ry.years);
+        println!("It's {}, so you can retire in {}.", ry.cur_year, ry.ret_year);
+    }
 }
