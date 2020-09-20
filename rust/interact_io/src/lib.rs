4c30b0b1738f126f54e6a5fa843f14181ff3699a
Brent J. Nordquist <brent@nordist.net>
2020-09-20 09:06:37 -0500

    Step 2: Propagate errors

--- rust/interact_io/src/lib.rs
+++ rust/interact_io/src/lib.rs
@@ -1,25 +1,25 @@
 pub mod readln {
     use std::io::{self, Write};
 
-    pub fn read_i32(prompt: &str) -> i32 {
+    pub fn read_i32(prompt: &str) -> Result<i32, io::Error> {
         loop {
-            print_prompt(prompt).unwrap();
-            let resp = read_response().unwrap();
+            print_prompt(prompt)?;
+            let resp = read_response()?;
             match resp.parse::<i32>() {
-                Ok(n) => break n,
+                Ok(n) => return Ok(n),
                 Err(e) => println!("That is not a valid number: {}", e)
             }
         }
     }
 
-    pub fn read_i32_range(prompt: &str, min: i32, max: i32) -> i32 {
+    pub fn read_i32_range(prompt: &str, min: i32, max: i32) -> Result<i32, io::Error> {
         loop {
-            print_prompt(prompt).unwrap();
-            let resp = read_response().unwrap();
+            print_prompt(prompt)?;
+            let resp = read_response()?;
             match resp.parse::<i32>() {
                 Ok(n) => {
                     if n >= min && n <= max {
-                        break n;
+                        return Ok(n);
                     } else {
                         println!("Number must be between {} and {}.", min, max);
                     }
@@ -29,31 +29,32 @@ pub mod readln {
         }
     }
 
-    pub fn read_u32(prompt: &str) -> u32 {
+    pub fn read_u32(prompt: &str) -> Result<u32, io::Error> {
         loop {
-            print_prompt(prompt).unwrap();
-            let resp = read_response().unwrap();
+            print_prompt(prompt)?;
+            let resp = read_response()?;
             match resp.parse::<u32>() {
-                Ok(n) => break n,
+                Ok(n) => return Ok(n),
                 Err(e) => println!("That is not a valid number: {}", e)
             }
         }
     }
 
-    pub fn read_f64(prompt: &str) -> f64 {
+    pub fn read_f64(prompt: &str) -> Result<f64, io::Error> {
         loop {
-            print_prompt(prompt).unwrap();
-            let resp = read_response().unwrap();
+            print_prompt(prompt)?;
+            let resp = read_response()?;
             match resp.parse::<f64>() {
-                Ok(n) => break n,
+                Ok(n) => return Ok(n),
                 Err(e) => println!("That is not a valid number: {}", e)
             }
         }
     }
 
-    pub fn read_string(prompt: &str) -> String {
-        print_prompt(prompt).unwrap();
-        read_response().unwrap()
+    pub fn read_string(prompt: &str) -> Result<String, io::Error> {
+        print_prompt(prompt)?;
+        let resp = read_response()?;
+        Ok(resp)
     }
 
     // private
--- rust/interact_io/src/main.rs
+++ rust/interact_io/src/main.rs
@@ -1,14 +1,14 @@
 use interact_io::readln;
 
 fn main() {
-    let n = readln::read_i32("Enter a signed integer: ");
+    let n = readln::read_i32("Enter a signed integer: ").unwrap();
     println!("Your signed integer was: {}", n);
-    let n = readln::read_i32_range("Enter a signed integer between -10 and 10: ", -10, 10);
+    let n = readln::read_i32_range("Enter a signed integer between -10 and 10: ", -10, 10).unwrap();
     println!("Your signed integer was: {}", n);
-    let n = readln::read_u32("Enter an unsigned integer: ");
+    let n = readln::read_u32("Enter an unsigned integer: ").unwrap();
     println!("Your unsigned integer was: {}", n);
-    let n = readln::read_f64("Enter a floating-point number: ");
+    let n = readln::read_f64("Enter a floating-point number: ").unwrap();
     println!("Your floating-poing number was: {}", n);
-    let s = readln::read_string("Enter a string: ");
+    let s = readln::read_string("Enter a string: ").unwrap();
     println!("Your string was: [{}]", s);
 }
