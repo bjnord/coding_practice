ea615769e435110ea8e7653fec65dd522f799b05
Brent J. Nordquist <brent@nordist.net>
2020-10-22 21:54:10 -0500

    Dynamically calculate necessary column widths

--- hogan-57/8-files/parse_file/src/main.rs
+++ hogan-57/8-files/parse_file/src/main.rs
@@ -13,7 +13,7 @@ struct Record {
 }
 
 impl Record {
-    fn new(line: &str) -> Result<Record, Box<dyn Error>> {
+    fn parse(line: &str) -> Result<Record, Box<dyn Error>> {
         let fields: Vec<&str> = line.split(",").collect();
         match fields.len() {
             3 => Ok(Record {
@@ -25,13 +25,23 @@ impl Record {
         }
     }
 
-    fn dump_header() {
-        println!("Last     First     Salary");
-        println!("-------------------------");
+    fn new() -> Result<Record, Box<dyn Error>> {
+        Ok(Record {l_name: String::new(), f_name: String::new(), salary: 0})
     }
 
-    fn dump(&self) {
-        println!("{ln:<lw$}{gn:<gw$}{s}", ln=self.l_name, lw=9, gn=self.f_name, gw=10, s=self.salary);
+    fn dump_header(widths: &Vec<usize>) {
+        println!("{ln:<lw$}{gn:<gw$}{sal:<sw$}", ln="Last", lw=widths[0]+1, gn="First", gw=widths[1]+1, sal="Salary", sw=widths[2]+1);
+        println!("{}", "-".repeat(widths[0]+widths[1]+widths[2]+3));
+    }
+
+    // "Make each column one space longer than the longest
+    // value in the column."
+    fn dump(&self, widths: &Vec<usize>) {
+        println!("{ln:<lw$}{gn:<gw$}{sal:<sw$}", ln=self.l_name, lw=widths[0]+1, gn=self.f_name, gw=widths[1]+1, sal=self.salary, sw=widths[2]+1);
+    }
+
+    fn field_widths(&self) -> Vec<usize> {
+        vec![self.l_name.len(), self.f_name.len(), 5]  // FIXME calculate salary width
     }
 }
 
@@ -39,15 +49,29 @@ fn read_records(filename: &str) -> Result<Vec<Record>, Box<dyn Error>> {
     let contents = fs::read_to_string(filename)?;
     let mut records = Vec::new();
     for line in contents.lines() {
-        let record = Record::new(line)?;
+        let record = Record::parse(line)?;
         records.push(record);
     }
     Ok(records)
 }
 
-fn dump_records(records: &Vec<Record>) {
-    Record::dump_header();
+fn calc_widths(records: &Vec<Record>) -> Vec<usize> {
+    let mut widths = Record::new().unwrap().field_widths();
     for record in records.iter() {
-        record.dump();
+        let r_widths = record.field_widths();
+        for (i, &width) in r_widths.iter().enumerate() {
+            if width > widths[i] {
+                widths[i] = width;
+            }
+        }
+    }
+    widths
+}
+
+fn dump_records(records: &Vec<Record>) {
+    let widths = calc_widths(records);
+    Record::dump_header(&widths);
+    for record in records.iter() {
+        record.dump(&widths);
     }
 }
