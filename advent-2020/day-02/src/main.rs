81bea2c39b55792857ad637647d24645e14ad3c2
Brent J. Nordquist <brent@nordist.net>
2020-12-02 05:59:20 -0600

    RF: Change min/max to first/second

--- advent-2020/day-02/src/main.rs
+++ advent-2020/day-02/src/main.rs
@@ -7,8 +7,8 @@ use std::time::Instant;
 
 #[derive(Debug, Clone, PartialEq)]
 struct Password {
-    min: usize,
-    max: usize,
+    first: usize,
+    second: usize,
     letter: char,
     password: String,
 }
@@ -16,16 +16,16 @@ struct Password {
 impl Password {
     /// Construct by parsing input line.
     ///
-    /// The line is in the form `M-N L: password` where
-    /// - `M` is the minimum count (integer)
-    /// - `N` is the maximum count (integer)
+    /// The line is in the form `F-S L: password` where
+    /// - `F` is the first value (integer)
+    /// - `S` is the second value (integer)
     /// - `L` is the letter (char)
     /// - `password` is the password (string)
     fn from_input_line(line: String) -> Result<Password, io::Error> {
         lazy_static! {
             static ref RE: Regex = Regex::new(r"(?x)^
-                (?P<min>\d+)-
-                (?P<max>\d+)\s+
+                (?P<first>\d+)-
+                (?P<second>\d+)\s+
                 (?P<letter>[a-z]):\s+
                 (?P<password>[a-z]+)
                 $").unwrap();
@@ -37,16 +37,16 @@ impl Password {
                 return Err(io::Error::new(ErrorKind::InvalidInput, e));
             },
         };
-        // FIXME handle ParseIntError for min and max
-        let min = String::from(cap.name("min").unwrap().as_str()).parse::<usize>().unwrap();
-        let max = String::from(cap.name("max").unwrap().as_str()).parse::<usize>().unwrap();
+        // FIXME handle ParseIntError for first and second
+        let first = String::from(cap.name("first").unwrap().as_str()).parse::<usize>().unwrap();
+        let second = String::from(cap.name("second").unwrap().as_str()).parse::<usize>().unwrap();
         let letter = cap.name("letter").unwrap().as_str().chars().next().unwrap();
         let password = String::from(cap.name("password").unwrap().as_str());
-        if min > max {
-            let e = format!("min > max on input line [{}]", line);
+        if first > second {
+            let e = format!("first > second on input line [{}]", line);
             return Err(io::Error::new(ErrorKind::InvalidInput, e));
         }
-        Ok(Password{min, max, letter, password})
+        Ok(Password{first, second, letter, password})
     }
 
     /// Is this password valid according to the given policy?
@@ -55,7 +55,7 @@ impl Password {
             .chars()
             .filter(|c| *c == self.letter)
             .count();
-        matches >= self.min && matches <= self.max
+        matches >= self.first && matches <= self.second
     }
 }
 
@@ -105,13 +105,13 @@ mod tests {
 
     #[test]
     fn test_valid_password() {
-        let password = Password{min: 1, max: 3, letter: 'a', password: String::from("abcde")};
+        let password = Password{first: 1, second: 3, letter: 'a', password: String::from("abcde")};
         assert!(password.is_valid());
     }
 
     #[test]
     fn test_invalid_password() {
-        let password = Password{min: 1, max: 3, letter: 'b', password: String::from("cdefg")};
+        let password = Password{first: 1, second: 3, letter: 'b', password: String::from("cdefg")};
         assert!(!password.is_valid());
     }
 
@@ -119,9 +119,9 @@ mod tests {
     fn test_read_passwords() {
         let passwords = read_passwords("input/example1.txt").unwrap();
         assert_eq!(3, passwords.len());
-        assert_eq!(Password{min: 1, max: 3, letter: 'a', password: String::from("abcde")}, passwords[0]);
-        assert_eq!(Password{min: 1, max: 3, letter: 'b', password: String::from("cdefg")}, passwords[1]);
-        assert_eq!(Password{min: 2, max: 9, letter: 'c', password: String::from("ccccccccc")}, passwords[2]);
+        assert_eq!(Password{first: 1, second: 3, letter: 'a', password: String::from("abcde")}, passwords[0]);
+        assert_eq!(Password{first: 1, second: 3, letter: 'b', password: String::from("cdefg")}, passwords[1]);
+        assert_eq!(Password{first: 2, second: 9, letter: 'c', password: String::from("ccccccccc")}, passwords[2]);
     }
 
     #[test]
