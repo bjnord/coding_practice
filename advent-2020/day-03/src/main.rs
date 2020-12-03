24b172f00539105d0f71bca631a2eea5a314b167
Brent J. Nordquist <brent@nordist.net>
2020-12-03 00:04:41 -0600

    Day 3 part 2, first working solution

--- advent-2020/day-03/src/main.rs
+++ advent-2020/day-03/src/main.rs
@@ -22,6 +22,16 @@ fn part1() {
 
 /// Output solution for part 2.
 fn part2() {
+    let dx = [1, 3, 5, 7, 1];
+    let dy = [1, 1, 1, 1, 2];
+    println!("== PART 2 ==");
+    let mut prod = 1;
+    for i in 0..5 {
+        let trees = trees_on_trajectory("input/input.txt", dy[i], dx[i]);
+        println!("will encounter {} trees on trajectory right={} down={}", trees, dx[i], dy[i]);
+        prod *= trees;
+    }
+    println!("product of trees is {} (should be 7540141059)", prod);
 }
 
 fn trees_on_trajectory(path: &str, dy: usize, dx: usize) -> usize {
@@ -46,6 +56,10 @@ mod tests {
 
     #[test]
     fn test_trees_on_trajectory() {
+        assert_eq!(2, trees_on_trajectory("input/example1.txt", 1, 1));
         assert_eq!(7, trees_on_trajectory("input/example1.txt", 1, 3));
+        assert_eq!(3, trees_on_trajectory("input/example1.txt", 1, 5));
+        assert_eq!(4, trees_on_trajectory("input/example1.txt", 1, 7));
+        assert_eq!(2, trees_on_trajectory("input/example1.txt", 2, 1));
     }
 }
