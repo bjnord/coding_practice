2245a7b9ef90c6f402b76f50ec6d468cf282b31e
Brent J. Nordquist <brent@nordist.net>
2020-12-04 07:10:09 -0600

    RF: Split Passport to lib.rs

---
+++ advent-2020/day-04/src/lib.rs
@@ -0,0 +1,46 @@
+#![warn(clippy::pedantic)]
+
+use std::collections::HashSet;
+use std::error;
+use std::io::{self, ErrorKind};
+
+pub struct Passport {
+    valid: bool,
+}
+
+impl Passport {
+    /// Construct from multi-line string block.
+    pub fn from_str(block: &str) -> Result<Self, Box<dyn error::Error>> {
+        let line = block.replace("\n", " ");
+        let mut names = HashSet::new();
+        for field in line.split(" ") {
+            if let Some(name) = field.split(":").next() {
+                if name != "" {
+                    names.insert(name);
+                }
+            } else {
+                let e = format!("invalid input field format [{}]", field);
+                return Err(Box::new(io::Error::new(ErrorKind::InvalidInput, e)));
+            };
+        }
+        Ok(Self{valid: Passport::is_complete(&names)})
+    }
+
+    /// Is this passport valid?
+    #[must_use]
+    pub fn is_valid(&self) -> bool {
+        self.valid
+    }
+
+    // Is list of passport fields complete?
+    fn is_complete(names: &HashSet<&str>) -> bool {
+        // per puzzle, ignore cid
+        let expects = vec!["ecl", "pid", "eyr", "hcl", "byr", "iyr", "hgt"];
+        for expect in expects {
+            if !names.contains(expect) {
+                return false;
+            }
+        }
+        true
+    }
+}
--- advent-2020/day-04/src/main.rs
+++ advent-2020/day-04/src/main.rs
@@ -1,14 +1,9 @@
 #![warn(clippy::pedantic)]
 
-use std::collections::HashSet;
+use day_04::Passport;
 use std::error;
 use std::fs;
-use std::io::{self, ErrorKind};
 use std::time::Instant;
-
-struct Passport {
-    valid: bool,
-}
 
 fn main() {
     let start = Instant::now();
@@ -21,7 +16,7 @@ fn main() {
 /// Output solution for part 1.
 fn part1() {
     let passports = read_passports("input/input.txt").unwrap();
-    let count = passports.into_iter().filter(|p| p.valid ).count();
+    let count = passports.into_iter().filter(|p| p.is_valid() ).count();
     println!("== PART 1 ==");
     println!("{} passports are valid (should be 206)", count);
 }
@@ -35,33 +30,9 @@ fn read_passports(path: &str) -> Result<Vec<Passport>, Box<dyn error::Error>> {
     let s: String = fs::read_to_string(path).unwrap();
     let mut passports = vec![];
     for block in s.split("\n\n") {
-        let line = block.replace("\n", " ");
-        let mut names = HashSet::new();
-        for field in line.split(" ") {
-            if let Some(name) = field.split(":").next() {
-                if name != "" {
-                    names.insert(name);
-                }
-            } else {
-                let e = format!("invalid input field format [{}]", field);
-                return Err(Box::new(io::Error::new(ErrorKind::InvalidInput, e)));
-            };
-        }
-        passports.push(Passport{valid: is_passport_complete(&names)});
+        passports.push(Passport::from_str(block)?);
     }
     Ok(passports)
-}
-
-/// Is list of passport fields complete?
-fn is_passport_complete(names: &HashSet<&str>) -> bool {
-    // per puzzle, ignore cid
-    let expects = vec!["ecl", "pid", "eyr", "hcl", "byr", "iyr", "hgt"];
-    for expect in expects {
-        if !names.contains(expect) {
-            return false;
-        }
-    }
-    true
 }
 
 #[cfg(test)]
@@ -71,6 +42,6 @@ mod tests {
     #[test]
     fn test_read_passports() {
         let passports = read_passports("input/example1.txt").unwrap();
-        assert_eq!(2, passports.into_iter().filter(|p| p.valid ).count());
+        assert_eq!(2, passports.into_iter().filter(|p| p.is_valid() ).count());
     }
 }
