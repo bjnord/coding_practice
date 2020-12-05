5696ea166c5124ba39fc9c701238ed7f5dd82959
Brent J. Nordquist <brent@nordist.net>
2020-12-05 07:13:31 -0600

    Day 5 part 2, first working solution

--- advent-2020/day-05/src/main.rs
+++ advent-2020/day-05/src/main.rs
@@ -6,6 +6,7 @@ use std::time::Instant;
 fn main() {
     let start = Instant::now();
     part1();
+    part2();
     let duration = start.elapsed();
     eprintln!("Finished after {:?}", duration);
 }
@@ -18,8 +19,35 @@ fn part1() {
     println!("highest seat ID is {} (should be 848)", max_seat);
 }
 
+/// Find the highest seat ID on any boarding pass.
 fn max_seat(passes: Vec<BoardingPass>) -> Option<usize> {
     passes.iter().map(BoardingPass::seat).max()
+}
+
+/// Output solution for part 2.
+fn part2() {
+    let passes = BoardingPass::read_from_file("input/input.txt").unwrap();
+    let empty_seat = empty_seat(passes).unwrap();
+    println!("== PART 2 ==");
+    println!("empty seat ID is {} (should be 682)", empty_seat);
+}
+
+/// Find the seat ID with no boarding pass.
+fn empty_seat(mut passes: Vec<BoardingPass>) -> Option<usize> {
+    passes.sort_by(|a, b| a.seat().cmp(&b.seat()));
+    let mut i = passes.iter();
+    let mut last_seat = i.next().unwrap().seat();
+    for pass in i {
+        let seat = pass.seat();
+        if seat <= last_seat {
+            panic!("sort failure");
+        }
+        if seat > last_seat + 1 {
+            return Some(last_seat + 1);
+        }
+        last_seat = seat;
+    }
+    None
 }
 
 #[cfg(test)]
@@ -36,5 +64,16 @@ mod tests {
     fn test_max_seat_empty_list() {
         let passes: Vec<BoardingPass> = vec![];
         assert_eq!(None, max_seat(passes));
+    }
+
+    #[test]
+    fn test_empty_seat() {
+        // this file (not in order) has:
+        // - row 0: empty
+        // - row 1: full
+        // - row 2: one empty seat in column 3
+        // - row 3: full
+        let passes = BoardingPass::read_from_file("input/example2.txt").unwrap();
+        assert_eq!(Some(2 * 8 + 3), empty_seat(passes));
     }
 }
