32d05e2b9ce9addf54fd3d36283d003ba77a1762
Brent J. Nordquist <brent@nordist.net>
2020-12-07 23:45:17 -0600

    Day 8 part 1, first working solution

---
+++ advent-2020/day-08/src/lib.rs
@@ -0,0 +1,109 @@
+#![warn(clippy::pedantic)]
+
+use std::error;
+use std::fmt;
+use std::fs;
+use std::result;
+use std::str::FromStr;
+
+type Result<Program> = result::Result<Program, Box<dyn error::Error>>;
+
+#[derive(Debug, Clone)]
+struct InstructionError(String);
+
+impl fmt::Display for InstructionError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Instruction error: {}", self.0)
+    }
+}
+
+impl error::Error for InstructionError {}
+
+#[derive(Debug)]
+pub struct Instruction {
+    opcode: String,
+    arg: i32,
+}
+
+#[derive(Debug)]
+pub struct Program {
+    instructions: Vec<Instruction>,
+}
+
+impl FromStr for Program {
+    type Err = Box<dyn error::Error>;
+
+    fn from_str(input: &str) -> Result<Self> {
+        let instructions = input
+            .lines()
+            .map(|line| {
+                let f: Vec<&str> = line.split(' ').collect();
+                Instruction {
+                    opcode: String::from(f[0]),
+                    arg: f[1].parse::<i32>().unwrap(),
+                }
+            })
+            .collect();
+        Ok(Program { instructions })
+    }
+}
+
+impl Program {
+    /// Read program from a file.
+    ///
+    /// # Errors
+    ///
+    /// Returns `Err` if the input file cannot be opened.
+    pub fn read_from_file(path: &str) -> Result<Program> {
+        let s: String = fs::read_to_string(path)?;
+        s.parse()
+    }
+
+    /// Run program until the same PC is seen again. Returns the accumulator
+    /// value at that point.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use day_08::Program;
+    /// let program = Program::read_from_file("input/example1.txt").unwrap();
+    /// assert_eq!(5, program.run_until_dup());
+    /// ```
+    pub fn run_until_dup(&self) -> i32 {
+        let mut acc: i32 = 0;
+        let mut pc: usize = 0;
+        let mut seen = [false; 1024];
+        while !seen[pc] {
+            let inst = &self.instructions[pc];
+            seen[pc] = true;
+            match &inst.opcode[..] {
+                "acc" => {
+                    acc += inst.arg;
+                    pc += 1;
+                },
+                "jmp" => {
+                    if inst.arg.is_negative() {
+                        pc -= inst.arg.wrapping_abs() as usize;
+                    } else {
+                        pc += inst.arg as usize;
+                    }
+                },
+                _ => {  // including "nop"
+                    pc += 1;
+                },
+            }
+        }
+        acc
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_read_from_file_bad_path() {
+        let result = Program::read_from_file("input/example99.txt");
+        assert!(result.is_err());
+    }
+}
---
+++ advent-2020/day-08/src/main.rs
@@ -0,0 +1,33 @@
+#![warn(clippy::pedantic)]
+
+use day_08::Program;
+use std::time::Instant;
+
+fn main() {
+    part1();
+    part2();
+}
+
+/// Output solution for part 1.
+fn part1() {
+    let start = Instant::now();
+    let program = Program::read_from_file("input/input.txt").unwrap();
+    let gen_time = start.elapsed();
+    let acc = program.run_until_dup();
+    let run_time = start.elapsed() - gen_time;
+    println!("Day 8 - Part 1 : {} <=> 2058 expected", acc);
+    println!("    generator: {:?}", gen_time);
+    println!("    runner: {:?}", run_time);
+}
+
+/// Output solution for part 2.
+fn part2() {
+    let start = Instant::now();
+    //let entries = Entry::read_from_file("input/input.txt").unwrap();
+    let gen_time = start.elapsed();
+    //...
+    let run_time = start.elapsed() - gen_time;
+    println!("Day 8 - Part 2 : {} <=> _ expected", 0);
+    println!("    generator: {:?}", gen_time);
+    println!("    runner: {:?}", run_time);
+}
