6ea5c73bd44fe052a756ff1e740856b3e430c418
Brent J. Nordquist <brent@nordist.net>
2020-12-11 06:58:35 -0600

    Seat filling and counting

--- advent-2020/day-11/src/lib.rs
+++ advent-2020/day-11/src/lib.rs
@@ -67,6 +67,11 @@ impl SeatLayout {
         self.grid[y * self.width + x]
     }
 
+    /// Return count of occupied seats.
+    pub fn occupied_seats(&self) -> usize {
+        self.grid.iter().filter(|&s| *s == Seat::Occupied).count()
+    }
+
     /// Return count of occupied seats adjacent to (y, x).
     pub fn occupied_seats_at(&self, y: usize, x: usize) -> usize {
         let mut seats: Vec<Seat> = Vec::new();
@@ -83,6 +88,37 @@ impl SeatLayout {
             if x < self.width-1 { seats.push(self.seat_at(y+1, x+1)); }
         }
         seats.iter().filter(|&s| *s == Seat::Occupied).count()
+    }
+
+    /// Do one round of seat filling.
+    // TODO replace this hack string implementation with an enum one
+    pub fn fill_seats(&self) -> SeatLayout {
+        let mut s = String::new();
+        for y in 0..self.height {
+            for x in 0..self.width {
+                match self.seat_at(y, x) {
+                    Seat::Floor => {
+                        s += ".";
+                    },
+                    Seat::Empty => {
+                        if self.occupied_seats_at(y, x) == 0 {
+                            s += "#";
+                        } else {
+                            s += "L";
+                        }
+                    },
+                    Seat::Occupied => {
+                        if self.occupied_seats_at(y, x) >= 4 {
+                            s += "L";
+                        } else {
+                            s += "#";
+                        }
+                    }
+                }
+            }
+            s += "\n";
+        }
+        s.parse().unwrap()
     }
 }
 
@@ -124,6 +160,16 @@ mod tests {
     }
 
     #[test]
+    fn test_occupied_seats() {
+        let layout = SeatLayout::read_from_file("input/example1.txt")
+            .unwrap();
+        assert_eq!(0, layout.occupied_seats());
+        let layout3 = SeatLayout::read_from_file("input/example3.txt")
+            .unwrap();
+        assert_eq!(20, layout3.occupied_seats());
+    }
+
+    #[test]
     fn test_occupied_seats_at() {
         let layout = SeatLayout::read_from_file("input/example1s.txt")
             .unwrap();
@@ -132,6 +178,25 @@ mod tests {
         assert_eq!(4, layout.occupied_seats_at(2, 1));
         assert_eq!(0, layout.occupied_seats_at(9, 6));
         assert_eq!(1, layout.occupied_seats_at(9, 9));
+    }
+
+    #[test]
+    fn test_fill_seats() {
+        let layout = SeatLayout::read_from_file("input/example1.txt")
+            .unwrap();
+        let layout2 = layout.fill_seats();
+        assert_eq!(71, layout2.occupied_seats());
+        let layout3 = layout2.fill_seats();
+        assert_eq!(20, layout3.occupied_seats());
+    }
+
+    #[test]
+    fn test_fill_seats_stable() {
+        let layout = SeatLayout::read_from_file("input/example1s.txt")
+            .unwrap();
+        let before_seats = layout.occupied_seats();
+        let next_layout = layout.fill_seats();
+        assert_eq!(before_seats, next_layout.occupied_seats());
     }
 
     #[test]
