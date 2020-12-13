7929d8bb4b8ae1a893d67a24f2ae141042aaa26a
Brent J. Nordquist <brent@nordist.net>
2020-12-12 23:21:03 -0600

    Day 13: Parser

---
+++ advent-2020/day-13/src/lib.rs
@@ -0,0 +1,88 @@
+use std::error;
+use std::fmt;
+use std::fs;
+use std::result;
+use std::str::FromStr;
+
+type Result<BusSchedule> = result::Result<BusSchedule, Box<dyn error::Error>>;
+
+#[derive(Debug, Clone)]
+struct BusScheduleError(String);
+
+impl fmt::Display for BusScheduleError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "BusSchedule error: {}", self.0)
+    }
+}
+
+impl error::Error for BusScheduleError {}
+
+#[derive(Debug, Clone)]
+pub struct BusSchedule {
+    earliest_depart: u32,
+    busses: Vec<u32>,
+}
+
+impl FromStr for BusSchedule {
+    type Err = Box<dyn error::Error>;
+
+    fn from_str(block: &str) -> Result<Self> {
+        let fields: Vec<&str> = block.trim().split("\n").take(2).collect();
+        let earliest_depart: u32 = fields[0].parse()?;
+        let busses: Vec<u32> = fields[1]
+            .split(',')
+            .filter(|&id| id != "x")
+            .map(|id| id.parse().unwrap())
+            .collect();
+        Ok(Self { earliest_depart, busses })
+    }
+}
+
+impl BusSchedule {
+    /// Return earliest departure time.
+    #[must_use]
+    pub fn earliest_depart(&self) -> u32 {
+        self.earliest_depart
+    }
+
+    /// Return list of busses.
+    #[must_use]
+    pub fn busses(&self) -> Vec<u32> {
+        self.busses.clone()
+    }
+
+    /// Read bus schedule from file.
+    ///
+    /// # Errors
+    ///
+    /// Returns `Err` if the input file cannot be opened, or if
+    /// an input line has an invalid format.
+    pub fn read_from_file(path: &str) -> Result<BusSchedule> {
+        let s: String = fs::read_to_string(path)?;
+        s.parse()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_read_from_file() {
+        let schedule = BusSchedule::read_from_file("input/example1.txt").unwrap();
+        assert_eq!(939, schedule.earliest_depart());
+        assert_eq!(vec![7, 13, 59, 31, 19], schedule.busses());
+    }
+
+    #[test]
+    fn test_read_from_file_no_file() {
+        let schedule = BusSchedule::read_from_file("input/example99.txt");
+        assert!(schedule.is_err());
+    }
+
+    #[test]
+    fn test_read_from_file_bad_format() {
+        let schedule = BusSchedule::read_from_file("input/bad1.txt");
+        assert!(schedule.is_err());
+    }
+}
