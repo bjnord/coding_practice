346e6ede70468a2d72d6d7664f72e82bca340aa4
Brent J. Nordquist <brent@nordist.net>
2020-12-12 20:32:54 -0600

    Simple iterated rounds display

--- advent-2020/day-11/src/main.rs
+++ advent-2020/day-11/src/main.rs
@@ -1,9 +1,19 @@
 use day_11::{FillRules, SeatLayout};
+use std::env;
 use std::time::Instant;
 
 fn main() {
-    part1();
-    part2();
+    let argv: Vec<String> = env::args().collect();
+    if argv.len() < 3 {
+        part1();
+        part2();
+    } else {
+        match &argv[1][..] {
+            "-s" | "--stringent" => dump_rounds(&argv[2], FillRules::Stringent),
+            "-t" | "--tolerant" => dump_rounds(&argv[2], FillRules::Tolerant),
+            _ => panic!("invalid arguments: {:?}", argv),
+        }
+    }
 }
 
 /// Output solution for part 1.
@@ -28,4 +38,12 @@ fn part2() {
     println!("Day 11 - Part 2 : {} <=> 2002 expected", stable_layout.occupied_seats());
     println!("    generator: {:?}", gen_time);
     println!("    runner: {:?}", run_time);
+}
+
+fn dump_rounds(path: &str, rules: FillRules) {
+    let layout = SeatLayout::read_from_file(path).unwrap();
+    println!("Initial State - Occupied {}\n{}", layout.occupied_seats(), layout);
+    for (i, round) in layout.iter(rules).enumerate() {
+        println!("Round {} - Occupied {}\n{}", i+1, round.occupied_seats(), round);
+    }
 }
