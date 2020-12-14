8b7ccfda9a12b08aa8802854de16765d5722fd58
Brent J. Nordquist <brent@nordist.net>
2020-12-13 19:20:09 -0600

    Day 13 part 2, first working solution

--- advent-2020/day-13/src/main.rs
+++ advent-2020/day-13/src/main.rs
@@ -1,9 +1,8 @@
-use day_13::{BusSchedule, egcd, something};
+use day_13::BusSchedule;
 use std::time::Instant;
 
 fn main() {
     part1();
-    solve_via_egcd();
     part2();
 }
 
@@ -29,85 +28,4 @@ fn part2() {
     println!("Day 1 - Part 2 : {} <=> _ expected", answer);
     println!("    generator: {:?}", gen_time);
     println!("    runner: {:?}", run_time);
-}
-
-/// Example from [this page at the Brilliant wiki](https://brilliant.org/wiki/chinese-remainder-theorem/)
-/// solved by hand
-#[allow(dead_code)]
-fn solve_brilliant() {
-    // 6 (mod 7)  ->  x = 7j + 6
-    let mut this_modu: u64;
-    let mut this_rem: u64;
-    let mut next_modu: u64 = 7;
-    let mut next_rem: u64 = 6;
-    let mut next_mult: u64;
-    let mut next_add: u64;
-
-    next_mult = next_modu;
-    next_add = next_rem;
-
-    this_modu = next_modu;
-    this_rem = next_rem;
-    next_modu = 5;
-    next_rem = 4;
-    let mut sol_rem = BusSchedule::solve_congruence(next_mult, next_add, next_rem, next_modu);
-    println!("brill 1+2  j={} mod={}", sol_rem, next_modu);
-    // 4 (mod 5)  ->  j = 5k + 4
-    //                x = 7(5k + 4) + 6
-    //                x = 35k + 34
-    let tuple = BusSchedule::substitute_congruence(next_modu, sol_rem, next_mult, next_add);
-    next_mult = tuple.0;
-    next_add = tuple.1;
-    println!("           this=(m{}, r{}) next=(m{}, a{})", this_modu, this_rem, next_mult, next_add);
-
-    this_modu = next_modu;
-    this_rem = next_rem;
-    next_modu = 3;
-    next_rem = 1;
-    sol_rem = BusSchedule::solve_congruence(next_mult, next_add, next_rem, next_modu);
-    println!("brill 3    k={} mod={}", sol_rem, next_modu);
-    // 0 (mod 3)  ->  k = 3l + 0
-    //                x = 35(3l + 0) + 34
-    //                x = 105l + 34
-    let tuple = BusSchedule::substitute_congruence(next_modu, sol_rem, next_mult, next_add);
-    next_mult = tuple.0;
-    next_add = tuple.1;
-    println!("           this=(m{}, r{}) next=(m{}, a{})", this_modu, this_rem, next_mult, next_add);
-
-    // solution: x == 34 (mod 105)
-    println!("solution   m{} - r{} = {}", next_mult, next_add, next_mult - next_add);
-}
-
-/// Example from [this page at the Brilliant wiki](https://brilliant.org/wiki/chinese-remainder-theorem/)
-/// but solved with our class
-#[allow(dead_code)]
-fn solve_brilliant_2() {
-    let schedule: BusSchedule = "0\nx,3,x,x,5,x,7\n".parse().unwrap();
-    let answer = schedule.earliest_staggered_time();
-    println!("solution   {}", answer);
-}
-
-fn solve_via_egcd() {
-    let mut tuple = egcd(1432, 123211);
-    println!("x={} y={} gcd={}", tuple.1, tuple.2, tuple.0);
-    println!("expect x=-22973 y=267");
-    println!();
-
-    tuple = egcd(7, 6);
-    let mut some = something(tuple.1, 5);
-    println!("x={} y={} gcd={} some={}", tuple.1, tuple.2, tuple.0, some);
-    println!("expect gcd=1 some={}", BusSchedule::solve_congruence(7, 6, 4, 5));
-    println!();
-
-    tuple = egcd(35, 34);
-    some = something(tuple.1, 1);
-    println!("x={} y={} gcd={} some={}", tuple.1, tuple.2, tuple.0, some);
-    println!("expect gcd=1 some={}", BusSchedule::solve_congruence(35, 34, 1, 3));
-    println!();
-
-    tuple = egcd(105, 34);
-    some = something(tuple.1, 1);
-    println!("x={} y={} gcd={} some={}", tuple.1, tuple.2, tuple.0, some);
-    println!("expect gcd=1 some={}", BusSchedule::solve_congruence(105, 34, 1, 3));
-    println!();
 }
