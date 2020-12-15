336f1ddb8939f4fb5a9fd335f6442d46065c387b
Brent J. Nordquist <brent@nordist.net>
2020-12-15 07:22:22 -0600

    Day 15 part 2, first working solution

--- advent-2020/day-15/src/main.rs
+++ advent-2020/day-15/src/main.rs
@@ -26,11 +26,15 @@ fn part1() {
 /// Output solution for part 2.
 fn part2() {
     let start = Instant::now();
-    //let entries = Entry::read_from_file("input/input.txt").unwrap();
+    let numbers: Vec<u32> = fs::read_to_string("input/input.txt").unwrap()
+        .trim()
+        .split(',')
+        .map(|n| n.parse().unwrap())
+        .collect();
     let gen_time = start.elapsed();
-    //...
+    let solution = Game::play(&numbers, 30000000);
     let run_time = start.elapsed() - gen_time;
-    println!("Day 1 - Part 2 : {} <=> _ expected", 0);
+    println!("Day 1 - Part 2 : {} <=> 62714 expected", solution);
     println!("    generator: {:?}", gen_time);
     println!("    runner: {:?}", run_time);
 }
