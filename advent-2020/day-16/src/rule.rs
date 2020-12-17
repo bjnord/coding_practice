143f509b37109c9e080e7f76efe097df55ce7a44
Brent J. Nordquist <brent@nordist.net>
2020-12-16 20:21:24 -0600

    RF: Turn parse_range() into a FromStr

--- advent-2020/day-16/src/rule.rs
+++ advent-2020/day-16/src/rule.rs
@@ -22,6 +22,27 @@ pub struct RuleRange {
     pub end: u32,
 }
 
+impl FromStr for RuleRange {
+    type Err = Box<dyn error::Error>;
+
+    fn from_str(range: &str) -> Result<Self> {
+        let range_bounds: Vec<u32> = range.split('-')
+            .map(|ns| ns.parse::<u32>().unwrap())
+            .collect();
+        if range_bounds.len() != 2 {
+            let e = format!("invalid range [{}]", range);
+            return Err(RuleRangeError(e).into());
+        }
+        match range_bounds.iter().next_tuple() {
+            Some((&start, &end)) => Ok(RuleRange { start, end }),
+            _ => {
+                let e = format!("invalid range [{}]", range);
+                return Err(RuleRangeError(e).into());
+            },
+        }
+    }
+}
+
 #[derive(Debug, Clone)]
 struct RuleError(String);
 
@@ -51,7 +72,7 @@ impl FromStr for Rule {
         let name = String::from(tokens[0]);
         let value = tokens[1];
         let ranges_result: Result<Vec<RuleRange>> = value.split(" or ")
-            .map(Rule::parse_range)
+            .map(str::parse)
             .collect();
         match ranges_result {
             Err(e) => Err(e),
@@ -71,24 +92,6 @@ impl Rule {
     #[must_use]
     pub fn ranges(&self) -> Vec<RuleRange> {
         self.ranges.clone()
-    }
-
-    // FIXME should be a FromStr on RuleRange
-    pub fn parse_range(range: &str) -> Result<RuleRange> {
-        let range_bounds: Vec<u32> = range.split('-')
-            .map(|ns| ns.parse::<u32>().unwrap())
-            .collect();
-        if range_bounds.len() != 2 {
-            let e = format!("invalid range [{}]", range);
-            return Err(RuleRangeError(e).into());
-        }
-        match range_bounds.iter().next_tuple() {
-            Some((&start, &end)) => Ok(RuleRange { start, end }),
-            _ => {
-                let e = format!("invalid range [{}]", range);
-                return Err(RuleRangeError(e).into());
-            },
-        }
     }
 
     /// Read rules from `input` (list of lines).
@@ -135,14 +138,14 @@ mod tests {
     }
 
     #[test]
-    fn test_parse_range() {
-        assert_eq!(RuleRange { start: 1, end: 3 }, Rule::parse_range("1-3").unwrap());
-        assert_eq!(RuleRange { start: 5, end: 7 }, Rule::parse_range("5-7").unwrap());
+    fn test_parse_rule_range() {
+        assert_eq!(RuleRange { start: 1, end: 3 }, "1-3".parse::<RuleRange>().unwrap());
+        assert_eq!(RuleRange { start: 5, end: 7 }, "5-7".parse::<RuleRange>().unwrap());
     }
 
     #[test]
-    fn test_parse_range_bad() {
-        assert!(Rule::parse_range("1").is_err());
-        assert!(Rule::parse_range("1-3-5").is_err());
+    fn test_parse_rule_range_bad() {
+        assert!("1".parse::<RuleRange>().is_err());
+        assert!("1-3-5".parse::<RuleRange>().is_err());
     }
 }
