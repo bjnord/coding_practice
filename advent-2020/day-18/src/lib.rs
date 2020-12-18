0c6733dc822add37421cc0e480bc2c2bbc2c084d
Brent J. Nordquist <brent@nordist.net>
2020-12-18 07:17:59 -0600

    Day 18: Simple solver (no parentheses)

--- advent-2020/day-18/src/lib.rs
+++ advent-2020/day-18/src/lib.rs
@@ -27,10 +27,10 @@ impl FromStr for Term {
 
     fn from_str(token: &str) -> Result<Self> {
         let ch = token.chars().next().ok_or("empty term")?;
-        let term = match ch {
-            '+' => Term::Operator('+'),
-            '*' => Term::Operator('*'),
-            _ => Term::Number(token.parse::<i32>()?),
+        let term = if ch.is_digit(10) {
+            Term::Number(token.parse::<i32>()?)
+        } else {
+            Term::Operator(ch)
         };
         Ok(term)
     }
@@ -96,6 +96,71 @@ impl Equation {
         let s: String = fs::read_to_string(path)?;
         s.lines().map(str::parse).collect()
     }
+
+    /// Solve equation.
+    ///
+    /// # Errors
+    ///
+    /// Returns `Err` if the equation terms do not form a valid equation.
+    pub fn solve(&self) -> Result<i32> {
+        let mut buf_n = None;
+        let mut buf_op = None;
+        for term in &self.terms {
+            match term {
+                Term::Number(n) => {
+                    buf_n = match (buf_n, buf_op) {
+                        (Some(Term::Number(n0)), Some(Term::Operator(op))) => {
+                            buf_op = None;
+                            match op {
+                                '+' => Some(Term::Number(n + n0)),
+                                '*' => Some(Term::Number(n * n0)),
+                                _ => {
+                                    let e = format!("invalid operator '{}'", op);
+                                    return Err(EquationError(e).into());
+                                },
+                            }
+                        },
+                        (Some(Term::Number(_)), None) => {
+                            let e = format!("missing operator before '{}'", n);
+                            return Err(EquationError(e).into());
+                        },
+                        (None, Some(Term::Operator(op))) => {
+                            let e = format!("missing first number '{} {}'", op, n);
+                            return Err(EquationError(e).into());
+                        },
+                        (None, None) => Some(Term::Number(*n)),
+                        // e.g. operator stored in buf_n, number stored in buf_op:
+                        (_, _) => panic!("buffer implementation error 1"),
+                    }
+                },
+                Term::Operator(op) => {
+                    buf_op = match buf_op {
+                        Some(op0) => {
+                            let e = format!("double operator '{} {}'", op0, op);
+                            return Err(EquationError(e).into());
+                        },
+                        None => Some(Term::Operator(*op)),
+                    }
+                },
+            }
+        }
+        match buf_op {
+            Some(Term::Operator(op0)) => {
+                let e = format!("trailing operator '{}'", op0);
+                Err(EquationError(e).into())
+            },
+            None => {
+                match buf_n {
+                    Some(Term::Number(n)) => Ok(n),
+                    None => panic!("no final n"),
+                    // e.g. operator stored in buf_n:
+                    _ => panic!("buffer implementation error 2"),
+                }
+            },
+            // e.g. number stored in buf_op:
+            _ => panic!("buffer implementation error 3"),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -117,14 +182,51 @@ mod tests {
     }
 
     #[test]
+    fn test_solve_equations_simple() {
+        let equations = Equation::read_from_file("input/example1.txt").unwrap();
+        assert_eq!(1, equations.len());
+        assert_eq!(71, equations[0].solve().unwrap());
+    }
+
+    #[test]
     fn test_parse_equation_bad_term() {
-        let result = "1 + X".parse::<Equation>();
+        let result = "1 + 2x".parse::<Equation>();
         assert!(result.is_err());
     }
 
     #[test]
     fn test_parse_equation_double_space() {
         let result = "1  + 2".parse::<Equation>();
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_solve_equation_invalid_operator() {
+        let result = "1 + 2 / 3".parse::<Equation>().unwrap().solve();
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_solve_equation_missing_operator() {
+        let result = "1 + 2 3".parse::<Equation>().unwrap().solve();
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_solve_equation_double_operator() {
+        let result = "1 * + 2".parse::<Equation>().unwrap().solve();
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_solve_equation_missing_first_number() {
+        let result = "+ 2".parse::<Equation>().unwrap().solve();
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_solve_equation_trailing_operator() {
+        let result = "1 *".parse::<Equation>().unwrap().solve();
         assert!(result.is_err());
     }
 }
