410ed90591e8bbf6b3619abce41f09f90750fa8f
Brent J. Nordquist <brent@nordist.net>
2020-12-20 17:02:10 -0600

    Day 20: Dead end
    About halfway through creating this utter monstrosity, better approaches
    finally dawned on me... but I kept stubbornly proceeding, hoping I could
    get a right answer and then reset. Lesson learned: I should have
    abandoned this trail a lot sooner.

--- advent-2020/day-20/src/main.rs
+++ advent-2020/day-20/src/main.rs
@@ -9,15 +9,11 @@ fn main() {
 /// Output solution for part 1.
 fn part1() {
     let start = Instant::now();
-    let tiles = Tile::read_from_file("input/example1.txt").unwrap();  // FIXME input.txt
-    println!("{}", tiles[7]);
-    for border in tiles[7].borders() {
-        println!("{}", border);
-    }
+    let tiles = Tile::read_from_file("input/input.txt").unwrap();
     let gen_time = start.elapsed();
-    //...
+    let answer = Tile::solve(&tiles);
     let run_time = start.elapsed() - gen_time;
-    println!("Day 1 - Part 1 : {} <=> _ expected", 0);
+    println!("Day 1 - Part 1 : {} <=> _ expected (31436724700061 is TOO HIGH)", answer);
     println!("    generator: {:?}", gen_time);
     println!("    runner: {:?}", run_time);
 }
