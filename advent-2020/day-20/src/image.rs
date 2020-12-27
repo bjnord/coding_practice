ae1f2e0002e0d42bcb70f91812fc5d7bb939decd
Brent J. Nordquist <brent@nordist.net>
2020-12-27 04:30:02 -0600

    Day 20 part 2: Image pattern parser and display

--- advent-2020/day-20/src/image.rs
+++ advent-2020/day-20/src/image.rs
@@ -2,6 +2,7 @@ use custom_error::custom_error;
 use crate::{Pixel, Tile};
 use std::collections::HashMap;
 use std::fmt;
+use std::str::FromStr;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
@@ -9,6 +10,8 @@ custom_error!{#[derive(PartialEq)]
     pub ImageError
     TilesNotFound = "aligned tiles not found",
     TilesNotSquare = "aligned tiles do not form a square",
+    GridNotFound = "image pattern grid not found",
+    InvalidPixel{pixel: char} = "invalid pixel character '{pixel}'",
 }
 
 #[derive(Debug, Clone, Eq, PartialEq)]
@@ -96,6 +99,75 @@ impl Image {
     }
 }
 
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+pub struct PixelPattern(bool);
+
+impl PixelPattern {
+    /// Construct pixel from input character.
+    #[must_use]
+    pub fn from_char(pixel: char) -> Result<PixelPattern> {
+        match pixel {
+            '_' => Ok(PixelPattern(false)),
+            'O' => Ok(PixelPattern(true)),
+            _ => Err(ImageError::InvalidPixel { pixel }.into())
+        }
+    }
+}
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct ImagePattern {
+    height: usize,
+    width: usize,
+    pixels: Vec<PixelPattern>,
+}
+
+impl fmt::Display for ImagePattern {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut s = String::new();
+        for y in 0..self.height {
+            for x in 0..self.width {
+                s += match self.pixel_at(y, x) {
+                    false => "_",
+                    true => "O",
+                };
+            }
+            s += "\n";
+        }
+        write!(f, "{}", s)
+    }
+}
+
+impl FromStr for ImagePattern {
+    type Err = Box<dyn std::error::Error>;
+
+    fn from_str(input: &str) -> Result<Self> {
+        let width = input
+            .lines()
+            .next()
+            .ok_or_else(|| ImageError::GridNotFound)?
+            .trim()
+            .len();
+        let pixels: Vec<PixelPattern> = input
+            .lines()
+            .flat_map(|line| line.trim().chars().map(PixelPattern::from_char))
+            .collect::<Result<Vec<PixelPattern>>>()?;
+        let height = pixels.len() / width;
+        Ok(Self { height, width, pixels })
+    }
+}
+
+impl ImagePattern {
+    /// Return pixel at (y, x).
+    #[must_use]
+    pub fn pixel_at(&self, y: usize, x: usize) -> bool {
+        self.pixels[self.pixels_index(y, x)].0
+    }
+
+    fn pixels_index(&self, y: usize, x: usize) -> usize {
+        y * self.width + x
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -106,5 +178,33 @@ mod tests {
         let image = Image::from_tiles(&tiles).unwrap();
         assert_eq!(24, image.edge);
         assert_eq!(24 * 24, image.pixels.len());
+    }
+
+    #[test]
+    fn test_parse_image_pattern() {
+        let pattern: ImagePattern = "__O_\n_OOO\nO___\n".parse().unwrap();
+        assert_eq!(4, pattern.width);
+        assert_eq!(3, pattern.height);
+        assert_eq!(false, pattern.pixel_at(0, 0));
+        assert_eq!(false, pattern.pixel_at(2, 3));
+        assert_eq!(true, pattern.pixel_at(0, 2));
+        assert_eq!(true, pattern.pixel_at(1, 3));
+        assert_eq!(true, pattern.pixel_at(2, 0));
+    }
+
+    #[test]
+    fn test_parse_image_pattern_none() {
+        match "".parse::<ImagePattern>() {
+            Err(e) => assert!(e.to_string().contains("image pattern grid not found")),
+            Ok(_)  => panic!("test did not fail"),
+        }
+    }
+
+    #[test]
+    fn test_parse_image_pattern_invalid_pixel() {
+        match "O_O\n_X_\n__O\n".parse::<ImagePattern>() {
+            Err(e) => assert!(e.to_string().contains("invalid pixel character 'X'")),
+            Ok(_)  => panic!("test did not fail"),
+        }
     }
 }
