d24e07e5a6e5e2cc26dbce5361f8ffc2fd972bc3
Brent J. Nordquist <brent@nordist.net>
2020-12-29 08:40:50 -0600

    Figured out how the program works (and documented)

--- advent-2018/day-19-21/machine/src/machine-optimized.c
+++ advent-2018/day-19-21/machine/src/machine-optimized.c
@@ -5,9 +5,17 @@ void main(argc, argv) {
 unsigned int r[NREG], di;
 for (di = 0; di < NREG; di++) r[di] = 0;
 
+	if (argc > 1) {
+		r[0] = 1;  /* part 2 */
+	}
 
 /* DECOMPILED PROGRAM: (r[2] bound to IP) */
 	goto label17;
+
+/*
+ * This is the program; it accumulates into R0 the sum of all prime factors
+ * of R5 (including 1 and R5).
+ */
 label1:
 	r[1] = 0x000001;
 label2:
@@ -18,6 +26,7 @@ label3:
 	if (r[4] == 1) goto label7;
 	goto label8;
 label7:
+	printf("prime factor R1 = %d (x%X)\n", r[1], r[1]);
 	r[0] = r[1] + r[0];
 label8:
 	r[3] = r[3] + 0x000001;
@@ -29,8 +38,19 @@ label12:
 	r[4] = (r[1] > r[5]) ? 1 : 0;
 	if (r[4] == 1) goto label16;
 	goto label2;
+
+/*
+ * This is the exit; it jumps to address 256 (beyond the end) which halts.
+ */
 label16:
 	r[2] = 16; r[2] = r[2] * r[2]; goto skipper; /* JUMP to r[2] * r[2] + 1 */
+
+/*
+ * These are the initializers.
+ * - part 1 (R0=0) will only do label17 yielding R5=955
+ * - part 2 (R0=1) will do both label17 and label27 yielding R5=10551355
+ * - either way, sets R0=0 and jumps back to label1 to start calculating
+ */
 label17:
 	r[5] = r[5] + 0x000002;
 	r[5] = r[5] * r[5];
@@ -42,7 +62,9 @@ label17:
 	r[5] = r[5] + r[4];
 	r[2] = 25; r[2] = r[2] + r[0]; goto skipper; /* JUMP to r[2] + r[0] + 1 */
 label26:
+	printf("initialized R5 to %d (x%X)\n", r[5], r[5]);
 	goto label1;
+label27:
 	r[2] = 27; r[4] = r[2]; /* read r[2] */
 	r[2] = 28; r[4] = r[4] * r[2]; /* read r[2] */
 	r[2] = 29; r[4] = r[2] + r[4]; /* read r[2] */
@@ -50,14 +72,18 @@ label26:
 	r[4] = r[4] * 0x00000E;
 	r[2] = 32; r[4] = r[4] * r[2]; /* read r[2] */
 	r[5] = r[5] + r[4];
+	printf("initialized R5 to %d (x%X)\n", r[5], r[5]);
 	r[0] = 0x000000;
 	goto label1;
+
 label36:
 	r[2] = 36; goto end; /* HALT (set bound reg) */
 
 skipper:
 	if (r[2] == 25) {
 		goto label26;
+	} else if (r[2] == 26) {
+		goto label27;
 	} else if (r[2] > 36) {
 		goto end;
 	}
