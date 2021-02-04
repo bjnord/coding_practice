1e12bf15d546a4a0622b701f9f0719d52ba8b390
Brent J. Nordquist <brent@nordist.net>
2021-02-03 21:58:38 -0600

    32-bit isqrt

--- euler-rust/src/math.rs
+++ euler-rust/src/math.rs
@@ -17,6 +17,14 @@ impl Math {
         // sqrt of 52-bit number will fit in u32
         (n as f64).sqrt() as u32
     }
+
+    // h/t <https://users.rust-lang.org/t/integer-square-root/96>
+    #[allow(clippy::cast_possible_truncation)]
+    #[allow(clippy::cast_precision_loss)]
+    #[allow(clippy::cast_sign_loss)]
+    pub fn isqrt(n: u32) -> u32 {
+        (n as f64).sqrt() as u32
+    }
 }
 
 #[cfg(test)]
@@ -33,5 +41,11 @@ mod tests {
     #[should_panic]
     fn test_big_n_isqrt_64() {
         Math::isqrt_64(MAX_52_BIT + 1);
+    }
+
+    #[test]
+    fn test_max_n_isqrt() {
+        let ns = Math::isqrt(std::u32::MAX);
+        assert_eq!(65535, ns);
     }
 }
