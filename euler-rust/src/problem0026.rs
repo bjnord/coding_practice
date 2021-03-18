6e3b8f05024db85deae9aae574874cc4ec3f6a91
Brent J. Nordquist <brent@nordist.net>
2021-03-18 18:48:56 -0500

    RF: Clean up using iterator chain

--- euler-rust/src/problem0026.rs
+++ euler-rust/src/problem0026.rs
@@ -3,40 +3,35 @@
 use crate::math::Math;
 use rug::Float;
 
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+struct RepeatLen {
+    len: usize,
+    d: u32,
+}
+
 pub struct Problem0026 { }
 
 impl Problem0026 {
     /// Find the value of d < `n` for which 1/d contains the longest
-    /// recurring cycle in its decimal fraction part.
+    /// recurring cycle in its decimal fraction part, using the given
+    /// precision `prec` (bits).
     #[must_use]
-    pub fn solve(n: u32) -> u32 {
-        let mut max_cycle: usize = 0;
-        let mut max_d: u32 = 0;
-        // for faster `cargo test` only use big-precision for real problem
-        let prec: u32 = match n > 11 {
-            true => 8192,
-            false => 128,
-        };
-        for d in 2..n {
-            let num = Float::with_val(prec, 1.0);
-            let denom = Float::with_val(prec, d);
-            let f = num / denom;
-            let (_sign, _nonrep, rep, _exp) = Math::decimal_cycle(&f);
-            //eprintln!("for d={} rep='{}' (len={})", d, rep, rep.len());
-            if rep.len() <= max_cycle {
-                //eprintln!("  ...shorter than previous max_cycle={}", max_cycle);
-                continue;
-            }
-            //eprintln!("  ...longer than previous max_cycle={}", max_cycle);
-            max_cycle = rep.len();
-            max_d = d;
-        }
-        max_d
+    pub fn solve(n: u32, prec: u32) -> u32 {
+        (2..n).map(|d| Self::find_replen(d, prec)).max().unwrap().d
+    }
+
+    // Find length of the repeating portion of `1/d` at the given precision.
+    fn find_replen(d: u32, prec: u32) -> RepeatLen {
+        let num = Float::with_val(prec, 1.0);
+        let denom = Float::with_val(prec, d);
+        let f = num / denom;
+        let (_sign, _nonrep, rep, _exp) = Math::decimal_cycle(&f);
+        RepeatLen { len: rep.len(), d }
     }
 
     #[must_use]
     pub fn output() -> String {
-        format!("Problem 26 answer is {}", Self::solve(1_000))
+        format!("Problem 26 answer is {}", Self::solve(1_000, 8192))
     }
 }
 
@@ -46,7 +41,7 @@ mod tests {
 
     #[test]
     fn test_solve_example() {
-        let answer = Problem0026::solve(11);
+        let answer = Problem0026::solve(11, 128);
         assert_eq!(7, answer);
     }
 
@@ -54,7 +49,7 @@ mod tests {
     #[ignore]
     fn test_solve_problem() {
         // FIXME OPTIMIZE takes over a minute to run
-        //let answer = Problem0026::solve(1_000);
+        //let answer = Problem0026::solve(1_000, 8192);
         //assert_eq!(983, answer);
         assert_eq!("too", "slow");
     }
