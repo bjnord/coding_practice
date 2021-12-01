7716d659d85dd707c3ddd9cdcbda917a10f89c2e
Brent J. Nordquist <brent@nordist.net>
2021-11-30 23:54:09 -0600

    Day 1 part 2, first working solution

--- advent-2021/day-01/depth/lib/depth.ex
+++ advent-2021/day-01/depth/lib/depth.ex
@@ -22,4 +22,32 @@ defmodule Depth do
     n = if middle > head, do: n + 1, else: n
     count_increases(n, tail)
   end
+
+  @doc """
+  Solve part 2.
+  """
+  def part2([filename]) do
+    depths = filename
+    |> File.read!
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.to_integer/1)
+    count_sliding_increases(depths)
+    |> IO.inspect(label: "Part 2 number of sliding-window increases is")
+  end
+
+  def count_sliding_increases(list) do
+    [a, b, c | tail] = list
+    count_sliding_increases(0, [a, b, c], tail)
+  end
+  def count_sliding_increases(n, _window, []), do: n
+  def count_sliding_increases(n, window, tail) do
+    a = Enum.sum(window)
+    # shift the window
+    [_ | keep] = window
+    [next | new_tail] = tail
+    new_window = keep ++ [next]
+    b = Enum.sum(new_window)
+    n = if b > a, do: n + 1, else: n
+    count_sliding_increases(n, new_window, new_tail)
+  end
 end
