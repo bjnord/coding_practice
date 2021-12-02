ee315be4bd1601b9a9f2947c6ed925be53243585
Brent J. Nordquist <brent@nordist.net>
2021-12-01 18:18:22 -0600

    Some property-based tests

--- advent-2021/day-01/depth/test/depth_test.exs
+++ advent-2021/day-01/depth/test/depth_test.exs
@@ -1,4 +1,42 @@
 defmodule DepthTest do
   use ExUnit.Case
+  use PropCheck
   doctest Depth
+
+  property "a single-element list has 0 increases" do
+    forall list <- single_integer_list() do
+      Depth.count_increases(list) == 0
+    end
+  end
+
+  property "the number of increases is less than the list length" do
+    forall list <- integer_list() do
+      Depth.count_increases(list) < length(list)
+    end
+  end
+
+  property "an always-increasing list has exactly length-1 increases" do
+    forall list <- integer_list() do
+      list = Enum.sort(list)
+             |> Enum.dedup
+      Depth.count_increases(list) == length(list) - 1
+    end
+  end
+
+  property "an always-decreasing list has 0 increases" do
+    forall list <- integer_list() do
+      list = Enum.sort(list)
+             |> Enum.reverse
+             |> Enum.dedup
+      Depth.count_increases(list) == 0
+    end
+  end
+
+  def integer_list do
+    non_empty(list(integer()))
+  end
+
+  def single_integer_list do
+    vector(1, integer())
+  end
 end
