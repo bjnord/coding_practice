bd127e650b47806f4da2884c090a6fadea477ed0
Brent J. Nordquist <brent@nordist.net>
2021-12-04 11:31:35 -0600

    Day 3 RF: Combine common code

--- advent-2021/day-03/diagnostic/lib/diagnostic.ex
+++ advent-2021/day-03/diagnostic/lib/diagnostic.ex
@@ -24,16 +24,14 @@ defmodule Diagnostic do
   Process input file and display part 1 solution.
   """
   def part1(input_file, opts \\ []) do
-    {gam, eps} = input_file
-                 |> parse_input(opts)
-                 |> compute_power_rates
-    IO.inspect(gam * eps, label: "Part 1 answer is")
+    {γ, ε} = input_file
+             |> parse_input(opts)
+             |> compute_power_rates
+    IO.inspect(γ * ε, label: "Part 1 answer is")
   end
 
   @doc """
-  Compute gamma and epsilon rates from diagnostic report entries.
-
-  Returns `{gamma, epsilon}`
+  Compute γ and ε rates from diagnostic report entries.
 
   ## Examples
       iex> entries = [[1, 0, 0], [1, 1, 0], [0, 0, 0]]
@@ -42,16 +40,19 @@ defmodule Diagnostic do
   """
   def compute_power_rates(entries) do
     x_entries = transpose(entries)
-    {gamma_rate(x_entries), epsilon_rate(x_entries)}
+    {
+      power_rate(x_entries, &Diagnostic.most_common/1),
+      power_rate(x_entries, &Diagnostic.least_common/1),
+    }
   end
 
   @doc """
-  Compute gamma rate from transposed diagnostic report entries.
+  Compute γ or ε rate from transposed diagnostic report entries.
   """
-  def gamma_rate(x_entries) do
+  def power_rate(x_entries, common_func) do
     x_entries
     |> Enum.map(&Diagnostic.count_bits/1)
-    |> Enum.map(&Diagnostic.most_common/1)
+    |> Enum.map(common_func)
     |> bits_to_integer
   end
   def count_bits(bits) do
@@ -62,6 +63,8 @@ defmodule Diagnostic do
   def plus_minus(_bit), do: 1
   def most_common(sum) when sum < 0, do: 0
   def most_common(_sum), do: 1
+  def least_common(sum) when sum < 0, do: 1
+  def least_common(_sum), do: 0
 
   @doc """
   Render bit list as an integer.
@@ -80,18 +83,6 @@ defmodule Diagnostic do
   end
 
   @doc """
-  Compute epsilon rate from transposed diagnostic report entries.
-  """
-  def epsilon_rate(x_entries) do
-    x_entries
-    |> Enum.map(&Diagnostic.count_bits/1)
-    |> Enum.map(&Diagnostic.least_common/1)
-    |> bits_to_integer
-  end
-  def least_common(sum) when sum < 0, do: 1
-  def least_common(_sum), do: 0
-
-  @doc """
   Process input file and display part 2 solution.
   """
   def part2(input_file, opts \\ []) do
@@ -104,49 +95,33 @@ defmodule Diagnostic do
   @doc """
   Compute O₂ and CO₂ ratings from diagnostic report entries.
 
-  Returns `{o2, co2}`
-
   ## Examples
       iex> entries = [[1, 0, 0], [1, 1, 0], [0, 1, 0]]
       iex> Diagnostic.compute_life_support_ratings(entries)
       {0b110, 0b010}
   """
   def compute_life_support_ratings(entries) do
-    {o2_rating(entries), co2_rating(entries)}
+    {
+      life_support_rating(entries, &Diagnostic.most_common/1),
+      life_support_rating(entries, &Diagnostic.least_common/1),
+    }
   end
 
   @doc """
-  Compute O₂ rating from diagnostic report entries.
+  Compute O₂ or CO₂ rating from diagnostic report entries.
   """
-  def o2_rating(entries), do: o2_rating(entries, 0)
-  def o2_rating([entry], _), do: bits_to_integer(entry)
-  def o2_rating(entries, pos) do
+  def life_support_rating(entries, common_func), do: life_support_rating(entries, 0, common_func)
+  def life_support_rating([entry], _, _), do: bits_to_integer(entry)
+  def life_support_rating(entries, pos, common_func) do
     # keep entries whose `pos`th bit has the most common value
     filter_bit = transpose(entries)
                  |> Enum.map(&Diagnostic.count_bits/1)
-                 |> Enum.map(&Diagnostic.most_common/1)
+                 |> Enum.map(common_func)
                  |> Enum.at(pos)
     filtered_entries = entries
                        |> Enum.filter(fn entry -> Enum.at(entry, pos) == filter_bit end)
     # continue to next `pos` (using tail recursion)
-    o2_rating(filtered_entries, pos + 1)
-  end
-
-  @doc """
-  Compute CO₂ rating from diagnostic report entries.
-  """
-  def co2_rating(entries), do: co2_rating(entries, 0)
-  def co2_rating([entry], _), do: bits_to_integer(entry)
-  def co2_rating(entries, pos) do
-    # keep entries whose `pos`th bit has the least common value
-    filter_bit = transpose(entries)
-                 |> Enum.map(&Diagnostic.count_bits/1)
-                 |> Enum.map(&Diagnostic.least_common/1)
-                 |> Enum.at(pos)
-    filtered_entries = entries
-                       |> Enum.filter(fn entry -> Enum.at(entry, pos) == filter_bit end)
-    # continue to next `pos` (using tail recursion)
-    co2_rating(filtered_entries, pos + 1)
+    life_support_rating(filtered_entries, pos + 1, common_func)
   end
 
   @doc """
