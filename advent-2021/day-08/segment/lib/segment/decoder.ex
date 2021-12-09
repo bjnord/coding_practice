4c221a87eaffed96be567854b33983b253e46f7d
Brent J. Nordquist <brent@nordist.net>
2021-12-08 23:01:50 -0600

    Day 8 RF: Much better
    ...more to come after Day 9

--- advent-2021/day-08/segment/lib/segment/decoder.ex
+++ advent-2021/day-08/segment/lib/segment/decoder.ex
@@ -188,12 +188,131 @@ defmodule Segment.Decoder do
   """
   def digits_of_note({signal_patterns, output_values}) do
     indexed_patterns =
-      digit_signal_patterns({signal_patterns, output_values})
+      digit_signal_patterns(signal_patterns)
       |> Enum.with_index()
     output_values
     |> Enum.map(fn o -> Enum.find(indexed_patterns, fn {p, _i} -> p == o end) end)
     |> Enum.map(fn t -> elem(t, 1) end)
     |> Enum.map(fn n -> n + ?0 end)
     |> List.to_string()
+  end
+
+    #####################
+   #######################
+  ###                   ###
+  ###  REFACTORED CODE  ###
+  ###                   ###
+   #######################
+    #####################
+
+  @doc ~S"""
+  Find the signal pattern in `signal_patterns` for the digit with `n` segments.
+
+  ## Examples
+      iex> sp = ['abcdefg', 'bcdef', 'acdfg', 'abcdf', 'abd', 'abcdef', 'bcdefg', 'abef', 'abcdeg', 'ab']
+      iex> Segment.Decoder.find_segments(sp, 3)
+      'abd'
+      iex> Segment.Decoder.find_segments(sp, 7)
+      'abcdefg'
+  """
+  # TODO RF this can be merged with the `_ne` one (optional `excluding` param)
+  def find_segments(signal_patterns, n) do
+    Enum.find(signal_patterns, fn p -> length(p) == n end)
+  end
+
+  @doc ~S"""
+  Find the signal pattern in `signal_patterns` for the digit with `n` segments
+  which is NOT in `excluding`.
+
+  ## Examples
+      iex> sp = ['abcdefg', 'bcdef', 'acdfg', 'abcdf', 'abd', 'abcdef', 'bcdefg', 'abef', 'abcdeg', 'ab']
+      iex> Segment.Decoder.find_segments_ne(sp, 5, ['bcdef', 'abcdf'])
+      'acdfg'
+  """
+  def find_segments_ne(signal_patterns, n, excluding) do
+    Enum.filter(signal_patterns, fn p -> length(p) == n end)
+    |> Enum.find(fn p -> !Enum.member?(excluding, p) end)
+  end
+
+  @doc ~S"""
+  Merge the segments in signal patterns `da` and `db`.
+
+  ## Examples
+      iex> Segment.Decoder.merge_segments('abef', 'abd')
+      'abdef'
+      iex> Segment.Decoder.merge_segments('bcdef', 'acdfg')
+      'abcdefg'
+  """
+  def merge_segments(da, db) do
+    da ++ db
+    |> Enum.uniq()
+    |> Enum.sort()
+  end
+
+  @doc ~S"""
+  Find the signal pattern in `signal_patterns` for the digit with `n` segments
+  which adds exactly one more segment to the ones in signal pattern `da`
+  and which is NOT in `excluding` (if provided).
+
+  ## Examples
+      iex> sp = ['abcdefg', 'bcdef', 'acdfg', 'abcdf', 'abd', 'abcdef', 'bcdefg', 'abef', 'abcdeg', 'ab']
+      iex> Segment.Decoder.find_1_segment_more(sp, 'bcdef', 6, ['abcdef'])
+      'bcdefg'
+  """
+  # TODO RF can calculate `n = length(da) + 1`, don't need to pass it
+  def find_1_segment_more(signal_patterns, da, n, excluding \\ []) do
+    Enum.filter(signal_patterns, fn p -> length(p) == n end)
+    |> Enum.filter(fn p -> !Enum.member?(excluding, p) end)
+    |> Enum.map(fn p -> {p -- da, p} end)
+    |> Enum.find(fn {seg, _d} -> length(seg) == 1 end)
+    |> elem(1)
+  end
+
+  @doc ~S"""
+  Find signal patterns for the 10 digits (in order from 0..9).
+
+  ## Examples
+      iex> sp = ['abcdefg', 'bcdef', 'acdfg', 'abcdf', 'abd', 'abcdef', 'bcdefg', 'abef', 'abcdeg', 'ab']
+      iex> Segment.Decoder.digit_signal_patterns(sp)
+      ['abcdeg', 'ab', 'acdfg', 'abcdf', 'abef', 'bcdef', 'bcdefg', 'abd', 'abcdefg', 'abcdef']
+  """
+  def digit_signal_patterns(signal_patterns) do
+    # Can determine digits 1, 4, 7, and 8 directly; each one's length is unique in the list:
+    #
+    d1 = find_segments(signal_patterns, 2)
+    d4 = find_segments(signal_patterns, 4)
+    d7 = find_segments(signal_patterns, 3)
+    d8 = find_segments(signal_patterns, 7)
+
+    # Can determine digit 9 by merging the segments of digits 4 and 7, and then finding the digit that adds exactly one segment to that (the bottom one):
+    #
+    d47 = merge_segments(d4, d7)
+    d9 = find_1_segment_more(signal_patterns, d47, 6)
+
+    # Can determine digit 5 by subtracting digit 1's segments from digit 9's (the right two), and then finding the digit that adds exactly one segment to that (the lower-right one):
+    #
+    d9m1 = d9 -- d1
+    d5 = find_1_segment_more(signal_patterns, d9m1, 5)
+
+    # Can determine digit 6 by finding the 6-segment digit that adds one segment to digit 5, and isn't digit 9:
+    #
+    d6 = find_1_segment_more(signal_patterns, d5, 6, [d9])
+
+    # Can determine digit 0 by finding the 6-segment digit that isn't 6 or 9:
+    #
+    d0 = find_segments_ne(signal_patterns, 6, [d6, d9])
+
+    # Can determine and distinguish digits 2 and 3: Only the segments from 2 + 5 yield 8, and 3 is the other one.
+    #
+    d2or3 = find_segments_ne(signal_patterns, 5, [d5])
+    d3or2 = find_segments_ne(signal_patterns, 5, [d5, d2or3])
+    {d2, d3} =
+      if merge_segments(d2or3, d5) == d8 do
+        {d2or3, d3or2}
+      else
+        {d3or2, d2or3}
+      end
+
+    [d0, d1, d2, d3, d4, d5, d6, d7, d8, d9]
   end
 end
