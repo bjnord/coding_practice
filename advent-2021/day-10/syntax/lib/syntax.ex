916e97d5f248a428e9a434dc4d84cff187fa4f7c
Brent J. Nordquist <brent@nordist.net>
2021-12-10 06:41:46 -0600

    Day 10 part 1, first working solution

--- advent-2021/day-10/syntax/lib/syntax.ex
+++ advent-2021/day-10/syntax/lib/syntax.ex
@@ -25,7 +25,70 @@ defmodule Syntax do
   def part1(input_file, opts \\ []) do
     input_file
     |> parse_input(opts)
+    |> Enum.map(&Syntax.entry_status/1)
+    |> Enum.filter(fn {status, _chars} -> status == :corrupted end)
+    |> Enum.map(&Syntax.entry_score/1)
+    |> Enum.sum()
     |> IO.inspect(label: "Part 1 answer is")
+  end
+
+  @doc """
+  Determine status of navigation entry.
+
+  Returns `{status, remaining_charlist}` where `status` is one of:
+  - `:ok` - valid line
+  - `:corrupted` - corrupted line
+  - `:incomplete` - incomplete line
+
+  ## Examples
+      iex> Syntax.entry_status('<([{}])>')
+      {:ok, ''}
+      iex> Syntax.entry_status('((()})')
+      {:corrupted, '})'}
+      iex> Syntax.entry_status('<([{}]')
+      {:incomplete, '(<'}
+  """
+  def entry_status([first | rem_charlist]), do: entry_status([first | rem_charlist], [])
+  def entry_status([], []), do: {:ok, []}
+  def entry_status([], openers), do: {:incomplete, openers}
+  def entry_status([next | rem_charlist], openers) do
+    [opener | rem_openers] =
+      if openers == [] do
+        [nil]
+      else
+        openers
+      end
+    {next, rem_charlist, opener, rem_openers}
+    case next do
+      ?( -> entry_status(rem_charlist, [next | openers])
+      ?[ -> entry_status(rem_charlist, [next | openers])
+      ?{ -> entry_status(rem_charlist, [next | openers])
+      ?< -> entry_status(rem_charlist, [next | openers])
+      ?) when opener == ?( -> entry_status(rem_charlist, rem_openers)
+      ?] when opener == ?[ -> entry_status(rem_charlist, rem_openers)
+      ?} when opener == ?{ -> entry_status(rem_charlist, rem_openers)
+      ?> when opener == ?< -> entry_status(rem_charlist, rem_openers)
+      ?) -> {:corrupted, [next | rem_charlist]}
+      ?] -> {:corrupted, [next | rem_charlist]}
+      ?} -> {:corrupted, [next | rem_charlist]}
+      ?> -> {:corrupted, [next | rem_charlist]}
+    end
+  end
+
+  @doc """
+  Calculate score of navigation entry.
+
+  ## Examples
+      iex> Syntax.entry_score({:corrupted, '})'})
+      1197
+  """
+  def entry_score({:corrupted, [next | _rem_charlist]}) do
+    case next do
+      ?) -> 3
+      ?] -> 57
+      ?} -> 1197
+      ?> -> 25137
+    end
   end
 
   @doc """
--- advent-2021/day-10/syntax/test/syntax_test.exs
+++ advent-2021/day-10/syntax/test/syntax_test.exs
@@ -1,4 +1,46 @@
 defmodule SyntaxTest do
   use ExUnit.Case
   doctest Syntax
+
+  describe "puzzle example" do
+    setup do
+      [
+        entries: [
+          '[({(<(())[]>[[{[]{<()<>>',
+          '[(()[<>])]({[<{<<[]>>(',
+          '{([(<{}[<>[]}>{[]{[(<()>',
+          '(((({<>}<{<{<>}{[]{[]{}',
+          '[[<[([]))<([[{}[[()]]]',
+          '[{[{({}]{}}([{[{{{}}([]',
+          '{<[[]]>}<{[{[{[]{()[[[]',
+          '[<(<(<(<{}))><([]([]()',
+          '<{([([[(<>()){}]>(<<{{',
+          '<{([{{}}[<[[[<>{}]]]>[]]',
+        ],
+        exp_corrupted_entries: [
+          {:corrupted, '}>{[]{[(<()>'},
+          {:corrupted, ')<([[{}[[()]]]'},
+          {:corrupted, ']{}}([{[{{{}}([]'},
+          {:corrupted, '))><([]([]()'},
+          {:corrupted, '>(<<{{'},
+        ],
+        exp_corrupted_entry_scores: [1197, 3, 57, 3, 25137]
+      ]
+    end
+
+    test "status checker finds corrupted entries", fixture do
+      act_corrupted = fixture.entries
+                      |> Enum.map(&Syntax.entry_status/1)
+                      |> Enum.filter(fn {status, _chars} -> status == :corrupted end)
+      assert act_corrupted == fixture.exp_corrupted_entries
+    end
+
+    test "corrupted entry scorer finds correct scores", fixture do
+      act_scores = fixture.entries
+                   |> Enum.map(&Syntax.entry_status/1)
+                   |> Enum.filter(fn {status, _chars} -> status == :corrupted end)
+                   |> Enum.map(&Syntax.entry_score/1)
+      assert act_scores == fixture.exp_corrupted_entry_scores
+    end
+  end
 end
