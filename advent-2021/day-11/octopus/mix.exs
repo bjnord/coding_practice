8fa471714381718eb8b0f8fa0f1c7144524cd4fa
Brent J. Nordquist <brent@nordist.net>
2021-12-11 14:47:19 -0600

    ExDoc documentation

--- advent-2021/day-11/octopus/mix.exs
+++ advent-2021/day-11/octopus/mix.exs
@@ -22,6 +22,7 @@ defmodule Octopus.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.22.0", only: :dev, runtime: false},
       {:logger_file_backend, "~> 0.0.12"},
       #{:math, "~> 0.7.0"},
       #{:propcheck, "~> 1.4", only: [:test]},
