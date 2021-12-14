9e1a83995704456b125610c3f96252354e2a5587
Brent J. Nordquist <brent@nordist.net>
2021-12-14 06:50:33 -0600

    Day 14 part 1, first working solution

--- advent-2021/day-14/polymer/lib/polymer.ex
+++ advent-2021/day-14/polymer/lib/polymer.ex
@@ -3,19 +3,6 @@ defmodule Polymer do
   Documentation for Polymer.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Polymer.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
-
   import Polymer.Parser
   import Submarine.CLI
 
@@ -36,9 +23,71 @@ defmodule Polymer do
   Process input file and display part 1 solution.
   """
   def part1(input_file) do
-    File.read!(input_file)
-    |> parse_input_string()
+    # "Apply 10 steps of pair insertion to the polymer template and find
+    # the most and least common elements in the result. What do you get
+    # if you take the quantity of the most common element and subtract the
+    # quantity of the least common element?"
+    {template, rules} =
+      File.read!(input_file)
+      |> parse_input_string()
+    polymer =
+      1..10
+      |> Enum.reduce(template, fn (_n, polymer) ->
+        Polymer.step(polymer, rules)
+      end)
+    min_max = Polymer.min_max(polymer)
+    (elem(min_max, 1) - elem(min_max, 0))
     |> IO.inspect(label: "Part 1 answer is")
+  end
+
+  @doc """
+  Perform one step of polymer insertion.
+
+  ## Examples
+      iex> rules = %{{?A, ?A} => ?B}
+      iex> Polymer.step("AAA", rules)
+      "ABABA"
+  """
+  def step(polymer, rules) do
+    elements =
+      polymer
+      |> String.to_charlist()
+      #|> IO.inspect(label: "elements")
+    insertions =
+      elements
+      |> Enum.chunk_every(2, 1, :discard)
+      |> Enum.map(&List.to_tuple/1)
+      #|> IO.inspect(label: "windows")
+      |> Enum.map(fn k -> rules[k] end)
+      #|> IO.inspect(label: "insertions")
+    new_elements =
+      [elements, insertions]
+      |> Enum.zip()
+      |> Enum.flat_map(&Tuple.to_list/1)
+      #|> IO.inspect(label: "zipped elements")
+    # FIXME OPTIMIZE this is really inefficient
+    new_elements ++ [List.last(elements)]
+    |> to_string()
+    #|> IO.inspect(label: "new polymer")
+  end
+
+  @doc """
+  Find the minimum and maximum number of element occurrences in the given
+  `polymer`.
+
+  ## Examples
+      iex> Polymer.min_max("AABACABA")
+      {1, 5}
+  """
+  def min_max(polymer) do
+    frequencies =
+      polymer
+      |> String.to_charlist()
+      # TODO Elixir v1.10 has `Enum.frequencies()`
+      |> Enum.reduce(%{}, fn x, acc -> Map.update(acc, x, 1, &(&1 + 1)) end)
+    min = Enum.min(Map.values(frequencies))
+    max = Enum.max(Map.values(frequencies))
+    {min, max}
   end
 
   @doc """
