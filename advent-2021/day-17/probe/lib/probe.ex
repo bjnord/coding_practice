b515914fd7bad6143c2ab2d85b64f41c6f0a61ab
Brent J. Nordquist <brent@nordist.net>
2021-12-17 05:19:44 -0600

    Day 17 part 1: Probe shooter

--- advent-2021/day-17/probe/lib/probe.ex
+++ advent-2021/day-17/probe/lib/probe.ex
@@ -27,6 +27,46 @@ defmodule Probe do
   end
 
   @doc """
+  Fire a probe at the velocity `{xv, yv}` toward the target area
+  `{{min_x, max_x}, {min_y, max_y}}`. Returns indicator of whether
+  the probe hit the target area, and a list of positions visited.
+  """
+  def fire({xv, yv}, {{min_x, max_x}, {min_y, max_y}}) do
+    Stream.cycle([true])
+    |> Enum.reduce_while({{0, 0}, {xv, yv}, []}, fn (_, {{x, y}, {xv, yv}, positions}) ->
+      {x, y} = move({x, y}, {xv, yv})
+      {xv, yv} = slow({xv, yv})
+      case within({x, y}, {{min_x, max_x}, {min_y, max_y}}) do
+        :moving -> {:cont, {{x, y}, {xv, yv}, [{x, y} | positions]}}
+        hit_miss -> {:halt, {hit_miss, Enum.reverse([{x, y} | positions])}}
+      end
+    end)
+  end
+
+  defp move({x, y}, {xv, yv}), do: {x + xv, y + yv}
+
+  defp slow({xv, yv}) do
+    xv = cond do
+      xv > 0 -> xv - 1
+      xv < 0 -> xv + 1
+      xv == 0 -> xv
+    end
+    {xv, yv - 1}
+  end
+
+  defp within({x, y}, {{min_x, max_x}, {min_y, max_y}}) do
+    cond do
+      between(x, min_x, max_x) && between(y, min_y, max_y) -> :hit
+      y < min_y -> :miss
+      true -> :moving
+    end
+  end
+
+  defp between(n, min_n, max_n) do
+    n >= min_n && n <= max_n
+  end
+
+  @doc """
   Process input file and display part 2 solution.
   """
   def part2(input_file) do
--- advent-2021/day-17/probe/test/probe_test.exs
+++ advent-2021/day-17/probe/test/probe_test.exs
@@ -1,4 +1,28 @@
 defmodule ProbeTest do
   use ExUnit.Case
   doctest Probe
+
+  describe "puzzle example" do
+    setup do
+      [
+        input: "target area: x=20..30, y=-10..-5\n",
+        shots: [{7, 2}, {6, 3}],
+        exp_results: [
+          {:hit, [{7, 2}, {13, 3}, {18, 3}, {22, 2}, {25, 0}, {27, -3}, {28, -7}]},
+          {:hit, [{6, 3}, {11, 5}, {15, 6}, {18, 6}, {20, 5}, {21, 3}, {21, 0}, {21, -4}, {21, -9}]},
+          {:hit, [{9, 0}, {17, -1}, {24, -3}, {30, -6}]},
+          {:miss, [{17, -4}, {33, -9}, {48, -15}]},
+        ],
+      ]
+    end
+
+    test "shooter gets expected results", fixture do
+      target = Probe.Parser.parse(fixture.input)
+      [fixture.shots, fixture.exp_results]
+      |> Enum.zip()
+      |> Enum.each(fn {shot, exp_result} ->
+        assert Probe.fire(shot, target) == exp_result
+      end)
+    end
+  end
 end
