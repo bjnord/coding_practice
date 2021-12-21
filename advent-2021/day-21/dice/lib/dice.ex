6f69b288abc73406780a9e2dd086067144e35519
Brent J. Nordquist <brent@nordist.net>
2021-12-20 23:55:59 -0600

    Day 21 part 1, first working solution

--- advent-2021/day-21/dice/lib/dice.ex
+++ advent-2021/day-21/dice/lib/dice.ex
@@ -23,10 +23,45 @@ defmodule Dice do
   Process input file and display part 1 solution.
   """
   def part1(input_file) do
-    File.read!(input_file)
-    |> Parser.parse()
-    nil  # TODO
+    # "Play a practice game using the deterministic 100-sided die. The
+    # moment either player wins, what do you get if you multiply the score
+    # of the losing player by the number of times the die was rolled during
+    # the game?"
+    {scores, _positions, turns, _die} =
+      File.read!(input_file)
+      |> Parser.parse()
+      |> play()
+    part1_answer({scores, turns})
     |> IO.inspect(label: "Part 1 answer is")
+  end
+
+  def part1_answer({scores, turns}) do
+    losing_score = Enum.min(Tuple.to_list(scores))
+    losing_score * (turns * 3)
+  end
+
+  @doc """
+  Play a game of Dirac Dice.
+  """
+  def play({start_p1, start_p2}) do
+    Stream.cycle([0, 1])
+    |> Enum.reduce_while({{0, 0}, {start_p1, start_p2}, 0, 1}, fn (player, {scores, positions, turns, die}) ->
+      # this is the "100-sided deterministic die" for part 1
+      move = die + (die + 1) + (die + 2)
+      die = rem((die - 1) + 3, 100) + 1
+      position = rem((elem(positions, player) - 1) + move, 10) + 1
+      score = elem(scores, player) + position
+      cont_halt = if score >= 1000, do: :halt, else: :cont
+      {
+        cont_halt,
+        {
+          put_elem(scores, player, score),
+          put_elem(positions, player, position),
+          turns + 1,
+          die,
+        },
+      }
+    end)
   end
 
   @doc """
--- advent-2021/day-21/dice/test/dice_test.exs
+++ advent-2021/day-21/dice/test/dice_test.exs
@@ -1,4 +1,23 @@
 defmodule DiceTest do
   use ExUnit.Case
   doctest Dice
+
+  describe "puzzle example" do
+    setup do
+      [
+        start_positions: {4, 8},
+        exp_part1_answer: 739785,
+      ]
+    end
+
+    test "player gets expected part 1 answer", fixture do
+      act_answer =
+        Dice.play(fixture.start_positions)
+        |> Tuple.delete_at(3)
+        |> Tuple.delete_at(1)
+        |> IO.inspect(label: "scores and turns")
+        |> Dice.part1_answer()
+      assert act_answer == fixture.exp_part1_answer
+    end
+  end
 end
