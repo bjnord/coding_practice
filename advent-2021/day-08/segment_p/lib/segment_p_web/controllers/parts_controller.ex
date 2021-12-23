dbb2ef13f3e1cf275999499441594feabb8a0a5f
Brent J. Nordquist <brent@nordist.net>
2021-12-22 22:27:03 -0600

    Day 8 Phoenix: Produces the part answers!
    This shows how to link the peer project's code in the Mix dependencies
    and call its functions from Phoenix code.

--- advent-2021/day-08/segment_p/lib/segment_p_web/controllers/parts_controller.ex
+++ advent-2021/day-08/segment_p/lib/segment_p_web/controllers/parts_controller.ex
@@ -2,6 +2,18 @@ defmodule SegmentPWeb.PartsController do
   use SegmentPWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    input =
+      File.read!("../segment/input/input.txt")
+      |> Segment.Parser.parse()
+    part1 =
+      input
+      |> Enum.map(&Segment.count_unique/1)
+      |> Enum.sum()
+    part2 =
+      input
+      |> Enum.map(&Segment.Decoder.digits_of_note/1)
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.sum()
+    render(conn, "index.html", part1: part1, part2: part2)
   end
 end
