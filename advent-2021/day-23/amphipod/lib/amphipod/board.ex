2a28cae9b991d5ba8bceab9363db4b4285e7bfa8
Brent J. Nordquist <brent@nordist.net>
2021-12-23 07:54:20 -0600

    Day 23: Track player positions on Board

--- advent-2021/day-23/amphipod/lib/amphipod/board.ex
+++ advent-2021/day-23/amphipod/lib/amphipod/board.ex
@@ -19,7 +19,7 @@ defmodule Amphipod.Board do
 
   alias Amphipod.Board
 
-  defstruct n_rooms: 0, hallway_pos: []
+  defstruct n_rooms: 0, hallway_pos: [], player_pos: %{}
 
   @doc ~S"""
   Construct new game board.
@@ -34,9 +34,15 @@ defmodule Amphipod.Board do
       amphipos
       |> Enum.map(fn {_type, {x, _y}} -> x end)
       |> Enum.min_max()
+    player_pos =
+      amphipos
+      |> Enum.with_index()
+      |> Enum.map(fn {{_type, pos}, n} -> {n, pos} end)
+      |> Enum.into(%{})
     %Board{
       n_rooms: n_rooms,
       hallway_pos: hallway_positions(n_rooms, min_x, max_x),
+      player_pos: player_pos,
     }
   end
 
@@ -47,5 +53,39 @@ defmodule Amphipod.Board do
     middle_x = for r <- 1..n_rooms-1, do: (r-1)*2+min_x+1
     leftcul_x ++ middle_x ++ rightcul_x
     |> Enum.map(&({&1, 2}))
+  end
+
+  @doc ~S"""
+  Return current player position on board.
+
+  Raises `KeyError` if the given `player` is not found.
+  """
+  def player_pos(board, player) do
+    pos = board.player_pos[player]
+    if pos == nil do
+      raise KeyError, "player #{player} not found on board"
+    else
+      pos
+    end
+  end
+
+  @doc ~S"""
+  Update player position on board.
+
+  Raises `KeyError` if the given `player` is not found.
+  """
+  def update_player_pos(board, player, pos) do
+    %Board{board | player_pos: Map.replace!(board.player_pos, player, pos)}
+  end
+
+  @doc ~S"""
+  Is the given position occupied by a player?
+  """
+  def occupied?(board, pos) do
+    # FIXME OPTIMIZE maintain a map keyed in the other direction
+    player =
+      Map.values(board.player_pos)
+      |> Enum.find(&(&1 == pos))
+    if player, do: true, else: false
   end
 end
--- advent-2021/day-23/amphipod/test/amphipod/board_test.exs
+++ advent-2021/day-23/amphipod/test/amphipod/board_test.exs
@@ -24,6 +24,10 @@ defmodule Amphipod.BoardTest do
             {4, 2}, {6, 2}, {8, 2},
             {10, 2}, {11, 2},
           ],
+          player_pos: %{
+            0 => {3, 1}, 1 => {5, 1}, 2 => {7, 1}, 3 => {9, 1},
+            4 => {3, 0}, 5 => {5, 0}, 6 => {7, 0}, 7 => {9, 0},
+          },
         },
         tiny_amphipos: [
           {0, {2, 1}},
@@ -38,6 +42,9 @@ defmodule Amphipod.BoardTest do
             {3, 2},
             {5, 2},
           ],
+          player_pos: %{
+            0 => {2, 1}, 1 => {4, 1}, 2 => {2, 0}, 3 => {4, 0},
+          },
         },
       ]
     end
@@ -48,6 +55,28 @@ defmodule Amphipod.BoardTest do
 
     test "board constructor produces expected board (input)", fixture do
       assert Board.new(fixture.input_amphipos) == fixture.exp_input_new_board
+    end
+
+    test "player position get and set", fixture do
+      board = Board.new(fixture.tiny_amphipos)
+      assert Board.player_pos(board, 0) == {2, 1}
+      upd_board = Board.update_player_pos(board, 0, {1, 2})
+      assert Board.player_pos(upd_board, 0) == {1, 2}
+    end
+
+    test "invalid player position get and set", fixture do
+      board = Board.new(fixture.tiny_amphipos)
+      assert_raise KeyError, fn -> Board.player_pos(board, 4) end
+      assert_raise KeyError, fn -> Board.update_player_pos(board, 5, {5, 2}) end
+    end
+
+    test "position occupied", fixture do
+      board = Board.new(fixture.tiny_amphipos)
+      assert Board.occupied?(board, {2, 1}) == true
+      assert Board.occupied?(board, {1, 2}) == false
+      upd_board = Board.update_player_pos(board, 0, {1, 2})
+      assert Board.occupied?(upd_board, {2, 1}) == false
+      assert Board.occupied?(upd_board, {1, 2}) == true
     end
   end
 end
