d0b582f2c1a727b952f52de2535e83bb4be7f8c2
Brent J. Nordquist <brent@nordist.net>
2022-11-30 23:40:07 -0600

    Day 1 part 2, first working solution

--- advent-2022/day-01/src/main.js
+++ advent-2022/day-01/src/main.js
@@ -9,12 +9,13 @@ const elfCalories = sections.map((items) => {
   return items.reduce((acc, cal) => acc + cal);
 });
 const maxElfCalories = Math.max(...elfCalories);
-console.log('part 1: expected answer:                64929');
-console.log(`part 1: calories of elf having most:    ${maxElfCalories}`);
+console.log('part 1: expected answer:                 64929');
+console.log(`part 1: calories of elf having most:     ${maxElfCalories}`);
 console.log('');
 
 // PART 2
-const answer2 = 0;
-console.log('part 2: expected answer:                376203951569712');
-console.log(`part 2: actual answer:                  ${answer2}`);
+const topElfCalories = elfCalories.sort((a, b) => b - a).slice(0, 3);
+const maxElfCalories3 = topElfCalories.reduce((acc, cal) => acc + cal);
+console.log('part 2: expected answer:                 193697');
+console.log(`part 2: calories of 3 elves having most: ${maxElfCalories3}`);
 console.log('');
