8c1281be765789e2baf0421557fde7910c05c76f
Brent J. Nordquist <brent@nordist.net>
2022-11-30 23:49:13 -0600

    Common files for 2022

---
+++ advent-2022/.eslintrc.js
@@ -0,0 +1,66 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true,
+        "mocha": true,
+    },
+    "parserOptions": {
+        "ecmaVersion": 8,
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly",
+    },
+    "rules": {
+        "keyword-spacing": [
+            "error",
+            {
+                "before": true,
+                "after": true,
+            },
+        ],
+        "space-before-function-paren": [
+            "error",
+            {
+                "anonymous": "always",
+                "named": "never",
+            },
+        ],
+        "eqeqeq": "error",
+        "strict": [
+            "error",
+            "global",
+        ],
+        "no-var": "error",
+        "prefer-const": "error",
+        "no-console": "off",
+        "indent": [
+            "error",
+            2,
+        ],
+        "linebreak-style": [
+            "error",
+            "unix",
+        ],
+        "quotes": [
+            "error",
+            "single",
+            "avoid-escape",
+        ],
+        "semi": [
+            "error",
+            "always",
+        ],
+        "no-extra-semi": "off",
+        "complexity": [
+            "error",
+            4,
+        ],
+        "max-statements": [
+            "error",
+            10,
+        ],
+    },
+};
