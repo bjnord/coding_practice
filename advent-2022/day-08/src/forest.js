c02e42cde5ab4174615b1616bfdbc14c491ec768
Brent J. Nordquist <brent@nordist.net>
2022-12-07 23:30:54 -0600

    Day 8 part 1: individual tree visibility

--- advent-2022/day-08/src/forest.js
+++ advent-2022/day-08/src/forest.js
@@ -13,3 +13,68 @@ exports.parse = (input) => {
     return line.split('').map((ch) => parseInt(ch));
   });
 };
+
+exports.isVisible = (grid, y, x) => {
+  const dim = grid[0].length;
+  // edge trees are always visible
+  if ((y <= 0) || (y >= dim - 1)) {
+    return true;
+  } else if ((x <= 0) || (x >= dim - 1)) {
+    return true;
+  }
+  // consider tree at [y, x]
+  //console.log(`[${y},${x}] = ${grid[y][x]}`);
+  // look from left
+  let j = 0;
+  for (; j < x; j++) {
+    //console.log(`  [${y},${j}] = ${grid[y][j]}`);
+    if (grid[y][j] >= grid[y][x]) {
+      //console.log(`    TALL`);
+      break;
+    }
+  }
+  if (j === x) {
+    //console.log(`    SHORT`);
+    return true;
+  }
+  // look from right
+  j = dim - 1;
+  for (; j > x; j--) {
+    //console.log(`  [${y},${j}] = ${grid[y][j]}`);
+    if (grid[y][j] >= grid[y][x]) {
+      //console.log(`    TALL`);
+      break;
+    }
+  }
+  if (j === x) {
+    //console.log(`    SHORT`);
+    return true;
+  }
+  // look from top
+  j = 0;
+  for (; j < y; j++) {
+    //console.log(`  [${j},${x}] = ${grid[j][x]}`);
+    if (grid[j][x] >= grid[y][x]) {
+      //console.log(`    TALL`);
+      break;
+    }
+  }
+  if (j === y) {
+    //console.log(`    SHORT`);
+    return true;
+  }
+  // look from bottom
+  j = dim - 1;
+  for (; j > y; j--) {
+    //console.log(`  [${j},${x}] = ${grid[j][x]}`);
+    if (grid[j][x] >= grid[y][x]) {
+      //console.log(`    TALL`);
+      break;
+    }
+  }
+  if (j === y) {
+    //console.log(`    SHORT`);
+    return true;
+  }
+  return false;
+};
--- advent-2022/day-08/test/forest_test.js
+++ advent-2022/day-08/test/forest_test.js
@@ -14,3 +14,29 @@ describe('parsing tests', () => {
     expect(forest.parse(exampleInput)).to.eql(expected);
   });
 });
+describe('visibility tests', () => {
+  it('should determine if tree is visible', () => {
+    const grid = forest.parse(exampleInput);
+    // All of the trees around the edge of the grid are visible - since they are already on the edge, there are no trees to block the view.
+    expect(forest.isVisible(grid, 0, 1)).to.equal(true);
+    expect(forest.isVisible(grid, 1, 4)).to.equal(true);
+    expect(forest.isVisible(grid, 4, 3)).to.equal(true);
+    expect(forest.isVisible(grid, 3, 0)).to.equal(true);
+    // The top-left 5 is visible from the left and top. (It isn't visible from the right or bottom since other trees of height 5 are in the way.)
+    expect(forest.isVisible(grid, 1, 1)).to.equal(true);
+    // The top-middle 5 is visible from the top and right.
+    expect(forest.isVisible(grid, 1, 2)).to.equal(true);
+    // The top-right 1 is not visible from any direction; for it to be visible, there would need to only be trees of height 0 between it and an edge.
+    expect(forest.isVisible(grid, 1, 3)).to.equal(false);
+    // The left-middle 5 is visible, but only from the right.
+    expect(forest.isVisible(grid, 2, 1)).to.equal(true);
+    // The center 3 is not visible from any direction; for it to be visible, there would need to be only trees of at most height 2 between it and an edge.
+    expect(forest.isVisible(grid, 2, 2)).to.equal(false);
+    // The right-middle 3 is visible from the right.
+    expect(forest.isVisible(grid, 2, 3)).to.equal(true);
+    // In the bottom row, the middle 5 is visible, but the 3 and 4 are not.
+    expect(forest.isVisible(grid, 3, 1)).to.equal(false);
+    expect(forest.isVisible(grid, 3, 2)).to.equal(true);
+    expect(forest.isVisible(grid, 3, 3)).to.equal(false);
+  });
+});
