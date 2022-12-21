81be8faabf3fd2b1f8eb08f5213a2ee64c3376e1
Brent J. Nordquist <brent@nordist.net>
2022-12-21 10:18:29 -0600

    Day 21 part 2: More test coverage

--- advent-2022/day-21/test/yell_test.js
+++ advent-2022/day-21/test/yell_test.js
@@ -7,6 +7,14 @@ describe('parsing tests', () => {
     expect(yell.parseLine('root: pppw + sjmn')).to.eql({name: 'root', arg1: 'pppw', op: '+', arg2: 'sjmn'});
     expect(yell.parseLine('dbpl: 5')).to.eql({name: 'dbpl', number: 5});
   });
+  it('should throw exception for bad line (no job)', () => {
+    const badLineFn = () => { yell.parseLine('root'); };
+    expect(badLineFn).to.throw(SyntaxError);
+  });
+  it('should throw exception for bad line (no op)', () => {
+    const badLineFn = () => { yell.parseLine('root: pppw + '); };
+    expect(badLineFn).to.throw(SyntaxError);
+  });
   it('should parse a whole input set correctly', () => {
     const monkeys = yell.parse(exampleInput);
     expect(monkeys.length).to.equal(15);
@@ -24,16 +32,41 @@ describe('yelling tests', () => {
     const monkeyNumbers = yell.monkeyNumbers(monkeys);
     expect(monkeyNumbers['root']).to.equal(152);
   });
+  it('should throw exception for bad op (root monkey)', () => {
+    const badOpInput = 'root: humn ^ dbpl\ndbpl: 5\nhumn: 5\n';
+    const monkeys = yell.parse(badOpInput);
+    const badOpFn = () => { yell.monkeyNumbers(monkeys); };
+    expect(badOpFn).to.throw(SyntaxError);
+  });
   it('should calculate what the human yells correctly', () => {
     const monkeys = yell.parse(exampleInput);
     yell.monkeyNumbers(monkeys);
     expect(yell.humanYell(monkeys)).to.equal(301);
   });
   it('should calculate what the human yells correctly (root flip)', () => {
-    const flipInput = exampleInput.replace('root: pppw + sjmn', 'root: sjmn + pppw');
-    const monkeys = yell.parse(flipInput);
+    const rootFlipInput = exampleInput.replace('root: pppw + sjmn', 'root: sjmn + pppw');
+    const monkeys = yell.parse(rootFlipInput);
     yell.monkeyNumbers(monkeys);
     expect(yell.humanYell(monkeys)).to.equal(301);
+  });
+  it('should calculate what the human yells correctly (commutative)', () => {
+    const commutative = [
+      'root: juli + josi',
+      'juli: amee + alex',
+      'amee: buki * abby',
+      'buki: 5',
+      'abby: 4',
+      'alex: 4',
+      'josi: benj / mark',
+      'benj: 360',
+      'mark: emly - humn',
+      'emly: 34',
+      'humn: 0',
+    ];
+    const commutativeInput = commutative.join('\n') + '\n';
+    const monkeys = yell.parse(commutativeInput);
+    yell.monkeyNumbers(monkeys);
+    expect(yell.humanYell(monkeys)).to.equal(19);
   });
   it('should throw exception if prereq function not called', () => {
     const monkeys = yell.parse(exampleInput);
