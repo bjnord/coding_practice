faa21fe76522ac1c4232725345491276e5ad301c
Brent J. Nordquist <brent@nordist.net>
2022-12-20 22:00:15 -0600

    Day 20 part 1, first working solution

--- advent-2022/day-20/src/grove.js
+++ advent-2022/day-20/src/grove.js
@@ -70,81 +70,46 @@ exports.rotatedEqual = ((a, b) => {
   return false;
 });
 
-exports.rotateLeft = ((state, fromSlot, toSlot) => {
-  if (fromSlot < 0) {
-    throw new SyntaxError(`fromSlot=${fromSlot} < 0`);
-  } else if (toSlot >= state.count) {
-    throw new SyntaxError(`fromSlot=${fromSlot} >= count=${state.count}`);
-  } else if (fromSlot > toSlot) {
-    throw new SyntaxError(`fromSlot=${fromSlot} > toSlot=${toSlot}`);
+exports.rotateLeft = ((state, slot, dist) => {
+  if (slot < 0) {
+    throw new SyntaxError(`slot=${slot} < 0`);
+  } else if (slot >= state.count) {
+    throw new SyntaxError(`slot=${slot} >= count=${state.count}`);
+  } else if (dist <= 0) {
+    throw new SyntaxError(`dist=${dist} <= 0`);
   }
-  const original = state.slots[fromSlot];
-  for (let i = fromSlot; i < toSlot; i++) {
+  const original = state.slots[slot];
+  for (let i = slot; i <= slot + dist; i++) {
     const modI = math.mod(i, state.count);
     const old = state.slots[modI];
-    state.slots[modI] = state.slots[math.mod(i + 1, state.count)];
+    const modJ = math.mod(i + 1, state.count);
+    state.slots[modI] = (i == slot + dist) ? original : state.slots[modJ];
     if (_debug) {
       console.debug(`  modI=${modI} old=${old} new=${state.slots[modI]}`);
     }
     state.slotIndex[state.slots[modI]] = modI;
   }
-  // FIXME DRY same as loop guts, except `original`
-  const modI = math.mod(toSlot, state.count);
-  const old = state.slots[modI];
-  state.slots[modI] = original;
-  if (_debug) {
-    console.debug(`  modI=${modI} old=${old} new=${state.slots[modI]}`);
-  }
-  state.slotIndex[state.slots[modI]] = modI;
 });
 
-exports.rotateRight = ((state, fromSlot, toSlot) => {
-  if (fromSlot < 0) {
-    throw new SyntaxError(`fromSlot=${fromSlot} < 0`);
-  } else if (toSlot >= state.count) {
-    throw new SyntaxError(`fromSlot=${fromSlot} >= count=${state.count}`);
-  } else if (fromSlot > toSlot) {
-    throw new SyntaxError(`fromSlot=${fromSlot} > toSlot=${toSlot}`);
+exports.rotateRight = ((state, slot, dist) => {
+  if (slot < 0) {
+    throw new SyntaxError(`slot=${slot} < 0`);
+  } else if (slot >= state.count) {
+    throw new SyntaxError(`slot=${slot} >= count=${state.count}`);
+  } else if (dist >= 0) {
+    throw new SyntaxError(`dist=${dist} >= 0`);
   }
-  const original = state.slots[toSlot];
-  for (let i = toSlot; i > fromSlot; i--) {
+  const original = state.slots[slot];
+  for (let i = slot; i >= slot + dist; i--) {
     const modI = math.mod(i, state.count);
     const old = state.slots[modI];
-    state.slots[modI] = state.slots[math.mod(i - 1, state.count)];
+    const modJ = math.mod(i - 1, state.count);
+    state.slots[modI] = (i == slot + dist) ? original : state.slots[modJ];
     if (_debug) {
       console.debug(`  modI=${modI} old=${old} new=${state.slots[modI]}`);
     }
     state.slotIndex[state.slots[modI]] = modI;
   }
-  // FIXME DRY same as loop guts, except `original`
-  const modI = math.mod(fromSlot, state.count);
-  const old = state.slots[modI];
-  state.slots[modI] = original;
-  if (_debug) {
-    console.debug(`  modI=${modI} old=${old} new=${state.slots[modI]}`);
-  }
-  state.slotIndex[state.slots[modI]] = modI;
-});
-
-/*
- * The rules for movement, as shown in the puzzle example, are NOT
- * simple modulo arithmetic. Movement never includes numbers at both
- * ends; it's always a single range that gets rotated.
- */
-exports.destSlot = ((curNumber, curSlot, count) => {
-  let destSlot = curSlot + curNumber;
-  if (_debug) {
-    console.debug(`curSlot=${curSlot} curNumber=${curNumber} INITIAL destSlot=${destSlot}`);
-  }
-  if (destSlot >= count) {
-    destSlot = math.mod(destSlot + 1, count);
-  } else if (destSlot <= 0) {
-    destSlot = math.mod(destSlot - 1, count);
-  }
-  if (_debug) {
-    console.debug(`curSlot=${curSlot} curNumber=${curNumber}   FINAL destSlot=${destSlot}`);
-  }
-  return destSlot;
 });
 
 exports.doMove = ((state) => {
@@ -166,15 +131,13 @@ exports.doMove = ((state) => {
       console.debug(`  no move`);
     }
   }
-  // move off the right edge
-  else if ((curSlot + curNumber) >= state.count) {
-    const destSlot = module.exports.destSlot(curNumber, curSlot, state.count);
-    module.exports.rotateRight(state, destSlot, curSlot);
+  // move left
+  else if (curNumber < 0) {
+    module.exports.rotateRight(state, curSlot, curNumber);
   }
-  // all other moves (including moving off the left edge)
+  // move right
   else {
-    const destSlot = module.exports.destSlot(curNumber, curSlot, state.count);
-    module.exports.rotateLeft(state, curSlot, destSlot);
+    module.exports.rotateLeft(state, curSlot, curNumber);
   }
 
   if (_debug) {
--- advent-2022/day-20/src/main.js
+++ advent-2022/day-20/src/main.js
@@ -9,7 +9,7 @@ const state = grove.state(numbers);
 grove.doMoves(state);
 const answer1 = grove.coordinates(state)
   .reduce((sum, coord) => sum + coord, 0);
-console.log('part 1: expected answer:                **TOOLOW** 1242');
+console.log('part 1: expected answer:                6712');
 console.log(`part 1: sum of grove coordinates:       ${answer1}`);
 console.log('');
 
