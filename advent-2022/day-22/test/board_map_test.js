173b96c7f2910c570cab355225aebbb92f8e9a93
Brent J. Nordquist <brent@nordist.net>
2022-12-22 11:08:51 -0600

    RF: Clean up cell tests and add a few more
    fixed one labeling bug; also keeps lint happy

--- advent-2022/day-22/test/board_map_test.js
+++ advent-2022/day-22/test/board_map_test.js
@@ -42,30 +42,40 @@ describe('BoardMap constructor tests', () => {
     const badRowFn = () => { map.rowRange(12); };
     expect(badRowFn).to.throw(SyntaxError);
   });
-  it('should return correct cell values', () => {
-    expect(map.cellIsVoid(0, 0), 'void 0,0').to.be.true;
+  it('floor cell test should return correct values', () => {
     expect(map.cellIsFloor(0, 0), '! floor 0,0').to.be.false;
-    expect(map.cellIsFloor(0, 8), 'floor 0,14').to.be.true;
-    expect(map.cellIsVoid(0, 8), '! void 0,14').to.be.false;
-    expect(map.cellIsWall(0, 11), 'wall 0,11').to.be.true;
-    expect(map.cellIsVoid(0, 12), 'void 0,12').to.be.true;
+    expect(map.cellIsFloor(0, 8), 'floor 0,8').to.be.true;
+    expect(map.cellIsFloor(0, 11), '! floor 0,11').to.be.false;
+    //
     expect(map.cellIsFloor(7, 11), 'floor 7,11').to.be.true;
+    expect(map.cellIsFloor(8, 15), 'floor 8,15').to.be.true;
+    expect(map.cellIsFloor(11, 15), 'floor 11,15').to.be.true;
+  });
+  it('wall cell test should return correct values', () => {
+    expect(map.cellIsWall(0, 0), '! wall 0,0').to.be.false;
+    expect(map.cellIsWall(0, 8), '! wall 0,8').to.be.false;
+    expect(map.cellIsWall(0, 11), 'wall 0,11').to.be.true;
+    //
+    expect(map.cellIsWall(8, 11), 'wall 8,11').to.be.true;
+    expect(map.cellIsWall(11, 14), 'wall 11,14').to.be.true;
+  });
+  it('void cell test should return correct values', () => {
+    expect(map.cellIsVoid(0, 0), 'void 0,0').to.be.true;
+    expect(map.cellIsVoid(0, 8), '! void 0,8').to.be.false;
+    expect(map.cellIsVoid(0, 11), '! void 0,11').to.be.false;
+    //
+    expect(map.cellIsVoid(0, 12), 'void 0,12').to.be.true;
     expect(map.cellIsVoid(7, 12), 'void 7,12').to.be.true;
     expect(map.cellIsVoid(8, 7), 'void 8,7').to.be.true;
-    expect(map.cellIsWall(8, 11), 'wall 8,11').to.be.true;
-    expect(map.cellIsFloor(8, 15), 'floor 8,15').to.be.true;
     expect(map.cellIsVoid(8, 16), 'void 8,16').to.be.true;
     expect(map.cellIsVoid(11, 7), 'void 11,7').to.be.true;
-    expect(map.cellIsWall(11, 14), 'wall 11,14').to.be.true;
-    expect(map.cellIsFloor(11, 15), 'floor 11,15').to.be.true;
     expect(map.cellIsVoid(11, 16), 'void 11,16').to.be.true;
+    expect(map.cellIsVoid(12, 0), 'void 12,0').to.be.true;
   });
-  it('should return undefined when testing void cell', () => {
+  it('cell tests should return undefined for void cells', () => {
     expect(map.cellIsFloor(0, 12), 'X floor 0,12').to.be.undefined;
     expect(map.cellIsWall(0, 12), 'X wall 0,12').to.be.undefined;
-    expect(map.cellIsVoid(0, 12), 'void 0,12').to.be.true;
     expect(map.cellIsFloor(12, 0), 'X floor 12,0').to.be.undefined;
     expect(map.cellIsWall(12, 0), 'X wall 12,0').to.be.undefined;
-    expect(map.cellIsVoid(12, 0), 'void 12,0').to.be.true;
   });
 });
