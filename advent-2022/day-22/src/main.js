2babb3ce93ea34d3e4a84a3d5e30adc0a2f01f68
Brent J. Nordquist <brent@nordist.net>
2022-12-22 12:46:02 -0600

    Dumper for example trail map

--- advent-2022/day-22/src/main.js
+++ advent-2022/day-22/src/main.js
@@ -4,6 +4,14 @@ const fs = require('fs');
 const input = fs.readFileSync('input/input.txt', 'utf8');
 const notes = gps.parse(input);
 
+if (process.argv[process.argv.length - 1] === '--dump') {
+  const exampleInput = fs.readFileSync('input/example.txt', 'utf8');
+  const exampleNotes = gps.parse(exampleInput);
+  gps.followNotes(exampleNotes);
+  console.log(gps.renderTrail(exampleNotes));
+  process.exit(0);
+}
+
 // PART 1
 gps.followNotes(notes);
 console.log('part 1: expected answer:                1');
