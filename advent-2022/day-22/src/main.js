785e56e0d23f8f9d31e11ccc046d8f9f4c8561d3
Brent J. Nordquist <brent@nordist.net>
2022-12-22 19:24:04 -0600

    Cube example dumper

--- advent-2022/day-22/src/main.js
+++ advent-2022/day-22/src/main.js
@@ -2,17 +2,18 @@
 const gps = require('../src/gps');
 const fs = require('fs');
 const input = fs.readFileSync('input/input.txt', 'utf8');
-const notes = gps.parse(input);
 
 if (process.argv[process.argv.length - 1] === '--dump') {
+  const cubicDump = (process.argv[process.argv.length - 2] === '--cubic');
   const exampleInput = fs.readFileSync('input/example.txt', 'utf8');
   const exampleNotes = gps.parse(exampleInput);
-  gps.followNotes(exampleNotes);
+  gps.followNotes(exampleNotes, cubicDump);
   console.log(gps.renderTrail(exampleNotes));
   process.exit(0);
 }
 
 // PART 1
+const notes = gps.parse(input);
 gps.followNotes(notes);
 console.log('part 1: expected answer:                1');
 console.log(`part 1: final password:                 ${gps.password(notes)}`);
