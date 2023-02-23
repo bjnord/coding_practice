4465b131db3ae4f1d4a395fdd16f0f0231889c4e
Brent J. Nordquist <brent@nordist.net>
2023-02-22 21:52:19 -0600

    2015-02-p2: Correct answer

--- advent-2015/day-02/src/lib.rs
+++ advent-2015/day-02/src/lib.rs
@@ -66,6 +66,18 @@ impl Wrapping {
             }
         }
     }
+
+    #[must_use]
+    pub fn ribbon(&self) -> u32 {
+        let min = self.min_dimensions();
+        2 * min.0
+            + 2 * min.1
+            + self.cubic()
+    }
+
+    fn cubic(&self) -> u32 {
+        self.length * self.width * self.height
+    }
 }
 
 #[cfg(test)]
@@ -124,5 +136,27 @@ mod tests {
     fn test_wrapping_paper_ex2() {
         let w = Wrapping::from_str("1x1x10").unwrap();
         assert_eq!(43, w.paper());
+    }
+
+    #[test]
+    fn test_wrapping_cubic() {
+        let w = Wrapping::from_str("6x7x5").unwrap();
+        assert_eq!(210, w.cubic());
+    }
+
+    // "A present with dimensions 2x3x4 requires 2+2+3+3 = 10 feet of ribbon to wrap the present
+    // plus 2*3*4 = 24 feet of ribbon for the bow, for a total of 34 feet."
+    #[test]
+    fn test_wrapping_ribbon_ex1() {
+        let w = Wrapping::from_str("2x3x4").unwrap();
+        assert_eq!(34, w.ribbon());
+    }
+
+    // "A present with dimensions 1x1x10 requires 1+1+1+1 = 4 feet of ribbon to wrap the present
+    // plus 1*1*10 = 10 feet of ribbon for the bow, for a total of 14 feet."
+    #[test]
+    fn test_wrapping_ribbon_ex2() {
+        let w = Wrapping::from_str("1x1x10").unwrap();
+        assert_eq!(14, w.ribbon());
     }
 }
--- advent-2015/day-02/src/main.rs
+++ advent-2015/day-02/src/main.rs
@@ -4,6 +4,7 @@ use std::time::Instant;
 
 fn main() {
     part1();
+    part2();
 }
 
 /// Output solution for part 1.
@@ -17,7 +18,23 @@ fn part1() {
     let gen_time = start.elapsed();
     let total_paper = packages.iter().map(Wrapping::paper).sum::<u32>();
     let run_time = start.elapsed() - gen_time;
-    println!("Day 1 - Part 1 : {} <=> 1606483 expected", total_paper);
+    println!("Day 2 - Part 1 : {} <=> 1606483 expected", total_paper);
+    println!("    generator: {:?}", gen_time);
+    println!("    runner: {:?}", run_time);
+}
+
+/// Output solution for part 2.
+fn part2() {
+    let start = Instant::now();
+    let input: String = fs::read_to_string("private/input.txt").unwrap();
+    let packages: Vec<Wrapping> = input
+        .lines()
+        .map(|line| line.trim().parse::<Wrapping>().unwrap())
+        .collect();
+    let gen_time = start.elapsed();
+    let total_ribbon = packages.iter().map(Wrapping::ribbon).sum::<u32>();
+    let run_time = start.elapsed() - gen_time;
+    println!("Day 2 - Part 2 : {} <=> 3842356 expected", total_ribbon);
     println!("    generator: {:?}", gen_time);
     println!("    runner: {:?}", run_time);
 }
