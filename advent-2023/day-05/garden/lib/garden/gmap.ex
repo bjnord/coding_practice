8c31fb3bf26c22aa15d8189acf81d213e8fb9ba6
Brent J. Nordquist <brent@nordist.net>
2023-12-05 19:04:52 -0600

    2023-05-p1: 3rd step, part 2 refactor for ranges

--- advent-2023/day-05/garden/lib/garden/gmap.ex
+++ advent-2023/day-05/garden/lib/garden/gmap.ex
@@ -6,6 +6,55 @@ defmodule Garden.Gmap do
   defstruct from: nil, to: nil, maps: []
 
   @doc """
+  Transform range, by applying a range map.
+
+  ## Parameters
+
+  - `{value, length}`: the range to transform (integer value and range length)
+  - `gmap`: the `Gmap` to apply
+
+  Returns a list of transformed ranges (`{value, length}` tuples)
+  """
+  def transform({value, length}, gmap) do
+    Stream.cycle([true])
+    |> Enum.reduce_while({value, length, []}, fn _, {acc_v, acc_len, ranges} ->
+      map =
+        gmap.maps
+        |> Enum.find(fn map -> matching_map?(acc_v, map) end)
+      {new_v, new_len} =
+        if map do
+          {
+            acc_v - map.from + map.to,
+            min(acc_len, map.from + map.length - acc_v),
+          }
+        else
+          map =
+            gmap.maps
+            |> nearest_map(acc_v)
+          if map do
+            {
+              acc_v,
+              min(acc_len, map.from - acc_v),
+            }
+          else
+            {
+              acc_v,
+              acc_len,
+            }
+          end
+        end
+      new_acc = {acc_v + new_len, acc_len - new_len, [{new_v, new_len} | ranges]}
+      if elem(new_acc, 1) < 1 do
+        {:halt, new_acc}
+      else
+        {:cont, new_acc}
+      end
+    end)
+    |> elem(2)
+    |> Enum.reverse()
+  end
+
+  @doc """
   Transform value, by applying a range map.
 
   ## Parameters
@@ -30,6 +79,12 @@ defmodule Garden.Gmap do
     from = map.from
     to = map.from + map.length - 1
     (value >= from) && (value <= to)
+  end
+
+  defp nearest_map(maps, value) do
+    maps
+    |> Enum.filter(fn map -> map.from > value end)
+    |> Enum.min_by(fn map -> map.from end, fn -> nil end)
   end
 
   @doc """
--- advent-2023/day-05/garden/test/garden/gmap_test.exs
+++ advent-2023/day-05/garden/test/garden/gmap_test.exs
@@ -43,7 +43,7 @@ defmodule Garden.GmapTest do
         60 56 37
         56 93 4
         """,
-        exp_seed_to_soil_mappings: [
+        exp_seed_to_soil_mappings_part1: [
           {49, 49},
           {50, 52},
           {97, 99},
@@ -54,37 +54,79 @@ defmodule Garden.GmapTest do
           {55, 57},
           {13, 13},
         ],
-        exp_locations: [
+        exp_locations_part1: [
           82,
           43,
           86,
           35,
         ],
+        exp_seed_to_soil_mappings_part2: [
+          {{96, 4}, [{98, 2}, {50, 2}]},
+          # values before earliest range:
+          {{48, 2}, [{48, 2}]},
+          {{48, 4}, [{48, 2}, {52, 2}]},
+          # values after latest range:
+          {{96, 6}, [{98, 2}, {50, 2}, {100, 2}]},
+          {{100, 2}, [{100, 2}]},
+        ],
+        input_notch_part2: """
+        seed-to-soil map:
+        30 40 7
+        60 50 7
+        """,
+        notch_range_part2: {44, 9},
+        exp_notch_mappings_part2: [
+          {34, 3},
+          {47, 3},
+          {60, 3},
+        ],
       ]
     end
 
-    test "seed-to-soil mappings", fixture do
+    test "seed-to-soil mappings (part 1)", fixture do
       {_seeds, gmaps} =
         fixture.input
-        |> parse_input_string()
+        |> parse_input_string(part: 1)
       assert gmaps[:seed].from == :seed
       assert gmaps[:seed].to == :soil
-      fixture.exp_seed_to_soil_mappings
+      fixture.exp_seed_to_soil_mappings_part1
       |> Enum.each(fn {seed, exp_soil} ->
         act_soil = Gmap.transform(seed, gmaps[:seed])
         assert act_soil == exp_soil
       end)
     end
 
-    test "seed locations", fixture do
+    test "seed locations (part 1)", fixture do
       {seeds, gmaps} =
         fixture.input
-        |> parse_input_string()
+        |> parse_input_string(part: 1)
       act_locations =
         seeds
         |> Enum.map(fn seed -> elem(seed, 0) end)
         |> Enum.map(fn seed -> Gmap.location(seed, gmaps) end)
-      assert act_locations == fixture.exp_locations
+      assert act_locations == fixture.exp_locations_part1
+    end
+
+    test "seed-to-soil mappings (part 2)", fixture do
+      {_seeds, gmaps} =
+        fixture.input
+        |> parse_input_string(part: 2)
+      assert gmaps[:seed].from == :seed
+      assert gmaps[:seed].to == :soil
+      fixture.exp_seed_to_soil_mappings_part2
+      |> Enum.each(fn {{seed, length}, exp_soil} ->
+        act_soil = Gmap.transform({seed, length}, gmaps[:seed])
+        assert act_soil == exp_soil
+      end)
+    end
+
+    test "seed-to-soil mappings with notch (part 2)", fixture do
+      gmap =
+        fixture.input_notch_part2
+        |> parse_gmap_block()
+      act_mappings =
+        Gmap.transform(fixture.notch_range_part2, gmap)
+      assert act_mappings == fixture.exp_notch_mappings_part2
     end
   end
 end
