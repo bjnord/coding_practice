a28298f82a9fa6dfbc0a13ac3828883d15762516
Brent J. Nordquist <brent@nordist.net>
2023-12-06 06:17:41 -0600

    2023-06-p2: Parser

--- advent-2023/day-06/race/lib/race/parser.ex
+++ advent-2023/day-06/race/lib/race/parser.ex
@@ -8,40 +8,67 @@ defmodule Race.Parser do
 
   Returns a list of races (`{time, record}` tuples).
   """
-  def parse_input(input_file, _opts \\ []) do
+  def parse_input(input_file, opts \\ []) do
     File.read!(input_file)
-    |> parse_input_string()
+    |> parse_input_string(opts)
   end
 
   @doc ~S"""
   Parse input as a block string.
 
+  (This produces a different result depending on the puzzle part rules.
+  See "Examples".)
+
+  ## Parameters
+
+  - `input` - block string containing puzzle input
+  - `opts` - `part` keyword (1 or 2)
+
   Returns a list of races (`{time, record}` tuples).
 
   ## Examples
-      iex> parse_input_string("Time:      3  4\nDistance:  1  2\n")
+      iex> parse_input_string("Time:      3  4\nDistance:  1  2\n", part: 1)
       [{3, 1}, {4, 2}]
+      iex> parse_input_string("Time:      3  4\nDistance:  1  2\n", part: 2)
+      [{34, 12}]
   """
-  def parse_input_string(input, _opts \\ []) do
+  def parse_input_string(input, opts \\ []) do
     input
     |> String.split("\n", trim: true)
-    |> Enum.map(&parse_line/1)
+    |> Enum.map(fn line -> parse_line(line, opts) end)
     |> Enum.zip()
   end
 
   @doc ~S"""
   Parse an input line containing a label and list of numbers.
 
+  (This produces a different result depending on the puzzle part rules.
+  See "Examples".)
+
+  ## Parameters
+
+  - `line` - one line of puzzle input
+  - `opts` - `part` keyword (1 or 2)
+
   Returns a list of integers.
 
   ## Examples
-      iex> parse_line("Time:      3  4\n")
+      iex> parse_line("Time:      3  4\n", part: 1)
       [3, 4]
+      iex> parse_line("Time:      3  4\n", part: 2)
+      [34]
   """
-  def parse_line(line) do
-    line
-    |> String.split()
-    |> Enum.drop(1)
+  def parse_line(line, opts \\ []) do
+    if opts[:part] == 2 do
+      line
+      |> String.split(":")
+      |> Enum.drop(1)
+      |> Enum.map(fn numbers -> Regex.replace(~r/\s+/, numbers, "") end)
+    else
+      line
+      |> String.split()
+      |> Enum.drop(1)
+    end
     |> Enum.map(&String.to_integer/1)
   end
 end
--- advent-2023/day-06/race/test/race/parser_test.exs
+++ advent-2023/day-06/race/test/race/parser_test.exs
@@ -11,18 +11,27 @@ defmodule Race.ParserTest do
         Time:      7  15   30
         Distance:  9  40  200
         """,
-        exp_races: [
+        exp_races_part1: [
           {7, 9},
           {15, 40},
           {30, 200},
         ],
+        exp_races_part2: [
+          {71530, 940200},
+        ]
       ]
     end
 
-    test "parser gets expected races", fixture do
+    test "parser gets expected races (part 1)", fixture do
       act_races = fixture.input
-                  |> parse_input_string()
-      assert act_races == fixture.exp_races
+                  |> parse_input_string(part: 1)
+      assert act_races == fixture.exp_races_part1
+    end
+
+    test "parser gets expected races (part 2)", fixture do
+      act_races = fixture.input
+                  |> parse_input_string(part: 2)
+      assert act_races == fixture.exp_races_part2
     end
   end
 end
