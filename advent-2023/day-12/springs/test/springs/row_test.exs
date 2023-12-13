62e50f4796c771a12d6defd6f68e7ae65c420da4
Brent J. Nordquist <brent@nordist.net>
2023-12-12 20:01:32 -0600

    2023-12-p1: Add'l test cases = big trouble
    it's at this point in the story that our hero realizes he made an
    assumption, and implemented a failed strategy as a result -- time to
    throw it out and start over

--- advent-2023/day-12/springs/test/springs/row_test.exs
+++ advent-2023/day-12/springs/test/springs/row_test.exs
@@ -41,13 +41,58 @@ defmodule Springs.RowTest do
           1,
           1,
         ],
+        rows_addl: [
+          %Row{
+            clusters: ['?#', '?', '?', '##', '###?'],
+            counts: [2, 1, 2, 3],
+          },
+          %Row{
+            clusters: [~c"#???????", ~c"??", ~c"?", ~c"??", ~c"?"],
+            counts: [8, 2, 1, 1]
+          },
+          %Row{
+            clusters: [~c"?", ~c"?#?", ~c"?????", ~c"?", ~c"?"],
+            counts: [1, 2, 5, 1]
+          },
+          %Row{
+            clusters: [~c"??", ~c"?????", ~c"?", ~c"##", ~c"?"],
+            counts: [1, 1, 3, 2]
+          },
+          %Row{
+            clusters: ['?', '?', '??#?', '#', '?', '????'],
+            counts: [1, 4, 1, 1, 2],
+          },
+          %Row{
+            clusters: [~c"?", ~c"#", ~c"???", ~c"??", ~c"???"],
+            counts: [1, 3, 1, 2]
+          },
+          %Row{
+            clusters: [~c"?", ~c"##?", ~c"???", ~c"??", ~c"?"],
+            counts: [1, 3, 2, 2]
+          },
+        ],
+        exp_arrangements_addl: [
+          1 * 2 * 1 * 1,
+          1 * 1 * (3 + 2),
+          1 * 2 * 1 * 2,
+          2 * 1 * 1,  # `1, 3` must fit in `?????`
+          2 * 1 * 1 * 1 * 3,
+          1 * 1 * 2 * 2,
+          1 * 1 * 2 * 1,
+        ],
       ]
     end
 
-    test "find arrangementss", fixture do
+    test "find arrangements", fixture do
       act_arrangements = fixture.rows
                          |> Enum.map(&Row.arrangements/1)
       assert act_arrangements == fixture.exp_arrangements
+    end
+
+    test "find arrangements (add'l test cases)", fixture do
+      act_arrangements = fixture.rows_addl
+                         |> Enum.map(&Row.arrangements/1)
+      assert act_arrangements == fixture.exp_arrangements_addl
     end
   end
 end
