8c4ea21edf45e32bcf81ae2d02fb243f66405748
Brent J. Nordquist <brent@nordist.net>
2023-12-22 14:27:35 -0600

    2023-22-p2: Larger example (doubled input)

--- advent-2023/day-22/sand/test/sand/brick_test.exs
+++ advent-2023/day-22/sand/test/sand/brick_test.exs
@@ -25,6 +25,38 @@ defmodule Sand.BrickTest do
           %Brick{n: 6, from: %{x: 0, y: 1, z: 4}, to: %{x: 2, y: 1, z: 4}, supported_by: [5, 4]},
           %Brick{n: 7, from: %{x: 1, y: 1, z: 5}, to: %{x: 1, y: 1, z: 6}, supported_by: [6]},
         ],
+        double_bricks: [
+          %Brick{n:  1, from: %{x: 1, y: 0, z:  1}, to: %{x: 1, y: 2, z:  1}},
+          %Brick{n:  2, from: %{x: 0, y: 0, z:  2}, to: %{x: 2, y: 0, z:  2}},
+          %Brick{n:  3, from: %{x: 0, y: 2, z:  3}, to: %{x: 2, y: 2, z:  3}},
+          %Brick{n:  4, from: %{x: 0, y: 0, z:  4}, to: %{x: 0, y: 2, z:  4}},
+          %Brick{n:  5, from: %{x: 2, y: 0, z:  5}, to: %{x: 2, y: 2, z:  5}},
+          %Brick{n:  6, from: %{x: 0, y: 1, z:  6}, to: %{x: 2, y: 1, z:  6}},
+          %Brick{n:  7, from: %{x: 1, y: 1, z:  8}, to: %{x: 1, y: 1, z:  9}},
+          %Brick{n:  8, from: %{x: 1, y: 0, z: 11}, to: %{x: 1, y: 2, z: 11}},
+          %Brick{n:  9, from: %{x: 0, y: 0, z: 12}, to: %{x: 2, y: 0, z: 12}},
+          %Brick{n: 10, from: %{x: 0, y: 2, z: 13}, to: %{x: 2, y: 2, z: 13}},
+          %Brick{n: 11, from: %{x: 0, y: 0, z: 14}, to: %{x: 0, y: 2, z: 14}},
+          %Brick{n: 12, from: %{x: 2, y: 0, z: 15}, to: %{x: 2, y: 2, z: 15}},
+          %Brick{n: 13, from: %{x: 0, y: 1, z: 16}, to: %{x: 2, y: 1, z: 16}},
+          %Brick{n: 14, from: %{x: 1, y: 1, z: 18}, to: %{x: 1, y: 1, z: 19}},
+        ],
+        exp_dropped_double_bricks: [
+          %Brick{n:  1, from: %{x: 1, y: 0, z:  1}, to: %{x: 1, y: 2, z:  1}, supported_by: []},
+          %Brick{n:  2, from: %{x: 0, y: 0, z:  2}, to: %{x: 2, y: 0, z:  2}, supported_by: [1]},
+          %Brick{n:  3, from: %{x: 0, y: 2, z:  2}, to: %{x: 2, y: 2, z:  2}, supported_by: [1]},
+          %Brick{n:  4, from: %{x: 0, y: 0, z:  3}, to: %{x: 0, y: 2, z:  3}, supported_by: [3, 2]},
+          %Brick{n:  5, from: %{x: 2, y: 0, z:  3}, to: %{x: 2, y: 2, z:  3}, supported_by: [3, 2]},
+          %Brick{n:  6, from: %{x: 0, y: 1, z:  4}, to: %{x: 2, y: 1, z:  4}, supported_by: [5, 4]},
+          %Brick{n:  7, from: %{x: 1, y: 1, z:  5}, to: %{x: 1, y: 1, z:  6}, supported_by: [6]},
+          %Brick{n:  8, from: %{x: 1, y: 0, z:  7}, to: %{x: 1, y: 2, z:  7}, supported_by: [7]},
+          %Brick{n:  9, from: %{x: 0, y: 0, z:  8}, to: %{x: 2, y: 0, z:  8}, supported_by: [8]},
+          %Brick{n: 10, from: %{x: 0, y: 2, z:  8}, to: %{x: 2, y: 2, z:  8}, supported_by: [8]},
+          %Brick{n: 11, from: %{x: 0, y: 0, z:  9}, to: %{x: 0, y: 2, z:  9}, supported_by: [10, 9]},
+          %Brick{n: 12, from: %{x: 2, y: 0, z:  9}, to: %{x: 2, y: 2, z:  9}, supported_by: [10, 9]},
+          %Brick{n: 13, from: %{x: 0, y: 1, z: 10}, to: %{x: 2, y: 1, z: 10}, supported_by: [12, 11]},
+          %Brick{n: 14, from: %{x: 1, y: 1, z: 11}, to: %{x: 1, y: 1, z: 12}, supported_by: [13]},
+        ],
         exp_supports: %{
           1 => [3, 2],
           2 => [5, 4],
@@ -33,6 +65,22 @@ defmodule Sand.BrickTest do
           5 => [6],
           6 => [7],
           7 => [],
+        },
+        exp_double_supports: %{
+           1 => [3, 2],
+           2 => [5, 4],
+           3 => [5, 4],
+           4 => [6],
+           5 => [6],
+           6 => [7],
+           7 => [8],
+           8 => [10, 9],
+           9 => [12, 11],
+          10 => [12, 11],
+          11 => [13],
+          12 => [13],
+          13 => [14],
+          14 => [],
         },
         exp_disintegratable: [2, 3, 4, 5, 7],
       ]
@@ -45,11 +93,25 @@ defmodule Sand.BrickTest do
       assert act_dropped_bricks == fixture.exp_dropped_bricks
     end
 
+    test "find new brick positions after drop (double example)", fixture do
+      act_dropped_double_bricks =
+        fixture.double_bricks
+        |> Brick.drop()
+      assert act_dropped_double_bricks == fixture.exp_dropped_double_bricks
+    end
+
     test "find bricks supported by", fixture do
       act_supports =
         fixture.exp_dropped_bricks
         |> Brick.supports()
       assert act_supports == fixture.exp_supports
+    end
+
+    test "find bricks supported by (double example)", fixture do
+      act_double_supports =
+        fixture.exp_dropped_double_bricks
+        |> Brick.supports()
+      assert act_double_supports == fixture.exp_double_supports
     end
 
     test "find disintegratable bricks", fixture do
--- advent-2023/day-22/sand/test/sand/parser_test.exs
+++ advent-2023/day-22/sand/test/sand/parser_test.exs
@@ -26,6 +26,38 @@ defmodule Sand.ParserTest do
           %Brick{n: 6, from: %{x: 0, y: 1, z: 6}, to: %{x: 2, y: 1, z: 6}},
           %Brick{n: 7, from: %{x: 1, y: 1, z: 8}, to: %{x: 1, y: 1, z: 9}},
         ],
+        double_input: """
+        1,0,1~1,2,1
+        0,0,2~2,0,2
+        0,2,3~2,2,3
+        0,0,4~0,2,4
+        2,0,5~2,2,5
+        0,1,6~2,1,6
+        1,1,8~1,1,9
+        1,0,11~1,2,11
+        0,0,12~2,0,12
+        0,2,13~2,2,13
+        0,0,14~0,2,14
+        2,0,15~2,2,15
+        0,1,16~2,1,16
+        1,1,18~1,1,19
+        """,
+        exp_double_bricks: [
+          %Brick{n:  1, from: %{x: 1, y: 0, z:  1}, to: %{x: 1, y: 2, z:  1}},
+          %Brick{n:  2, from: %{x: 0, y: 0, z:  2}, to: %{x: 2, y: 0, z:  2}},
+          %Brick{n:  3, from: %{x: 0, y: 2, z:  3}, to: %{x: 2, y: 2, z:  3}},
+          %Brick{n:  4, from: %{x: 0, y: 0, z:  4}, to: %{x: 0, y: 2, z:  4}},
+          %Brick{n:  5, from: %{x: 2, y: 0, z:  5}, to: %{x: 2, y: 2, z:  5}},
+          %Brick{n:  6, from: %{x: 0, y: 1, z:  6}, to: %{x: 2, y: 1, z:  6}},
+          %Brick{n:  7, from: %{x: 1, y: 1, z:  8}, to: %{x: 1, y: 1, z:  9}},
+          %Brick{n:  8, from: %{x: 1, y: 0, z: 11}, to: %{x: 1, y: 2, z: 11}},
+          %Brick{n:  9, from: %{x: 0, y: 0, z: 12}, to: %{x: 2, y: 0, z: 12}},
+          %Brick{n: 10, from: %{x: 0, y: 2, z: 13}, to: %{x: 2, y: 2, z: 13}},
+          %Brick{n: 11, from: %{x: 0, y: 0, z: 14}, to: %{x: 0, y: 2, z: 14}},
+          %Brick{n: 12, from: %{x: 2, y: 0, z: 15}, to: %{x: 2, y: 2, z: 15}},
+          %Brick{n: 13, from: %{x: 0, y: 1, z: 16}, to: %{x: 2, y: 1, z: 16}},
+          %Brick{n: 14, from: %{x: 1, y: 1, z: 18}, to: %{x: 1, y: 1, z: 19}},
+        ],
       ]
     end
 
@@ -33,6 +65,12 @@ defmodule Sand.ParserTest do
       act_bricks = fixture.input
                    |> parse_input_string()
       assert act_bricks == fixture.exp_bricks
+    end
+
+    test "parser gets expected bricks (double example)", fixture do
+      act_double_bricks = fixture.double_input
+                          |> parse_input_string()
+      assert act_double_bricks == fixture.exp_double_bricks
     end
   end
 end
