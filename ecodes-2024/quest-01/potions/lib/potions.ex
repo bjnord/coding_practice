1714f4f9746552c5612de148168e591cba6c402b
Brent J. Nordquist <brent@nordist.net>
2024-11-15 08:34:03 -0600

    ecodes-2024 q01-p2: Correct answer

--- ecodes-2024/quest-01/potions/lib/potions.ex
+++ ecodes-2024/quest-01/potions/lib/potions.ex
@@ -4,16 +4,17 @@ defmodule Potions do
   """
 
   @doc """
-  Calculate number of potions required for a sequence of battles.
+  Calculate number of potions required for a sequence of battles (using
+  part 1 rules).
 
   ## Examples
 
-      iex> Potions.calc("ABC")
+      iex> Potions.calc_p1("ABC")
       4
 
   """
-  def calc(battles) do
-    to_charlist(battles)
+  def calc_p1(creatures) do
+    to_charlist(creatures)
     |> Enum.map(&battle_potions/1)
     |> Enum.sum()
   end
@@ -23,13 +24,44 @@ defmodule Potions do
       ?A -> 0
       ?B -> 1
       ?C -> 3
-      _  -> raise "unknown battle #{c}"
+      ?D -> 5
+      _  -> raise "unknown creature #{c}"
     end
   end
 
+  @doc """
+  Calculate number of potions required for a sequence of battles (using
+  part 2 rules).
+
+  ## Examples
+
+      iex> Potions.calc_p2("ABCD")
+      13
+
+  """
+  def calc_p2(creatures) do
+    to_charlist(creatures)
+    |> Enum.chunk_every(2)
+    |> Enum.map(&pair_strength/1)
+    |> Enum.sum()
+  end
+
+  def pair_strength([?x, ?x]), do: 0
+  def pair_strength([m, ?x]), do: battle_potions(m)
+  def pair_strength([?x, m]), do: battle_potions(m)
+  def pair_strength([m1, m2]) do
+    battle_potions(m1) + 1 + battle_potions(m2) + 1
+  end
+
   def main(argv) do
-    potions = File.read!(hd(argv))
-              |> calc()
+    [p1_file | [p2_file | _]] = argv
+    potions = p1_file
+              |> File.read!()
+              |> calc_p1()
     IO.puts("Part 1: #{potions} potions")
+    potions = p2_file
+              |> File.read!()
+              |> calc_p2()
+    IO.puts("Part 2: #{potions} potions")
   end
 end
--- ecodes-2024/quest-01/potions/test/potions_test.exs
+++ ecodes-2024/quest-01/potions/test/potions_test.exs
@@ -2,7 +2,17 @@ defmodule PotionsTest do
   use ExUnit.Case
   doctest Potions
 
-  test "puzzle example" do
-    assert Potions.calc("ABBAC") == 5
+  test "part 1 example" do
+    assert Potions.calc_p1("ABBAC") == 5
+  end
+
+  test "part 2 example" do
+    assert Potions.calc_p2("AxBCDDCAxD") == 28
+  end
+
+  # (the puzzle description doesn't cover this case, but it appears in
+  # the part 2 input file)
+  test "part 2 with no creatures" do
+    assert Potions.calc_p2("xx") == 0
   end
 end
