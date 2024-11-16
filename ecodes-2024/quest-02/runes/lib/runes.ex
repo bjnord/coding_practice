53a554eb9a2c34f3a63cfb9dabe32f64695024c3
Brent J. Nordquist <brent@nordist.net>
2024-11-16 06:51:11 -0600

    ecodes-2024 q02-p1: Correct answer

--- ecodes-2024/quest-02/runes/lib/runes.ex
+++ ecodes-2024/quest-02/runes/lib/runes.ex
@@ -2,4 +2,46 @@ defmodule Runes do
   @moduledoc """
   Documentation for `Runes`.
   """
+
+  import Kingdom.CLI
+  import Runes.Parser
+
+  @doc """
+  Find matching rune word at beginning of inscription.
+  """
+  def begin_match?(words, chars) do
+    words
+    |> Enum.find(fn word ->
+      word_ch = to_charlist(word)
+      Enum.slice(chars, 0, length(word_ch)) == word_ch
+    end)
+  end
+
+  @doc """
+  Find count of matching rune words in inscription.
+  """
+  def match_count(words, inscription) do
+    i_chars = to_charlist(inscription)
+    match_count(words, i_chars, 0)
+  end
+
+  defp match_count(_words, [], count), do: count
+  defp match_count(words, i_chars, count) do
+    case begin_match?(words, i_chars) do
+      nil  -> match_count(words, tl(i_chars), count)
+      _    -> match_count(words, tl(i_chars), count + 1)
+    end
+  end
+
+  def main(argv) do
+    _opts = parse_args(argv)
+    [1]  # TODO `opts[:parts]`
+    |> Enum.each(fn part ->
+      {words, inscription} =
+        "private/everybody_codes_e2024_q02_p#{part}.txt"
+        |> parse_input()
+      match_count(words, inscription)
+      |> IO.inspect(label: "Part #{part}")
+    end)
+  end
 end
--- ecodes-2024/quest-02/runes/test/runes_test.exs
+++ ecodes-2024/quest-02/runes/test/runes_test.exs
@@ -1,4 +1,75 @@
 defmodule RunesTest do
   use ExUnit.Case
   doctest Runes
+
+  describe "part 1 examples" do
+    setup do
+      [
+        part1_words: ["THE","OWE","MES","ROD","HER"],
+        part1_inscription1: "AWAKEN THE POWER ADORNED WITH THE FLAMES BRIGHT IRE",
+        part1_exp_match1a: nil,
+        part1_exp_match1b: "THE",
+        part1_exp_match1c: "OWE",
+        part1_exp_count1: 4,
+        part1_inscription2: "THE FLAME SHIELDED THE HEART OF THE KINGS",
+        part1_exp_count2: 3,
+        part1_inscription3: "POWE PO WER P OWE R",
+        part1_exp_count3: 2,
+        part1_inscription4: "THERE IS THE END",
+        part1_exp_count4: 3,
+      ]
+    end
+
+    test "part 1 rune word match example 1a", fixture do
+      chars1 = fixture.part1_inscription1
+               |> to_charlist()
+      act_match1a = fixture.part1_words
+                    |> Runes.begin_match?(chars1)
+      assert act_match1a == fixture.part1_exp_match1a
+    end
+
+    test "part 1 rune word match example 1b", fixture do
+      chars1 = fixture.part1_inscription1
+               |> to_charlist()
+               |> Enum.slice(7, 999)
+      act_match1b = fixture.part1_words
+                    |> Runes.begin_match?(chars1)
+      assert act_match1b == fixture.part1_exp_match1b
+    end
+
+    test "part 1 rune word match example 1c", fixture do
+      chars1 = fixture.part1_inscription1
+               |> to_charlist()
+               |> Enum.slice(12, 999)
+      act_match1c = fixture.part1_words
+                    |> Runes.begin_match?(chars1)
+      assert act_match1c == fixture.part1_exp_match1c
+    end
+
+    # TODO RF use array for the 4 examples
+
+    test "part 1 rune word count example 1", fixture do
+      act_count1 = fixture.part1_words
+                   |> Runes.match_count(fixture.part1_inscription1)
+      assert act_count1 == fixture.part1_exp_count1
+    end
+
+    test "part 1 rune word count example 2", fixture do
+      act_count2 = fixture.part1_words
+                   |> Runes.match_count(fixture.part1_inscription2)
+      assert act_count2 == fixture.part1_exp_count2
+    end
+
+    test "part 1 rune word count example 3", fixture do
+      act_count3 = fixture.part1_words
+                   |> Runes.match_count(fixture.part1_inscription3)
+      assert act_count3 == fixture.part1_exp_count3
+    end
+
+    test "part 1 rune word count example 4", fixture do
+      act_count4 = fixture.part1_words
+                   |> Runes.match_count(fixture.part1_inscription4)
+      assert act_count4 == fixture.part1_exp_count4
+    end
+  end
 end
