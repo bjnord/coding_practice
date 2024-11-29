39ba714ef7a874ffe47ee5afbfae4c64c5939a64
Brent J. Nordquist <brent@nordist.net>
2024-11-29 09:42:12 -0600

    ecodes-2024: Kingdom.Grid

---
+++ ecodes-2024/kingdom/lib/kingdom/grid.ex
@@ -0,0 +1,59 @@
+defmodule Kingdom.Grid do
+  @moduledoc """
+  Grid structure and functions for `Kingdom`.
+  """
+
+  defstruct size: %{y: 0, x: 0}, squares: %{}
+
+  @type t :: %__MODULE__{
+    size: %{y: integer(), x: integer()},
+    squares: %{{integer(), integer()} => any()}
+  }
+
+  alias Kingdom.Grid
+
+  @doc ~S"""
+  Form a `Grid` from a list of squares.
+
+  ## Parameters
+
+  - `square_list` - a list of `{{y, x}, value}` squares
+
+  Returns a `Grid`.
+  """
+  def from_squares(square_list) do
+    squares = Enum.into(square_list, %{})
+    {dim_y, dim_x} = dimensions(square_list)
+    %Grid{
+      size: %{y: dim_y, x: dim_x},
+      squares: squares,
+    }
+  end
+
+  defp dimensions(square_list) do
+    positions = Enum.map(square_list, &(elem(&1, 0)))
+    {
+      elem(Enum.max_by(positions, fn {y, _x} -> y end), 0) + 1,
+      elem(Enum.max_by(positions, fn {_y, x} -> x end), 1) + 1,
+    }
+  end
+
+  def cardinals_of(grid, {y, x}) do
+    [:north, :east, :south, :west]
+    |> Enum.map(fn dir -> delta_pos({y, x}, delta(dir)) end)
+    |> Enum.reject(fn {ny, nx} ->
+      out_of_bounds?(ny, grid.size.y) || out_of_bounds?(nx, grid.size.x)
+    end)
+  end
+
+  defp delta(:north), do: {-1, 0}
+  defp delta(:east), do: {0, 1}
+  defp delta(:south), do: {1, 0}
+  defp delta(:west), do: {0, -1}
+
+  defp delta_pos({y, x}, {dy, dx}), do: {y + dy, x + dx}
+
+  defp out_of_bounds?(n, _max) when (n < 0), do: true
+  defp out_of_bounds?(n, max) when (n >= max), do: true
+  defp out_of_bounds?(_n, _max), do: false
+end
---
+++ ecodes-2024/kingdom/test/kingdom/grid_test.exs
@@ -0,0 +1,75 @@
+defmodule Kingdom.GridTest do
+  use ExUnit.Case
+  doctest Kingdom.Grid, import: true
+
+  alias Kingdom.Grid
+
+  describe "examples" do
+    setup do
+      [
+        squares_1: [
+          {{0, 0}, 0},
+          {{0, 1}, 0},
+          {{0, 2}, 1},
+          {{0, 3}, 2},
+          {{1, 0}, 3},
+          {{1, 1}, 3},
+          {{1, 2}, 1},
+          {{1, 3}, 1},
+          {{2, 0}, 4},
+          {{2, 1}, 3},
+          {{2, 2}, 0},
+          {{2, 3}, 0},
+        ],
+        exp_grid_1: %Grid{
+          size: %{y: 3, x: 4},
+          squares: %{
+            {0, 0} => 0,
+            {0, 1} => 0,
+            {0, 2} => 1,
+            {0, 3} => 2,
+            {1, 0} => 3,
+            {1, 1} => 3,
+            {1, 2} => 1,
+            {1, 3} => 1,
+            {2, 0} => 4,
+            {2, 1} => 3,
+            {2, 2} => 0,
+            {2, 3} => 0,
+          }
+        },
+        positions_1: [
+          {0, 0},
+          {0, 1},
+          {1, 0},
+          {1, 1},
+          {1, 3},
+          {2, 2},
+          {2, 3},
+        ],
+        exp_cardinals_1: [
+          [{0, 1}, {1, 0}],
+          [{0, 2}, {1, 1}, {0, 0}],
+          [{0, 0}, {1, 1}, {2, 0}],
+          [{0, 1}, {1, 2}, {2, 1}, {1, 0}],
+          [{0, 3}, {2, 3}, {1, 2}],
+          [{1, 2}, {2, 3}, {2, 1}],
+          [{1, 3}, {2, 2}],
+        ],
+      ]
+    end
+
+    test "produce correct grids", fixture do
+      act_grid = Grid.from_squares(fixture.squares_1)
+      assert act_grid == fixture.exp_grid_1
+    end
+
+    test "produce correct cardinals", fixture do
+      grid = Grid.from_squares(fixture.squares_1)
+      act_cardinals =
+        fixture.positions_1
+        |> Enum.map(&(Grid.cardinals_of(grid, &1)))
+      assert act_cardinals == fixture.exp_cardinals_1
+    end
+  end
+end
