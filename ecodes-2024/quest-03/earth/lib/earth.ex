2d6d166a8f12c8cd8e4065a91b28720e401c6147
Brent J. Nordquist <brent@nordist.net>
2024-12-01 14:45:27 -0600

    ecodes-2024 q03-p1: Diggable square test

--- ecodes-2024/quest-03/earth/lib/earth.ex
+++ ecodes-2024/quest-03/earth/lib/earth.ex
@@ -2,4 +2,34 @@ defmodule Earth do
   @moduledoc """
   Documentation for `Earth`.
   """
+
+  # alias Kingdom.Grid
+  #
+  # def dig(grid), do: dig(Grid.keys(grid), grid)
+  #
+  # def dig([], grid), do: grid
+  # def dig(squares, grid) do
+  #   squares = diggable_squares(squares, grid)
+  #   grid = Enum.reduce(squares, grid, &(dig_square(&1, grid)))
+  #   dig(squares, grid)
+  # end
+  #
+  # defp diggable_squares(squares, grid) do
+  #   Enum.filter(squares, fn square ->
+  #     square_depth = Grid.get(grid, square)
+  #     neighbor_depths = Grid.cardinals_of(grid, square)
+  #                       |> Enum.map(&(Grid.get(grid, &1)))
+  #     diggable_square?(square_depth, neighbor_depths)
+  # end
+  #
+  # defp dig_square(square, grid) do
+  #   Grid.get_and_update(grid, square, &(&1 + 1))
+  #   |> elem(1)
+  # end
+
+  def diggable_square?(0, _neighbor_depths), do: false
+  def diggable_square?(depth, neighbor_depths) do
+    neighbor_depths
+    |> Enum.all?(&(&1 >= depth))
+  end
 end
--- ecodes-2024/quest-03/earth/test/earth_test.exs
+++ ecodes-2024/quest-03/earth/test/earth_test.exs
@@ -1,4 +1,53 @@
 defmodule EarthTest do
   use ExUnit.Case
   doctest Earth
+
+  import Earth
+
+  describe "examples" do
+    setup do
+      [
+        diggable_squares: [
+          {
+            {0, [0, 0]},
+            false,
+          },
+          {
+            {0, [1, 1, 1, 0]},
+            false,
+          },
+          {
+            {1, [0, 1, 1, 1]},
+            false,
+          },
+          {
+            {1, [1, 1, 1, 1]},
+            true,
+          },
+          {
+            {1, [1, 1, 1, 2]},
+            true,
+          },
+          {
+            {2, [1, 2, 2, 1]},
+            false,
+          },
+          {
+            {2, [2, 2, 2, 2]},
+            true,
+          },
+        ]
+      ]
+    end
+
+    test "detects correct diggable squares", fixture do
+      act_diggable_squares =
+        fixture.diggable_squares
+        |> Enum.map(fn {{depth, neighbor_depths}, _is} ->
+          is = diggable_square?(depth, neighbor_depths)
+          {{depth, neighbor_depths}, is}
+        end)
+      assert act_diggable_squares == fixture.diggable_squares
+    end
+  end
 end
