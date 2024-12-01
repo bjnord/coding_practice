48284198ce4f15b3f349bb4915ccba5cd8bbc02a
Brent J. Nordquist <brent@nordist.net>
2024-12-01 09:21:25 -0600

    aoc-2024 01: RF: Simplify parser
    and fix ExDoc

--- advent-2024/day-01/location/lib/location/parser.ex
+++ advent-2024/day-01/location/lib/location/parser.ex
@@ -6,7 +6,7 @@ defmodule Location.Parser do
   @doc ~S"""
   Parse the input file.
 
-  Returns a list of charlists (one per line).
+  Returns a list of tuples (corresponding values from each location list).
   """
   def parse_input(input_file, _opts \\ []) do
     input_file
@@ -17,33 +17,36 @@ defmodule Location.Parser do
   @doc ~S"""
   Parse input as a block string.
 
-  Returns a list of charlists (one per line).
+  Returns a list of tuples (corresponding values from each location list).
 
   ## Examples
-      iex> parse_input_string("5   7\n11  9\n") |> Enum.to_list()
+      iex> parse_input_string("5   7\n 11  9 \n")
       [{5, 7}, {11, 9}]
   """
   def parse_input_string(input, _opts \\ []) do
     input
     |> String.splitter("\n", trim: true)
-    |> Stream.map(&parse_line/1)
+    |> Enum.map(&parse_line/1)
   end
 
   @doc ~S"""
-  Parse an input line containing a navigation subsystem entry.
+  Parse an input line.
 
-  Returns a charlist.
+  ## Parameters
+
+  - `line`: input line containing a pair of location values
+
+  Returns a tuple with the two location values.
 
   ## Examples
       iex> parse_line("5   7\n")
       {5, 7}
-      iex> parse_line("11  9\n")
+      iex> parse_line(" 11  9 \n")
       {11, 9}
   """
   def parse_line(line) do
     line
-    |> String.trim_trailing()
-    |> String.split(~r{\s+})
+    |> String.split()
     |> Enum.map(&String.to_integer/1)
     |> List.to_tuple()
   end
