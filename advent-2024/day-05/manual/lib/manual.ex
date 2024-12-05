7228f5817f150c1b922383748429c559110d2f93
Brent J. Nordquist <brent@nordist.net>
2024-12-05 01:00:37 -0600

    aoc-2024 05-p1: Correct answer

--- advent-2024/day-05/manual/lib/manual.ex
+++ advent-2024/day-05/manual/lib/manual.ex
@@ -6,6 +6,47 @@ defmodule Manual do
   import Manual.Parser
   import Snow.CLI
 
+  def correct_order?(pages, rules) do
+    correct_order?(pages, [], rules)
+  end
+
+  defp correct_order?([], _seen, _rules), do: true
+  defp correct_order?([page | pages], seen, rules) do
+    bad_order =
+      Map.get(rules, page, [])
+      |> Enum.reduce(false, fn later_page, failed ->
+        if Enum.find(seen, &(&1 == later_page)) do
+          true
+        else
+          failed
+        end
+      end)
+    if bad_order do
+      false
+    else
+      correct_order?(pages, [page | seen], rules)
+    end
+  end
+
+  @doc """
+  Find the middle page of a page set.
+
+  # TODO test even page set length (raises exception)
+
+  ## Examples
+      iex> Manual.middle_page([2, 3, 4, 1, 5])
+      4
+      iex> Manual.middle_page([6, 8, 7])
+      8
+  """
+  def middle_page(pages) do
+    if rem(length(pages), 2) == 1 do
+      Enum.at(pages, div(length(pages), 2))
+    else
+      raise "even page set length"
+    end
+  end
+
   @doc """
   Parse arguments and call puzzle part methods.
 
@@ -23,8 +64,11 @@ defmodule Manual do
   Process input file and display part 1 solution.
   """
   def part1(input_path) do
-    {_rules, _page_sets} = parse_input_file(input_path)
-    nil  # TODO
+    {rules, page_sets} = parse_input_file(input_path)
+    page_sets
+    |> Enum.filter(&(Manual.correct_order?(&1, rules)))
+    |> Enum.map(&Manual.middle_page/1)
+    |> Enum.sum()
     |> IO.inspect(label: "Part 1 answer is")
   end
 
--- advent-2024/day-05/manual/test/manual_test.exs
+++ advent-2024/day-05/manual/test/manual_test.exs
@@ -1,4 +1,55 @@
 defmodule ManualTest do
   use ExUnit.Case
   doctest Manual
+
+  describe "puzzle example" do
+    setup do
+      [
+        rules: %{
+          29 => [13],
+          53 => [13, 29],
+          61 => [29, 53, 13],
+          47 => [29, 61, 13, 53],
+          75 => [13, 61, 47, 53, 29],
+          97 => [75, 53, 29, 47, 61, 13],
+        },
+        page_sets: [
+          [75, 47, 61, 53, 29],
+          [97, 61, 53, 29, 13],
+          [75, 29, 13],
+          [75, 97, 47, 61, 53],
+          [61, 13, 29],
+          [97, 13, 75, 29, 47],
+        ],
+        exp_corrects: [
+          true,
+          true,
+          true,
+          false,
+          false,
+          false,
+        ],
+        exp_middles: [
+          61,
+          53,
+          29,
+        ],
+      ]
+    end
+
+    test "determine page sets with correct order", fixture do
+      act_corrects =
+        fixture.page_sets
+        |> Enum.map(&(Manual.correct_order?(&1, fixture.rules)))
+      assert act_corrects == fixture.exp_corrects
+    end
+
+    test "determine middle page of page sets with correct order", fixture do
+      act_middles =
+        fixture.page_sets
+        |> Enum.filter(&(Manual.correct_order?(&1, fixture.rules)))
+        |> Enum.map(&Manual.middle_page/1)
+      assert act_middles == fixture.exp_middles
+    end
+  end
 end
