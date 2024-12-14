055677ce80130ec711f0d663b7514702556b3982
Brent J. Nordquist <brent@nordist.net>
2024-12-14 12:57:35 -0600

    aoc-2024 14-p1: Problem with stepper / mod?

--- advent-2024/day-14/robot/lib/robot.ex
+++ advent-2024/day-14/robot/lib/robot.ex
@@ -6,6 +6,75 @@ defmodule Robot do
   import Robot.Parser
   import History.CLI
 
+  @doc ~S"""
+  Calculate robot's position after a number of seconds.
+
+  ## Examples
+      iex> location_after({{4, 2}, {-3, 2}}, {7, 11}, 5)
+      {3, 1}
+  """
+  def location_after({{y, x}, {dy, dx}}, {dim_y, dim_x}, secs) do
+    {
+      neg_mod(y + dy * secs, dim_y),
+      neg_mod(x + dx * secs, dim_x),
+    }
+  end
+
+  defp neg_mod(n, m) when n > 0, do: rem(n, m);
+  defp neg_mod(n, m) when n < 0, do: rem(n, m) + m;
+  defp neg_mod(0, _m), do: 0
+
+  def quadrant_count(robots, dim) do
+    robots
+    |> Enum.reduce(%{}, fn pos, acc ->
+      Map.update(acc, pos, 1, &(&1 + 1))
+    end)
+    |> Enum.map(fn {pos, n} ->
+      # TODO extract
+      q = quadrant_of(pos, dim)
+      if q do
+        {q, n}
+      else
+        nil
+      end
+    end)
+    |> Enum.reject(&(&1 == nil))
+    |> dbg()
+    # TODO tally them into 4-length list
+  end
+
+  @doc ~S"""
+  Calculate robot's quadrant.
+
+  ## Examples
+      iex> quadrant_of({4, 2}, {7, 11})
+      3
+      iex> quadrant_of({3, 2}, {7, 11})
+      nil
+      iex> quadrant_of({2, 2}, {7, 11})
+      1
+      iex> quadrant_of({4, 6}, {7, 11})
+      4
+      iex> quadrant_of({2, 6}, {7, 11})
+      2
+  """
+  def quadrant_of({y, x}, {dim_y, dim_x}) do
+    cond do
+      y == div(dim_y - 1, 2) ->
+        nil
+      x == div(dim_x - 1, 2) ->
+        nil
+      y < div(dim_y - 1, 2) && x < div(dim_x - 1, 2) ->
+        1
+      y < div(dim_y - 1, 2) && x > div(dim_x - 1, 2) ->
+        2
+      y > div(dim_y - 1, 2) && x < div(dim_x - 1, 2) ->
+        3
+      y > div(dim_y - 1, 2) && x > div(dim_x - 1, 2) ->
+        4
+    end
+  end
+
   @doc """
   Parse arguments and call puzzle part methods.
 
--- advent-2024/day-14/robot/test/robot_test.exs
+++ advent-2024/day-14/robot/test/robot_test.exs
@@ -1,4 +1,35 @@
 defmodule RobotTest do
   use ExUnit.Case
-  doctest Robot
+  doctest Robot, import: true
+
+  describe "puzzle example" do
+    setup do
+      [
+        robots: [
+          {{4, 0}, {-3, 3}},
+          {{3, 6}, {-3, -1}},
+          {{3, 10}, {2, -1}},
+          {{0, 2}, {-1, 2}},
+          {{0, 0}, {3, 1}},
+          {{0, 3}, {-2, -2}},
+          {{6, 7}, {-3, -1}},
+          {{0, 3}, {-2, -1}},
+          {{3, 9}, {3, 2}},
+          {{3, 7}, {2, -1}},
+          {{4, 2}, {-3, 2}},
+          {{5, 9}, {-3, -3}},
+        ],
+        small_dim: {7, 11},
+        exp_quadrant_count: [1, 3, 4, 1],
+      ]
+    end
+
+    test "find quadrant count after 100 seconds", fixture do
+      act_quadrant_count =
+        fixture.robots
+        |> Enum.map(&(Robot.location_after(&1, fixture.small_dim, 100)))
+        |> Robot.quadrant_count(fixture.small_dim)
+      assert act_quadrant_count == fixture.exp_quadrant_count
+    end
+  end
 end
