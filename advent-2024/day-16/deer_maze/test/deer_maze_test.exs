2327578ebd1f3c1d7b4170a30d1773e5abd5fb6d
Brent J. Nordquist <brent@nordist.net>
2024-12-16 07:12:29 -0600

    aoc-2024 16-p1: Graph, but need DAG

--- advent-2024/day-16/deer_maze/test/deer_maze_test.exs
+++ advent-2024/day-16/deer_maze/test/deer_maze_test.exs
@@ -44,19 +44,31 @@ defmodule DeerMazeTest do
           #S#.............#
           #################
           """,
+          """
+          #######
+          #....E#
+          ###.#.#
+          #.....#
+          #.#.#.#
+          #S..#.#
+          #######
+          """,
         ],
         exp_scores: [
           7036,
           11048,
+          2008,
         ],
       ]
     end
 
     test "produces correct scores", fixture do
       act_scores = fixture.inputs
+                   |> Enum.slice(2..2)  # FIXME DEBUG TEMP
                    |> Enum.map(&parse_input_string/1)
                    |> Enum.map(&DeerMaze.score/1)
       assert act_scores == fixture.exp_scores
+                           |> Enum.slice(2..2)  # FIXME DEBUG TEMP
     end
   end
 end
