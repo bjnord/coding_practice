bf0e4f837ce828c75265e98f8989dbd17ca1eba3
Brent J. Nordquist <brent@nordist.net>
2024-12-17 06:30:27 -0600

    aoc-2024 17-p1: Framework; implement bst

--- advent-2024/day-17/computer/lib/computer.ex
+++ advent-2024/day-17/computer/lib/computer.ex
@@ -6,6 +6,140 @@ defmodule Computer do
   import Computer.Parser
   import History.CLI
 
+  def run({registers, program}) do
+    exec({registers, program}, 0, [], next_ops(program, 0))
+  end
+
+  defp next_ops(program, pc) do
+    if Map.get(program, pc) == nil do
+      []
+    else
+      Map.take(program, [pc, pc+1])
+      |> Map.to_list()
+      |> Enum.sort()
+      |> Enum.map(fn {_k, v} -> v end)
+    end
+  end
+
+  # If the computer tries to read an opcode past the end of the program, it instead halts.
+  defp exec({registers, _program}, _pc, output, []), do: {registers, output}
+  defp exec({registers, program}, pc, output, [op1, op2]) do
+    {registers, output} =
+      case op1 do
+        2 -> bst({registers, output}, op1, op2)
+      end
+    exec({registers, program}, pc + 2, output, next_ops(program, pc + 2))
+  end
+
+  @doc """
+  The value of a combo operand can be found as follows:
+
+  - Combo operands 0 through 3 represent literal values 0 through 3.
+  - Combo operand 4 represents the value of register A.
+  - Combo operand 5 represents the value of register B.
+  - Combo operand 6 represents the value of register C.
+  - Combo operand 7 is reserved and will not appear in valid programs.
+  """
+  def combo({a, b, c}, op) do
+    cond do
+      op >= 0 && op <= 3 -> op
+      op == 4            -> a
+      op == 5            -> b
+      op == 6            -> c
+    end
+  end
+
+  @doc """
+  The `adv` instruction (opcode `0`) performs **division**. The numerator
+  is the value in the `A` register. The denominator is found by raising
+  2 to the power of the instruction's **combo** operand. (So, an operand
+  of `2` would divide `A` by `4` (`2^2`); an operand of `5` would divide
+  `A` by `2^B`.) The result of the division operation is **truncated**
+  to an integer and then written to the `A` register.
+  """
+  def adv({{a, b, c}, output}, _op1, _op2) do
+    # TODO
+    {{a, b, c}, output}
+  end
+
+  @doc """
+  The `bxl` instruction (opcode `1`) calculates the
+  [bitwise XOR](https://en.wikipedia.org/wiki/Bitwise_operation#XOR)
+  of register `B` and the instruction's **literal** operand, then stores
+  the result in register `B`.
+  """
+  def bxl({{a, b, c}, output}, _op1, _op2) do
+    # TODO
+    {{a, b, c}, output}
+  end
+
+  @doc """
+  The `bst` instruction (opcode `2`) calculates the value of its
+  **combo** operand
+  [modulo](https://en.wikipedia.org/wiki/Modulo)
+  8 (thereby keeping only its lowest 3 bits), then writes that value to
+  the `B` register.
+  """
+  def bst({{a, b, c}, output}, _op1, op2) do
+    b = rem(combo({a, b, c}, op2), 8)
+    {{a, b, c}, output}
+  end
+
+  @doc """
+  The `jnz` instruction (opcode `3`) does **nothing** if the `A` register
+  is `0`. However, if the `A` register is **not zero**, it **jumps**\*
+  by setting the instruction pointer to the value of its **literal**
+  operand; if this instruction jumps, the instruction pointer is **not**
+  increased by `2` after this instruction.
+
+  \* The instruction does this using a little trampoline.
+  """
+  def jnz({{a, b, c}, output}, _op1, _op2) do
+    # TODO
+    {{a, b, c}, output}
+  end
+
+  @doc """
+  The `bxc` instruction (opcode `4`) calculates the **bitwise XOR**
+  of register `B` and register `C`, then stores the result in register
+  `B`. (For legacy reasons, this instruction reads an operand but
+  **ignores** it.)
+  """
+  def bxc({{a, b, c}, output}, _op1, _op2) do
+    # TODO
+    {{a, b, c}, output}
+  end
+
+  @doc """
+  The `out` instruction (opcode `5`) calculates the value of its **combo**
+  operand modulo 8, then **outputs** that value. (If a program outputs
+  multiple values, they are separated by commas.)
+  """
+  def out({{a, b, c}, output}, _op1, _op2) do
+    # TODO
+    {{a, b, c}, output}
+  end
+
+  @doc """
+  The `bdv` instruction (opcode `6`) works exactly like the
+  `adv` instruction except that the result is stored in the **`B`
+  register**. (The numerator is still read from the `A` register.)
+  """
+  def bdv({{a, b, c}, output}, _op1, _op2) do
+    # TODO
+    {{a, b, c}, output}
+  end
+
+  @doc """
+  The `cdv` instruction (opcode `7`) works exactly like the
+  `adv` instruction except that the result is stored in the **`C`
+  register**. (The numerator is still read from the `A` register.)
+  """
+  def cdv({{a, b, c}, output}, _op1, _op2) do
+    # TODO
+    {{a, b, c}, output}
+  end
+
   @doc """
   Parse arguments and call puzzle part methods.
 
--- advent-2024/day-17/computer/test/computer_test.exs
+++ advent-2024/day-17/computer/test/computer_test.exs
@@ -1,4 +1,53 @@
 defmodule ComputerTest do
   use ExUnit.Case
   doctest Computer
+
+  # TODO some edge-case tests
+  #      - empty program should halt instantly, return same registers + empty output
+  #      - combo op 7 should raise exception
+
+  describe "puzzle example" do
+    setup do
+      [
+        examples: [
+          # If register C contains 9, the program 2,6 would set register B to 1.
+          {
+            {{0, 0, 9}, %{0 => 2, 1 => 6}},
+            %{registers: {nil, 1, nil}},
+          },
+          # If register A contains 10, the program 5,0,5,1,5,4 would output 0,1,2.
+          # If register A contains 2024, the program 0,1,5,4,3,0 would output 4,2,5,6,7,7,7,7,3,1,0 and leave 0 in register A.
+          # If register B contains 29, the program 1,7 would set register B to 26.
+          # If register B contains 2024 and register C contains 43690, the program 4,0 would set register B to 44354.
+        ],
+        computer: {
+          {729, 0, 0},
+          %{0 => 0, 1 => 1, 2 => 5, 3 => 4, 4 => 3, 5 => 0},
+        },
+      ]
+    end
+
+    def assert_matches({act_registers, act_output}, expected) do
+      exp_registers = Map.get(expected, :registers)
+      if exp_registers do
+        0..2
+        |> Enum.each(fn i ->
+          if elem(exp_registers, i) != nil do
+            assert {?A + i, elem(act_registers, i)} == {?A + i, elem(exp_registers, i)}
+          end
+        end)
+      end
+      exp_output = Map.get(expected, :output)
+      if exp_output do
+        assert act_output == exp_output
+      end
+    end
+
+    test "examples produce correct behavior", fixture do
+      fixture.examples
+      |> Enum.map(fn {computer, exp_result} ->
+        assert_matches(Computer.run(computer), exp_result)
+      end)
+    end
+  end
 end
