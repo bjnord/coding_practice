edd36b9175ce921a751dceb5a3ed91c4b99ab18f
Brent J. Nordquist <brent@nordist.net>
2024-12-18 06:34:06 -0600

    aoc-2024 16-p1: Start over

--- advent-2024/day-16/deer_maze/lib/deer_maze/graph.ex
+++ advent-2024/day-16/deer_maze/lib/deer_maze/graph.ex
@@ -27,67 +27,14 @@ defmodule DeerMaze.Graph do
   @spec from_grid(Grid.t()) :: Graph.t()
   def from_grid(grid) do
     %Graph{
-      nodes: recursive_walk(grid, [{grid.meta.start, :east}], %{}),
+      nodes: walk(grid, [{grid.meta.start, :east}], %{}),
       meta: grid.meta,
     }
   end
 
-  # TODO need to eliminate loops (DAG)
-  defp recursive_walk(_grid, [], acc_nodes), do: acc_nodes
-  defp recursive_walk(grid, [{pos, dir} | rem_nodes], acc_nodes) do
-    new_walks = walk_from_node(grid, {pos, dir})
-    unseen_walks =
-      new_walks
-      |> Enum.reject(fn {pos, dir, _cost} ->
-        Map.has_key?(acc_nodes, {pos, dir})
-      end)
-      # TODO do we need to reduce multiple A->B to lowest-cost one?
-    if length(unseen_walks) == 0 do
-      recursive_walk(grid, rem_nodes, acc_nodes)
-    else
-      unseen_nodes =
-        unseen_walks
-        |> Enum.map(fn {pos, dir, _cost} -> {pos, dir} end)
-      recursive_walk(grid, rem_nodes ++ unseen_nodes, Map.put(acc_nodes, {pos, dir}, new_walks))
-    end
-  end
-
-  defp walk_from_node(grid, {pos, dir}) do
-    branches(grid, {pos, dir}, %{})
-    |> Enum.map(fn branch ->
-      walk(grid, branch, %{pos => true}, 0)
-    end)
-    |> Enum.reject(&(&1 == nil))
-  end
-
-  defp walk(grid, {pos, _dir, cost}, _seen, acc_cost) when pos == grid.meta.end, do: {pos, :end, acc_cost + cost}
-  defp walk(grid, {pos, dir, cost}, seen, acc_cost) do
-    choices = branches(grid, {pos, dir}, seen)
-    case length(choices) do
-      0 ->
-        nil
-      1 ->
-        walk(grid, hd(choices), Map.put(seen, pos, true), acc_cost + cost)
-      _ ->
-        {pos, dir, acc_cost + cost}
-    end
-  end
-
-  defp branches(grid, {pos, dir}, seen) do
-    Grid.cardinals_of(grid, pos)
-    |> Enum.reject(fn npos ->
-      (Grid.get(grid, npos) == ?#) || Map.get(seen, npos)
-    end)
-    |> Enum.map(&(branch_to_choice(pos, dir, &1)))
-  end
-
-  defp branch_to_choice(pos, dir, npos) do
-    ndir = dir_of(pos, npos)
-    if dir == ndir do
-      {npos, dir, 1}
-    else
-      {npos, ndir, 1001}
-    end
+  defp walk(_grid, [], acc_nodes), do: acc_nodes
+  defp walk(grid, [{pos, dir} | rem_nodes], acc_nodes) do
+    acc_nodes
   end
 
   defp dir_of({y, x}, {ny, nx}) do
