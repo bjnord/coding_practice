917f7069b639ccf2a69cdf898ce45d12250b9cab
Brent J. Nordquist <brent@nordist.net>
2024-12-18 20:44:13 -0600

    aoc-2024 16-p1: First part of rewrite (junctions)

--- advent-2024/day-16/deer_maze/lib/deer_maze/graph.ex
+++ advent-2024/day-16/deer_maze/lib/deer_maze/graph.ex
@@ -27,14 +27,78 @@ defmodule DeerMaze.Graph do
   @spec from_grid(Grid.t()) :: Graph.t()
   def from_grid(grid) do
     %Graph{
-      nodes: walk(grid, [{grid.meta.start, :east}], %{}),
+      nodes: walk_junctions(grid, [{grid.meta.start, :east}], %{}, %{}),
       meta: grid.meta,
     }
   end
 
-  defp walk(_grid, [], acc_nodes), do: acc_nodes
-  defp walk(grid, [{pos, dir} | rem_nodes], acc_nodes) do
-    acc_nodes
+  # terminology:
+  # - a "corridor" square has only 1-2 passable neighbor squares
+  # - a "junction" square (`junct`) has 3-4 passable neighbor squares
+  #   (NB the start and end squares are treated as junctions)
+  # - a "node" is a **junction plus a facing direction**
+
+  defp walk_junctions(_grid, [], nodes, seen), do: nodes
+  defp walk_junctions(grid, [{pos, dir} | rem_q_juncts], nodes, seen) do
+    {nodes, q_juncts, seen} =
+      initial_unwalked_squares(grid, pos, seen)
+      |> Enum.reduce({nodes, [], seen}, fn {npos, ndir}, {nodes, q_juncts, seen} ->
+        {next_junct_cost, seen} = junct_and_cost_from(grid, {pos, dir}, {npos, ndir}, seen)
+        walk_junction_accumulate(nodes, {pos, dir}, next_junct_cost, q_juncts, seen)
+      end)
+      |> dbg()
+    # TODO walk_junctions(grid, rem_q_juncts ++ q_juncts, nodes, seen)
+  end
+
+  # returns `{next_pos, next_dir}` for the neighbor squares
+  # - which are the routes out of the junction at `pos`
+  # - which have not yet been walked
+  defp initial_unwalked_squares(grid, pos, seen) do
+    passable_neighbors(grid, pos)
+    |> Enum.reject(&(Map.get(seen, &1) == true))
+    |> Enum.map(&({&1, dir_of(pos, &1)}))
+  end
+
+  defp passable_neighbors(grid, pos) do
+    grid
+    |> Grid.cardinals_of(pos)
+    |> Enum.reject(fn pos -> Grid.get(grid, pos) == ?# end)
+  end
+
+  defp walk_junction_accumulate(nodes, _this_junct, nil, q_juncts, seen) do
+    {nodes, q_juncts, seen}
+  end
+  defp walk_junction_accumulate(nodes, this_junct, next_junct_cost, q_juncts, seen) do
+    {
+      add_next_junct(nodes, this_junct, next_junct_cost),
+      add_to_queue(nodes, q_juncts, next_junct_cost),
+      seen,
+    }
+  end
+
+  defp add_to_queue(nodes, q_juncts, next_junct_cost) do
+    # only add to walk queue if it hasn't been added to graph yet
+    if Map.get(nodes, elem(next_junct_cost, 0)) do
+      q_juncts
+    else
+      [elem(next_junct_cost, 0) | q_juncts]
+    end
+  end
+
+  defp add_next_junct(nodes, this_junct, next_junct_cost) do
+    Map.update(nodes, this_junct, [next_junct_cost], &[next_junct_cost | &1])
+  end
+
+  # need to turn before walking: creates a new node, same position but
+  # different facing direction
+  defp junct_and_cost_from(grid, {pos, dir}, {npos, ndir}, seen) when dir != ndir do
+    {{{pos, ndir}, 1000}, seen}
+  end
+
+  # walk straight ahead down corridor
+  defp junct_and_cost_from(grid, {pos, dir}, {npos, ndir}, seen) when dir == ndir do
+    # TODO walk_corridor(..., {npos, ndir})
+    {{{:ipos, :idir}, :cost}, seen}  # TODO updated `seen` from `walk_corridor()`
   end
 
   defp dir_of({y, x}, {ny, nx}) do
