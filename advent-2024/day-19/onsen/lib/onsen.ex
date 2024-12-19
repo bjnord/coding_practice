26a4966ba83b512dc19a86c83b3892874fa59b94
Brent J. Nordquist <brent@nordist.net>
2024-12-19 04:14:48 -0600

    aoc-2024 19-p1: Correct answer

--- advent-2024/day-19/onsen/lib/onsen.ex
+++ advent-2024/day-19/onsen/lib/onsen.ex
@@ -6,6 +6,26 @@ defmodule Onsen do
   import Onsen.Parser
   import History.CLI
 
+  def possible?(towel, patterns) do
+    subpossible?(towel, patterns, patterns)
+  end
+
+  def subpossible?(_towel, nil, _all_patterns), do: false
+  def subpossible?([], patterns, _all_patterns) do
+    Map.get(patterns, ?.) == true
+  end
+  def subpossible?([color | towel], patterns, all_patterns) do
+    if Map.get(patterns, ?.) == true do
+      if possible?([color | towel], all_patterns) do
+        true
+      else
+        subpossible?(towel, Map.get(patterns, color), all_patterns)
+      end
+    else
+      subpossible?(towel, Map.get(patterns, color), all_patterns)
+    end
+  end
+
   @doc """
   Parse arguments and call puzzle part methods.
 
@@ -23,8 +43,10 @@ defmodule Onsen do
   Process input file and display part 1 solution.
   """
   def part1(input_path) do
-    parse_input_file(input_path)
-    nil  # TODO
+    {towel_patterns, towels} = parse_input_file(input_path)
+    towels
+    |> Enum.map(&(Onsen.possible?(&1, towel_patterns)))
+    |> Enum.count(&(&1))
     |> IO.inspect(label: "Part 1 answer is")
   end
 
--- advent-2024/day-19/onsen/test/onsen_test.exs
+++ advent-2024/day-19/onsen/test/onsen_test.exs
@@ -1,4 +1,68 @@
 defmodule OnsenTest do
   use ExUnit.Case
   doctest Onsen
+
+  describe "puzzle example" do
+    setup do
+      [
+        towel_patterns: %{
+          ?r => %{
+            ?. => true,
+            ?b => %{
+              ?. => true,
+            },
+          },
+          ?g => %{
+            ?. => true,
+            ?b => %{
+              ?. => true,
+            },
+          },
+          ?b => %{
+            ?. => true,
+            ?r => %{
+              ?. => true,
+            },
+            ?w => %{
+              ?u => %{
+                ?. => true,
+              },
+            },
+          },
+          ?w => %{
+            ?r => %{
+              ?. => true,
+            },
+          },
+        },
+        towels: [
+          [?b, ?r, ?w, ?r, ?r],
+          [?b, ?g, ?g, ?r],
+          [?g, ?b, ?b, ?r],
+          [?r, ?r, ?b, ?g, ?b, ?r],
+          [?u, ?b, ?w, ?u],
+          [?b, ?w, ?u, ?r, ?r, ?g],
+          [?b, ?r, ?g, ?r],
+          [?b, ?b, ?r, ?g, ?w, ?b],
+        ],
+        exp_possibles: [
+          true,
+          true,
+          true,
+          true,
+          false,
+          true,
+          true,
+          false,
+        ],
+      ]
+    end
+
+    test "finds possible towel arrangements", fixture do
+      act_possibles =
+        fixture.towels
+        |> Enum.map(&(Onsen.possible?(&1, fixture.towel_patterns)))
+      assert act_possibles == fixture.exp_possibles
+    end
+  end
 end
