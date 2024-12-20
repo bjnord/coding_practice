03ebd73f319c71c6ade28b290682a6a6301c2998
Brent J. Nordquist <brent@nordist.net>
2024-12-20 06:06:43 -0600

    aoc-2024 20-p1: Find cheats

--- advent-2024/day-20/race/lib/race.ex
+++ advent-2024/day-20/race/lib/race.ex
@@ -5,6 +5,34 @@ defmodule Race do
 
   import Race.Parser
   import History.CLI
+  alias History.Grid
+
+  def cheats(grid) do
+    for y <- 0..(grid.size.y - 1),
+      x <- 0..(grid.size.x - 1) do
+        cheats(grid, {y, x}, Grid.get(grid, {y, x}))
+      end
+      |> List.flatten()
+      |> Enum.sort()
+  end
+
+  defp cheats(_grid, _pos, ?#), do: []
+  defp cheats(grid, {y, x}, nil) do
+    Grid.cardinals_of(grid, {y, x})
+    |> Enum.map(&(cheat(grid, {y, x}, &1)))
+    |> Enum.reject(&(&1 == nil))
+  end
+
+  defp cheat(grid, {y, x}, {ny, nx}) do
+    {dy, dx} = {ny - y, nx - x}
+    {nny, nnx} = {ny + dy, nx + dx}
+    cond do
+      Grid.get(grid, {ny, nx}) != ?#     -> nil
+      !Grid.in_bounds?(grid, {nny, nnx}) -> nil
+      Grid.get(grid, {nny, nnx}) == ?#   -> nil
+      true                               -> {{ny, nx}, {nny, nnx}}
+    end
+  end
 
   @doc """
   Parse arguments and call puzzle part methods.
