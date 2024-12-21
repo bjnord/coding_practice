28b9d7124589892f9eb90fd30b81bb75a4c9c016
Brent J. Nordquist <brent@nordist.net>
2024-12-21 10:24:46 -0600

    aoc-2024 21-p1: More test cases (trigger the bug)
    h/t Reddit user avquiraniel
    <https://www.reddit.com/r/adventofcode/comments/1hj6o1j/2024_day_21_part_1_can_anyone_give_me_more/>

--- advent-2024/day-21/keypad/test/keypad_test.exs
+++ advent-2024/day-21/keypad/test/keypad_test.exs
@@ -6,11 +6,16 @@ defmodule KeypadTest do
     setup do
       [
         examples: [
-          {~c"029A", ~c"<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A"},
-          {~c"980A", ~c"<v<A>>^AAAvA^A<vA<AA>>^AvAA<^A>A<v<A>A>^AAAvA<^A>A<vA>^A<A>A"},
-          {~c"179A", ~c"<v<A>>^A<vA<A>>^AAvAA<^A>A<v<A>>^AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A"},
-          {~c"456A", ~c"<v<A>>^AA<vA<A>>^AAvAA<^A>A<vA>^A<A>A<vA>^A<A>A<v<A>A>^AAvA<^A>A"},
-          {~c"379A", ~c"<v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A"},
+          {~c"029A", ~c"<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A", 68},
+          {~c"980A", ~c"<v<A>>^AAAvA^A<vA<AA>>^AvAA<^A>A<v<A>A>^AAAvA<^A>A<vA>^A<A>A", 60},
+          {~c"179A", ~c"<v<A>>^A<vA<A>>^AAvAA<^A>A<v<A>>^AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A", 68},
+          {~c"456A", ~c"<v<A>>^AA<vA<A>>^AAvAA<^A>A<vA>^A<A>A<vA>^A<A>A<v<A>A>^AAvA<^A>A", 64},
+          {~c"379A", ~c"<v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A", 64},
+          {~c"159A", nil, 82},
+          {~c"375A", ~c"<v<A>>^AvA^A<vA<AA>>^AAvA^<A>AAvA^A<v<A>A>^AvA^A<A>A<v<A>A>^AAvA^A<A>A", 70},
+          {~c"613A", nil, 62},
+          {~c"894A", nil, 78},
+          {~c"080A", nil, 60},
         ],
         exp_complexity: 126384,
       ]
@@ -18,8 +23,7 @@ defmodule KeypadTest do
 
     test "calculate correct minimal lengths", fixture do
       exp_lengths = fixture.examples
-                    |> Enum.map(&(elem(&1, 1)))
-                    |> Enum.map(&(Enum.count(&1)))
+                    |> Enum.map(&(elem(&1, 2)))
       act_lengths = fixture.examples
                     |> Enum.map(&(elem(&1, 0)))
                     |> Keypad.code_sequences()
