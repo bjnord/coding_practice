d751f6a58eab517262e4d52dee6725d1c2bff79d
Brent J. Nordquist <brent@nordist.net>
2024-12-23 07:41:29 -0600

    aoc-2024 12-p2: Correct answer

--- advent-2024/day-12/fence/lib/fence.ex
+++ advent-2024/day-12/fence/lib/fence.ex
@@ -57,7 +57,7 @@ defmodule Fence do
   def bulk_prices(grid) do
     areas_sides(grid)
     |> Enum.reduce(0, fn {_plant, {area, side}}, acc ->
-      acc + area * side  # FIXME `acc` isn't an integer
+      acc + area * side
     end)
   end
 
@@ -81,8 +81,65 @@ defmodule Fence do
   def sides(grid, {plant, positions}) do
     positions
     |> Enum.flat_map(&(position_sides(grid, &1, plant)))
+    |> fence_sides()
   end
 
+  def fence_sides(fences) do
+    horiz_sides(fences) + vert_sides(fences)
+  end
+
+  defp horiz_sides(fences) do
+    fences
+    |> Enum.filter(fn {_pos, ch} -> (ch == ?-) || (ch == ?=) end)
+    |> Enum.sort_by(fn {{y, x}, _ch} -> {y, x} end)
+    |> sides_of(&horiz_cmp/1)
+  end
+
+  def horiz_cmp([{{_ny, _nx}, _nch}]), do: :side
+  def horiz_cmp([{{y, x}, ch}, {{ny, nx}, nch}]) do
+    cond do
+      ch != nch ->                       :side
+      (ny == y) && (abs(nx - x) == 2) -> :more_side
+      true ->                            :side
+    end
+  end
+
+  defp vert_sides(fences) do
+    fences
+    |> Enum.filter(fn {_pos, ch} -> (ch == ?|) || (ch == ?I) end)
+    |> Enum.sort_by(fn {{y, x}, _ch} -> {x, y} end)
+    |> sides_of(&vert_cmp/1)
+  end
+
+  def vert_cmp([{{_ny, _nx}, _nch}]), do: :side
+  def vert_cmp([{{y, x}, ch}, {{ny, nx}, nch}]) do
+    cond do
+      ch != nch ->                       :side
+      (nx == x) && (abs(ny - y) == 2) -> :more_side
+      true ->                            :side
+    end
+  end
+
+  defp sides_of(fences, f) do
+    fences
+    |> Enum.chunk_every(2, 1)
+    |> Enum.map(&(f.(&1)))
+    |> Enum.count(&(&1 == :side))
+  end
+
+  # returns list of `{plant, [{pos, ch}]}`
+  def fence_squares(grid) do
+    grid.meta.regions
+    |> Enum.map(&(region_fence_squares(grid, &1)))
+  end
+
+  defp region_fence_squares(grid, {plant, positions}) do
+    positions
+    |> Enum.flat_map(&(position_sides(grid, &1, plant)))
+    |> then(&({plant, &1}))
+  end
+
+  # returns list of fence {pos, ch}
   def position_sides(grid, pos, plant) do
     neighbors = Grid.cardinals_of(grid, pos, oob: true)
     neighbors
@@ -106,7 +163,9 @@ defmodule Fence do
 
   defp double(n), do: n * 2 + 1
 
+  defp fence_of(dy, 0) when dy > 0, do: ?=
   defp fence_of(_dy, 0), do: ?-
+  defp fence_of(0, dx) when dx > 0, do: ?I
   defp fence_of(0, _dx), do: ?|
 
   @doc """
@@ -136,7 +195,7 @@ defmodule Fence do
   """
   def part2(input_path) do
     parse_input_file(input_path)
-    nil  # TODO
+    |> Fence.bulk_prices()
     |> IO.inspect(label: "Part 2 answer is")
   end
 end
--- advent-2024/day-12/fence/test/fence_test.exs
+++ advent-2024/day-12/fence/test/fence_test.exs
@@ -83,33 +83,72 @@ defmodule FenceTest do
           772,   # [1]
           1930,  # [2]
         ],
-        exp_areas_sides: [
-          {?A, {4, [
-            {{0, 1}, ?-}, {{2, 1}, ?-}, {{1, 0}, ?|},
-            {{0, 3}, ?-}, {{2, 3}, ?-},
-            {{0, 5}, ?-}, {{2, 5}, ?-},
-            {{0, 7}, ?-}, {{1, 8}, ?|}, {{2, 7}, ?-},
-          ]}},
-          {?B, {4, [
-            {{2, 1}, ?-}, {{3, 0}, 124},
-            {{2, 3}, 45}, {{3, 4}, 124},
-            {{6, 1}, 45}, {{5, 0}, 124},
-            {{5, 4}, ?|}, {{6, 3}, 45},
-          ]}},
-          {?C, {4, [
-            {{2, 5}, ?-}, {{3, 6}, ?|}, {{3, 4}, ?|},
-            {{6, 5}, ?-}, {{5, 4}, ?|},
-            {{4, 7}, ?-}, {{5, 8}, ?|},
-            {{7, 8}, ?|}, {{8, 7}, ?-}, {{7, 6}, ?|},
-          ]}},
-          {?D, {1, [
-            {{2, 7}, 45}, {{3, 8}, 124}, {{4, 7}, 45}, {{3, 6}, 124},
-          ]}},
-          {?E, {3, [
-            {{6, 1}, 45}, {{8, 1}, 45}, {{7, 0}, 124},
-            {{6, 3}, 45}, {{8, 3}, 45},
-            {{6, 5}, 45}, {{7, 6}, 124}, {{8, 5}, 45},
-          ]}},
+        exp_fence_squares: [
+          {?A, [
+            {{0, 1}, ?-}, {{2, 1}, ?=}, {{1, 0}, ?|},
+            {{0, 3}, ?-}, {{2, 3}, ?=},
+            {{0, 5}, ?-}, {{2, 5}, ?=},
+            {{0, 7}, ?-}, {{1, 8}, ?I}, {{2, 7}, ?=},
+          ]},
+          {?B, [
+            {{2, 1}, ?-}, {{3, 0}, ?|},
+            {{2, 3}, ?-}, {{3, 4}, ?I},
+            {{6, 1}, ?=}, {{5, 0}, ?|},
+            {{5, 4}, ?I}, {{6, 3}, ?=},
+          ]},
+          {?C, [
+            {{2, 5}, ?-}, {{3, 6}, ?I}, {{3, 4}, ?|},
+            {{6, 5}, ?=}, {{5, 4}, ?|},
+            {{4, 7}, ?-}, {{5, 8}, ?I},
+            {{7, 8}, ?I}, {{8, 7}, ?=}, {{7, 6}, ?|},
+          ]},
+          {?D, [
+            {{2, 7}, ?-}, {{3, 8}, ?I}, {{4, 7}, ?=}, {{3, 6}, ?|},
+          ]},
+          {?E, [
+            {{6, 1}, ?-}, {{8, 1}, ?=}, {{7, 0}, ?|},
+            {{6, 3}, ?-}, {{8, 3}, ?=},
+            {{6, 5}, ?-}, {{7, 6}, ?I}, {{8, 5}, ?=},
+          ]},
+        ],
+        exp_sides: [
+          [
+            {?A, {4, 4}},
+            {?B, {4, 4}},
+            {?C, {4, 8}},
+            {?D, {1, 4}},
+            {?E, {3, 4}},
+          ],
+          [
+            {?O, {21, 20}},
+            {?X, {1, 4}},
+            {?X, {1, 4}},
+            {?X, {1, 4}},
+            {?X, {1, 4}},
+          ],
+          [
+            {?E, {13, 8}},
+            {?S, {3, 6}},
+            {?F, {10, 12}},
+            {?C, {14, 22}},
+            {?J, {11, 12}},
+            {?I, {4, 4}},
+            {?V, {13, 10}},
+            {?R, {12, 10}},
+            {?C, {1, 4}},
+            {?I, {14, 16}},
+            {?M, {5, 6}},
+          ],
+          [
+            {?E, {17, 12}},
+            {?X, {4, 4}},
+            {?X, {4, 4}},
+          ],
+          [
+            {?B, {4, 4}},
+            {?A, {28, 12}},
+            {?B, {4, 4}},
+          ],
         ],
         exp_bulk_prices: [
           80,    # [0]
@@ -133,13 +172,21 @@ defmodule FenceTest do
       assert act_areas_perims == fixture.exp_areas_perims
     end
 
-    test "finds correct area and sides", fixture do
-      act_areas_sides =
+    test "finds correct fence squares", fixture do
+      act_fence_squares =
         hd(fixture.inputs)
         |> parse_input_string()
-        |> Fence.areas_sides()
+        |> Fence.fence_squares()
         |> Enum.sort()
-      assert act_areas_sides == fixture.exp_areas_sides
+      assert act_fence_squares == fixture.exp_fence_squares
+    end
+
+    test "finds correct sides", fixture do
+      act_sides =
+        fixture.inputs
+        |> Enum.map(&parse_input_string/1)
+        |> Enum.map(&Fence.areas_sides/1)
+      assert act_sides == fixture.exp_sides
     end
 
     test "finds correct prices", fixture do
@@ -151,13 +198,12 @@ defmodule FenceTest do
       assert act_prices == fixture.exp_prices
     end
 
-    # FIXME temporary comment-out
-    #test "finds correct bulk prices", fixture do
-    #  act_bulk_prices =
-    #    fixture.inputs
-    #    |> Enum.map(&parse_input_string/1)
-    #    |> Enum.map(&Fence.bulk_prices/1)
-    #  assert act_bulk_prices == fixture.exp_bulk_prices
-    #end
+    test "finds correct bulk prices", fixture do
+      act_bulk_prices =
+        fixture.inputs
+        |> Enum.map(&parse_input_string/1)
+        |> Enum.map(&Fence.bulk_prices/1)
+      assert act_bulk_prices == fixture.exp_bulk_prices
+    end
   end
 end
