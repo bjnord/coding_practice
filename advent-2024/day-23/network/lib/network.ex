94a29c317d3c12d4f023cb077d099c5087ad3311
Brent J. Nordquist <brent@nordist.net>
2024-12-23 02:15:50 -0600

    aoc-2024 23-p1: Correct answer

--- advent-2024/day-23/network/lib/network.ex
+++ advent-2024/day-23/network/lib/network.ex
@@ -6,6 +6,64 @@ defmodule Network do
   import Network.Parser
   import History.CLI
 
+  def t_triads(connections) do
+    connections
+    |> form_network()
+    |> triads()
+  end
+
+  def form_network(connections) do
+    connections
+    |> Enum.reduce(%{}, fn {a, b}, acc ->
+      acc
+      |> connect(a, b)
+      |> connect(b, a)
+    end)
+  end
+
+  def connect(network, a, b) do
+    Map.update(network, a, MapSet.new([b]), fn nodes ->
+      MapSet.put(nodes, b)
+    end)
+  end
+
+  def triads(network) do
+    Map.keys(network)
+    |> Enum.filter(&starts_t?/1)
+    |> Enum.sort()
+    |> Enum.flat_map(fn node ->
+      peers = Map.get(network, node)
+      MapSet.to_list(peers)
+      |> Enum.reduce({peers, []}, fn a, {rem_peers, acc} ->
+        rem_peers =
+          MapSet.delete(rem_peers, a)
+        acc =
+          MapSet.to_list(rem_peers)
+          |> Enum.reduce(acc, fn b, acc ->
+            if connects_to?(network, a, b) do
+              [Enum.sort([node, a, b]) | acc]
+            else
+              acc
+            end
+          end)
+        {rem_peers, acc}
+      end)
+      |> elem(1)
+      |> Enum.reverse()
+    end)
+    |> Enum.sort()
+    |> Enum.uniq()
+  end
+
+  defp starts_t?(node) do
+    String.starts_with?(node, "t")
+  end
+
+  def connects_to?(network, a, b) do
+    Map.get(network, a)
+    |> MapSet.member?(b)
+  end
+
   @doc """
   Parse arguments and call puzzle part methods.
 
@@ -24,7 +82,8 @@ defmodule Network do
   """
   def part1(input_path) do
     parse_input_file(input_path)
-    nil  # TODO
+    |> Network.t_triads()
+    |> Enum.count()
     |> IO.inspect(label: "Part 1 answer is")
   end
 
--- advent-2024/day-23/network/test/network_test.exs
+++ advent-2024/day-23/network/test/network_test.exs
@@ -1,4 +1,60 @@
 defmodule NetworkTest do
   use ExUnit.Case
   doctest Network
+
+  describe "puzzle example" do
+    setup do
+      [
+        connections: [
+          {"kh", "tc"},
+          {"qp", "kh"},
+          {"de", "cg"},
+          {"ka", "co"},
+          {"yn", "aq"},
+          {"qp", "ub"},
+          {"cg", "tb"},
+          {"vc", "aq"},
+          {"tb", "ka"},
+          {"wh", "tc"},
+          {"yn", "cg"},
+          {"kh", "ub"},
+          {"ta", "co"},
+          {"de", "co"},
+          {"tc", "td"},
+          {"tb", "wq"},
+          {"wh", "td"},
+          {"ta", "ka"},
+          {"td", "qp"},
+          {"aq", "cg"},
+          {"wq", "ub"},
+          {"ub", "vc"},
+          {"de", "ta"},
+          {"wq", "aq"},
+          {"wq", "vc"},
+          {"wh", "yn"},
+          {"ka", "de"},
+          {"kh", "ta"},
+          {"co", "tc"},
+          {"wh", "qp"},
+          {"tb", "vc"},
+          {"td", "yn"},
+        ],
+        exp_t_triads: [
+          ["co", "de", "ta"],
+          ["co", "ka", "ta"],
+          ["de", "ka", "ta"],
+          ["qp", "td", "wh"],
+          ["tb", "vc", "wq"],
+          ["tc", "td", "wh"],
+          ["td", "wh", "yn"],
+        ],
+      ]
+    end
+
+    test "find triads with 't' computers", fixture do
+      act_t_triads = fixture.connections
+                     |> Network.t_triads()
+      assert act_t_triads == fixture.exp_t_triads
+    end
+  end
 end
