ce2fa11a23c7f8ce57b9aed430dacf6dd25696ef
Brent J. Nordquist <brent@nordist.net>
2024-12-24 05:38:19 -0600

    aoc-2024 24-p1: Correct answer

--- advent-2024/day-24/wire/lib/wire.ex
+++ advent-2024/day-24/wire/lib/wire.ex
@@ -3,8 +3,71 @@ defmodule Wire do
   Documentation for `Wire`.
   """
 
-  import Wire.Parser
+  import Bitwise
   import History.CLI
+  import Wire.Parser
+
+  def eval(diagram) do
+    Map.keys(diagram)
+    |> Enum.filter(&z_wire?/1)
+    |> build_queue(diagram)
+    |> then(fn queue -> {%{}, queue} end)
+    |> eval_queue(diagram)
+    |> reduce_value()
+  end
+
+  defp build_queue(z_wires, diagram) do
+    z_wires
+    |> Enum.reduce([], fn z_wire, queue ->
+      add_to_queue(queue, z_wire, diagram)
+    end)
+  end
+
+  defp z_wire?(ident) do
+    Atom.to_string(ident)
+    |> String.first()
+    |> then(fn gr -> gr == "z" end)
+  end
+
+  defp add_to_queue(queue, ident = rhs, diagram) when is_atom(rhs) do
+    rhs = Map.get(diagram, ident)
+    add_to_queue([ident | queue], rhs, diagram)
+  end
+  defp add_to_queue(queue, {ident1, _gate, ident2} = _rhs, diagram) do
+    add_to_queue(queue, ident1, diagram)
+    |> add_to_queue(ident2, diagram)
+  end
+  defp add_to_queue(queue, _rhs, _diagram), do: queue
+
+  defp eval_queue({values, []}, _diagram), do: values
+  defp eval_queue({values, [item | queue]}, diagram) do
+    value = eval_item(values, Map.get(diagram, item))
+    next = {Map.put(values, item, value), queue}
+    eval_queue(next, diagram)
+  end
+
+  defp eval_item(values, ident = rhs) when is_atom(rhs) do
+    Map.get(values, ident)
+  end
+  defp eval_item(values, {ident1, gate, ident2} = _rhs) do
+    value1 = Map.get(values, ident1)
+    value2 = Map.get(values, ident2)
+    expr(value1, gate, value2)
+  end
+  defp eval_item(_values, value = _rhs), do: value
+
+  defp expr(v1, :AND, v2), do: v1 &&& v2
+  defp expr(v1, :OR, v2), do: v1 ||| v2
+  defp expr(v1, :XOR, v2), do: bxor(v1, v2)
+
+  def reduce_value(values) do
+    Map.keys(values)
+    |> Enum.filter(&z_wire?/1)
+    |> Enum.sort(&(&1 >= &2))
+    |> Enum.reduce(0, fn ident, acc ->
+      acc * 2 + Map.get(values, ident)
+    end)
+  end
 
   @doc """
   Parse arguments and call puzzle part methods.
@@ -24,7 +87,7 @@ defmodule Wire do
   """
   def part1(input_path) do
     parse_input_file(input_path)
-    nil  # TODO
+    |> Wire.eval()
     |> IO.inspect(label: "Part 1 answer is")
   end
 
