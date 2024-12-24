6d9617c165260e3f85acac5bdb9bd0472d31b0e4
Brent J. Nordquist <brent@nordist.net>
2024-12-24 14:58:41 -0600

    aoc-2024 24-p1: Dump show both names

--- advent-2024/day-24/wire/lib/wire.ex
+++ advent-2024/day-24/wire/lib/wire.ex
@@ -64,7 +64,7 @@ defmodule Wire do
     end)
   end
 
-  def dump(path, diagram, {_f_mapping, mapping}) do
+  def dump(path, diagram, {_f_mapping, mapping}, opts \\ []) do
     {:ok, f} = File.open(path, [:write])
     IO.binwrite(f, "--\n")
     Map.keys(diagram)
@@ -72,52 +72,70 @@ defmodule Wire do
     |> Enum.sort()
     |> Enum.reduce(%{}, fn wire, seen ->
       IO.binwrite(f, "#{wire}\n")
-      dump_subtree(f, diagram, Map.get(diagram, wire), 3, mapping, seen)
+      dump_subtree(f, diagram, Map.get(diagram, wire), 3, mapping, seen, opts)
     end)
     IO.binwrite(f, "--\n")
     File.close(f)
   end
 
-  defp dump_subtree(f, _diagram, value = rhs, indent, _mapping, seen) when is_integer(rhs) do
+  defp dump_subtree(f, _diagram, value = rhs, indent, _mapping, seen, opts) when is_integer(rhs) do
     Integer.to_string(value)
     |> String.pad_leading(indent, " ")
     |> then(fn s -> IO.binwrite(f, "#{s}\n") end)
     seen
   end
-  defp dump_subtree(f, diagram, {wire1, gate, wire2} = _rhs, indent, mapping, seen) do
-    deob_wire1 = Map.get(mapping, wire1, wire1)
-    deob_wire2 = Map.get(mapping, wire2, wire2)
+  defp dump_subtree(f, diagram, {wire1, gate, wire2} = _rhs, indent, mapping, seen, opts) do
+    {deob_wire1, deob_len1} = deobfuscate_wire(mapping, wire1, opts)
+    {deob_wire2, deob_len2} = deobfuscate_wire(mapping, wire2, opts)
     #
     seen =
       if Map.get(seen, wire1) do
-        String.pad_leading(deob_wire1, deob_length(deob_wire1) + indent, " ")
+        String.pad_leading(deob_wire1, deob_len1 + indent, " ")
         |> then(fn s -> s <> " #{gate}" end)
         |> then(fn s -> IO.binwrite(f, "#{s}\n") end)
         seen
       else
-        String.pad_leading(deob_wire1, deob_length(deob_wire1) + indent, " ")
+        String.pad_leading(deob_wire1, deob_len1 + indent, " ")
         |> then(fn s -> s <> " #{gate}" end)
         |> then(fn s -> IO.binwrite(f, "#{s}\n") end)
         seen = Map.put(seen, wire1, true)
-        dump_subtree(f, diagram, Map.get(diagram, wire1), indent + 3, mapping, seen)
+        dump_subtree(f, diagram, Map.get(diagram, wire1), indent + 3, mapping, seen, opts)
       end
     #
     seen =
       if Map.get(seen, wire2) do
-        String.pad_leading(deob_wire2, deob_length(deob_wire2) + indent, " ")
+        String.pad_leading(deob_wire2, deob_len2 + indent, " ")
         |> then(fn s -> IO.binwrite(f, "#{s}\n") end)
         seen
       else
-        String.pad_leading(deob_wire2, deob_length(deob_wire2) + indent, " ")
+        String.pad_leading(deob_wire2, deob_len2 + indent, " ")
         |> then(fn s -> IO.binwrite(f, "#{s}\n") end)
         seen = Map.put(seen, wire2, true)
-        dump_subtree(f, diagram, Map.get(diagram, wire2), indent + 3, mapping, seen)
+        dump_subtree(f, diagram, Map.get(diagram, wire2), indent + 3, mapping, seen, opts)
       end
     #
     seen
   end
 
-  defp deob_length(w) when is_integer(w), do: 1
+  defp deobfuscate_wire(mapping, wire, opts) do
+    deob_wire =
+      cond do
+        xyz_wire?(wire) ->
+          Map.get(mapping, wire, wire)
+        opts[:both] ->
+          deob_wire = Map.get(mapping, wire, wire)
+          if deob_wire != wire do
+            "#{deob_wire} [#{wire}]"
+          else
+            "? [#{wire}]"
+          end
+        true ->
+          Map.get(mapping, wire, wire)
+      end
+    {deob_wire, deob_length(deob_wire)}
+  end
+
+  #defp deob_length(w) when is_integer(w), do: 1
   defp deob_length(w), do: String.length(w)
 
   def generate_adder(ww) do
@@ -163,7 +181,7 @@ defmodule Wire do
       |> add_fulls_to_catalog(diagram)
       #|> dbg()
     if opts[:verbose] do
-      dump("log/dump.out", diagram, catalog)
+      dump("log/dump.out", diagram, catalog, both: true)
       adder_diagram = generate_adder(4)
       dump("log/adder.out", adder_diagram, {%{}, %{}})
     end
