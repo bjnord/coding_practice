4b49810a9f86e0ef1f07c15b7762ae23da37fc4d
Brent J. Nordquist <brent@nordist.net>
2024-12-25 04:23:04 -0600

    aoc-2024 25-p1: Correct answer

--- advent-2024/day-25/lock/lib/lock.ex
+++ advent-2024/day-25/lock/lib/lock.ex
@@ -3,8 +3,61 @@ defmodule Lock do
   Documentation for `Lock`.
   """
 
+  import Bitwise
   import Lock.Parser
+  require Logger
   import History.CLI
+
+  def fits(objects, n_slots \\ 5, max_height \\ 7) do
+    {locks, keys} = locks_and_keys(objects, max_height)
+    locks
+    |> Enum.reduce(0, fn lock, acc ->
+      keys
+      |> Enum.reduce(acc, fn key, acc ->
+        debug(lock, key, n_slots * max_height)
+        if (lock &&& key) == 0b0 do
+          acc + 1
+        else
+          acc
+        end
+      end)
+    end)
+  end
+
+  defp debug(lock, key, bwidth) do
+    Logger.debug("")
+    Logger.debug("lock: #{debug_bin(lock, bwidth)}")
+    Logger.debug("key:  #{debug_bin(key, bwidth)}")
+    Logger.debug("AND:  #{debug_bin((lock &&& key), bwidth)}")
+  end
+
+  defp debug_bin(b, bwidth) do
+    Integer.to_string(b, 2)
+    |> String.pad_leading(bwidth, "0")
+  end
+
+  defp locks_and_keys(objects, max_height) do
+    locks =
+      objects
+      |> Enum.filter(&(elem(&1, 0) == :lock))
+      |> Enum.map(&(bitmap_of_object(&1, max_height)))
+    keys =
+      objects
+      |> Enum.filter(&(elem(&1, 0) == :key))
+      |> Enum.map(&(bitmap_of_object(&1, max_height)))
+    {locks, keys}
+  end
+
+  def bitmap_of_object({_obj_type, pairs} = _object, max_height) do
+    bitmap_concat(pairs, max_height)
+  end
+
+  def bitmap_concat(pairs, max_height) do
+    pairs
+    |> Enum.reduce(0b0, fn {_height, bitmap}, acc ->
+      acc * (2 ** max_height) + bitmap
+    end)
+  end
 
   @doc """
   Parse arguments and call puzzle part methods.
@@ -24,7 +77,7 @@ defmodule Lock do
   """
   def part1(input_path) do
     parse_input_file(input_path)
-    nil  # TODO
+    |> Lock.fits()
     |> IO.inspect(label: "Part 1 answer is")
   end
 
--- advent-2024/day-25/lock/test/lock_test.exs
+++ advent-2024/day-25/lock/test/lock_test.exs
@@ -1,4 +1,62 @@
 defmodule LockTest do
   use ExUnit.Case
   doctest Lock
+
+  import Lock.Parser
+
+  describe "puzzle example" do
+    setup do
+      [
+        input: """
+        #####
+        .####
+        .####
+        .####
+        .#.#.
+        .#...
+        .....
+
+        #####
+        ##.##
+        .#.##
+        ...##
+        ...#.
+        ...#.
+        .....
+
+        .....
+        #....
+        #....
+        #...#
+        #.#.#
+        #.###
+        #####
+
+        .....
+        .....
+        #.#..
+        ###..
+        ###.#
+        ###.#
+        #####
+
+        .....
+        .....
+        .....
+        #....
+        #.#..
+        #.#.#
+        #####
+        """,
+        exp_fits: 3,
+      ]
+    end
+
+    test "find number of lock-and-key fits", fixture do
+      act_fits = fixture.input
+                 |> parse_input_string()
+                 |> Lock.fits()
+      assert act_fits == fixture.exp_fits
+    end
+  end
 end
