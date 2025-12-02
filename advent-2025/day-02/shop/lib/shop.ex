2ed8729980d9e50f13155b3c6f55f968d4cb719d
Brent J. Nordquist <brent@nordist.net>
2025-12-02 06:32:23 -0600

    aoc-2025 02-p1: Partway to correct answer

--- advent-2025/day-02/shop/lib/shop.ex
+++ advent-2025/day-02/shop/lib/shop.ex
@@ -3,21 +3,57 @@ defmodule Shop do
   Documentation for `Shop`.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Shop.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
-
   import Shop.Parser
   import History.CLI
+
+  @doc """
+  Count doubled product IDs within a product ID range.
+
+  ## Parameters
+
+  - `range`: the product ID range
+  """
+  # FIXME use `product_range()` type
+  @spec count_doubled({integer(), integer()}) :: integer()
+  def count_doubled(range) do
+    Range.new(elem(range, 0), elem(range, 1))
+    |> Enum.map(&is_doubled?/1)
+    # FIXME use Enum.count() with function?
+    |> Enum.map(fn doub -> if doub, do: 1, else: 0 end)
+    |> Enum.sum()
+  end
+
+  @doc """
+  Does product ID contain a sequence of digits repeated twice?
+
+  ## Parameters
+
+  - `product_id`: the product ID (integer)
+
+  ## Examples
+      iex> Shop.is_doubled?(55)
+      true
+      iex> Shop.is_doubled?(56)
+      false
+      iex> Shop.is_doubled?(646)
+      false
+      iex> Shop.is_doubled?(6464)
+      true
+      iex> Shop.is_doubled?(123123)
+      true
+  """
+  @spec is_doubled?(integer()) :: boolean()
+  def is_doubled?(product_id) do
+    s = Integer.to_string(product_id)
+    len = String.length(s)
+    if rem(len, 2) == 0 do
+      l = String.slice(s, 0, div(len, 2))
+      r = String.slice(s, div(len, 2), div(len, 2))
+      l == r
+    else
+      false
+    end
+  end
 
   @doc """
   Parse arguments and call puzzle part methods.
--- advent-2025/day-02/shop/test/shop_test.exs
+++ advent-2025/day-02/shop/test/shop_test.exs
@@ -2,7 +2,42 @@ defmodule ShopTest do
   use ExUnit.Case
   doctest Shop
 
-  test "greets the world" do
-    assert Shop.hello() == :world
+  describe "puzzle example" do
+    setup do
+      [
+        product_ranges: [
+          {11, 22},
+          {95, 115},
+          {998, 1012},
+          {1188511880, 1188511890},
+          {222220, 222224},
+          {1698522, 1698528},
+          {446443, 446449},
+          {38593856, 38593862},
+          {565653, 565659},
+          {824824821, 824824827},
+          {2121212118, 2121212124},
+        ],
+        exp_doub_counts: [
+          2,
+          1,
+          1,
+          1,
+          1,
+          0,
+          1,
+          1,
+          0,
+          0,
+          0,
+        ],
+      ]
+    end
+
+    test "correctly count doubled product IDs", fixture do
+      act_doub_counts = fixture.product_ranges
+                        |> Enum.map(&Shop.count_doubled/1)
+      assert act_doub_counts == fixture.exp_doub_counts
+    end
   end
 end
