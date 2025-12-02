121da1613796487a95f2d2de507781eadf621447
Brent J. Nordquist <brent@nordist.net>
2025-12-02 05:57:48 -0600

    aoc-2025 02-p1: Parser

---
+++ advent-2025/day-02/shop/lib/shop/parser.ex
@@ -0,0 +1,76 @@
+defmodule Shop.Parser do
+  @moduledoc """
+  Parsing for `Shop`.
+  """
+
+  @type product_range() :: {integer(), integer()}
+
+  @doc ~S"""
+  Parse an input file.
+
+  See `parse_input_string/1` for details.
+
+  ## Parameters
+
+  - `path`: the puzzle input file path
+
+  ## Returns
+
+  a list of product ID ranges
+  """
+  @spec parse_input_file(String.t()) :: [product_range()]
+  def parse_input_file(path) do
+    path
+    |> File.read!()
+    |> parse_input_string()
+  end
+
+  @doc ~S"""
+  Parse an input string.
+
+  ## Parameters
+
+  - `input`: the puzzle input
+
+  ## Returns
+
+  a list of product ID ranges
+
+  ## Examples
+      iex> parse_input_string("11-22,95-115\n")
+      [{11, 22}, {95, 115}]
+  """
+  @spec parse_input_string(String.t()) :: [product_range()]
+  def parse_input_string(input) do
+    input
+    |> String.trim_trailing()
+    |> String.split(",", trim: true)
+    |> Enum.map(&parse_range/1)
+  end
+
+  @doc ~S"""
+  Parse a product ID range string.
+
+  ## Parameters
+
+  - `range`: the product ID range string
+
+  ## Returns
+
+  a product ID range
+
+  ## Examples
+      iex> parse_range("70-86")
+      {70, 86}
+      iex> parse_range("95-115")
+      {95, 115}
+  """
+  @spec parse_range(String.t()) :: product_range()
+  def parse_range(range) do
+    range
+    |> String.split("-")
+    |> then(fn [a, b] ->
+      {String.to_integer(a), String.to_integer(b)}
+    end)
+  end
+end
---
+++ advent-2025/day-02/shop/test/shop/parser_test.exs
@@ -0,0 +1,35 @@
+defmodule Shop.ParserTest do
+  use ExUnit.Case
+  doctest Shop.Parser, import: true
+
+  import Shop.Parser
+
+  describe "puzzle example" do
+    setup do
+      [
+        input: """
+        11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124
+        """,
+        exp_product_ranges: [
+          {11, 22},
+          {95, 115},
+          {998, 1012},
+          {1188511880, 1188511890},
+          {222220, 222224},
+          {1698522, 1698528},
+          {446443, 446449},
+          {38593856, 38593862},
+          {565653, 565659},
+          {824824821, 824824827},
+          {2121212118, 2121212124},
+        ],
+      ]
+    end
+
+    test "parser gets expected product ranges", fixture do
+      act_product_ranges = fixture.input
+                           |> parse_input_string()
+      assert act_product_ranges == fixture.exp_product_ranges
+    end
+  end
+end
