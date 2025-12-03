9aa7f9e603e42c5487cfd617e7569e9a169beb68
Brent J. Nordquist <brent@nordist.net>
2025-12-03 07:28:12 -0600

    aoc-2025 03-p1: Correct answer

--- advent-2025/day-03/lift/lib/lift.ex
+++ advent-2025/day-03/lift/lib/lift.ex
@@ -7,6 +7,65 @@ defmodule Lift do
   import History.CLI
 
   @doc """
+  Generate a map of battery positions within a bank.
+
+  ## Examples
+      iex> Lift.position_map(~c"1234")
+      %{1 => [0], 2 => [1], 3 => [2], 4 => [3], 5 => [], 6 => [], 7 => [], 8 => [], 9 => []}
+      iex> Lift.position_map(~c"4142")
+      %{1 => [1], 2 => [3], 3 => [], 4 => [0, 2], 5 => [], 6 => [], 7 => [], 8 => [], 9 => []}
+  """
+  def position_map(bank) do
+    pos_map =
+      bank
+      |> Enum.with_index()
+      |> Enum.reduce(%{}, fn {ch, i}, acc ->
+        Map.update(acc, ch - ?0, [i], fn list -> [i | list] end)
+      end)
+    1..9
+    |> Enum.reduce(pos_map, fn batt, acc ->
+      Map.update(acc, batt, [], fn list -> Enum.reverse(list) end)
+    end)
+  end
+
+  @doc """
+  Compute the maximum joltage of a battery bank.
+
+  ## Examples
+      iex> Lift.max_joltage(~c"1234")
+      34
+      iex> Lift.max_joltage(~c"4142")
+      44
+  """
+  def max_joltage(bank) do
+    pos_map = position_map(bank)
+    find_max_joltage(pos_map, [{9, 0}])
+  end
+
+  defp find_max_joltage(_pos_map, [{9, _}, {batt_b, _}, {batt_a, _}]) do
+    # termination condition: found the highest two batteries
+    batt_a * 10 + batt_b
+  end
+  defp find_max_joltage(pos_map, [{0, _}, {batt, _}]) do
+    # couldn't find any batteries to the right of the first one
+    # start over and find a new first battery
+    find_max_joltage(pos_map, [{batt - 1, 0}])
+  end
+  defp find_max_joltage(pos_map, [{batt, min_i} | found]) do
+    positions = Map.get(pos_map, batt)
+    match_i = Enum.find(positions, fn pos -> pos >= min_i end)
+    if match_i do
+      # found a battery that matches the requirements
+      # keep it, and start searching for the next one to the right
+      find_max_joltage(pos_map, [{9, match_i + 1}, {batt, match_i} | found])
+    else
+      # didn't find a matching battery
+      # try the next highest battery
+      find_max_joltage(pos_map, [{batt - 1, min_i} | found])
+    end
+  end
+
+  @doc """
   Parse arguments and call puzzle part methods.
 
   ## Parameters
@@ -24,7 +83,8 @@ defmodule Lift do
   """
   def part1(input_path) do
     parse_input_file(input_path)
-    nil  # TODO
+    |> Enum.map(&max_joltage/1)
+    |> Enum.sum()
     |> IO.inspect(label: "Part 1 answer is")
   end
 
--- advent-2025/day-03/lift/test/lift_test.exs
+++ advent-2025/day-03/lift/test/lift_test.exs
@@ -1,4 +1,29 @@
 defmodule LiftTest do
   use ExUnit.Case
   doctest Lift
+
+  describe "puzzle example" do
+    setup do
+      [
+        banks: [
+          ~c"987654321111111",
+          ~c"811111111111119",
+          ~c"234234234234278",
+          ~c"818181911112111"
+        ],
+        exp_max_joltages: [
+          98,
+          89,
+          78,
+          92,
+        ],
+      ]
+    end
+
+    test "computes expected max joltages", fixture do
+      act_max_joltages = fixture.banks
+                         |> Enum.map(&Lift.max_joltage/1)
+      assert act_max_joltages == fixture.exp_max_joltages
+    end
+  end
 end
