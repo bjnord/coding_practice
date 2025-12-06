1a77ff88ed84a95ab162a226264d08644a623901
Brent J. Nordquist <brent@nordist.net>
2025-12-06 16:49:00 -0600

    aoc-2025 06: Add FreedomFormatter plugin

--- advent-2025/day-06/trash/.formatter.exs
+++ advent-2025/day-06/trash/.formatter.exs
@@ -1,4 +1,7 @@
 # Used by "mix format"
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  plugins: [FreedomFormatter],
+  trailing_comma: true,
+  local_pipe_with_parens: true,
 ]
