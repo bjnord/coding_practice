abdd9286ae81d34a5a71c78040d4de1c45248cd3
Brent J. Nordquist <brent@nordist.net>
2025-12-07 05:53:10 -0600

    aoc-2025 07-p1: Correct answer

--- advent-2025/day-07/split/lib/split.ex
+++ advent-2025/day-07/split/lib/split.ex
@@ -5,6 +5,49 @@ defmodule Split do
 
   import Split.Parser
   import Decor.CLI
+  alias Decor.Grid
+
+  @doc """
+  Count the number of times the beam will be split (part 1 rules).
+  """
+  @spec count_splits(Grid.t()) :: integer()
+  def count_splits(grid) do
+    {start_y, start_x} = grid.meta.start
+    count_splits_on_row(grid, start_y + 1, grid.size.y, [start_x], 0)
+  end
+
+  defp count_splits_on_row(_grid, y, max_y, _beams, count) when y > max_y, do: count
+
+  defp count_splits_on_row(grid, y, max_y, beams, count) do
+    # in both `splitter_map` and `new_beams_map`, key = `x` (column)
+    splitter_map = splitters_on_row(grid, y)
+    {n_splits, new_beams_map} =
+      beams
+      |> Enum.reduce({0, %{}}, fn beam, {n_splits, new_beams_map} ->
+        if Map.get(splitter_map, beam) do
+          {
+            n_splits + 1,
+            Map.put(new_beams_map, beam - 1, true) |> Map.put(beam + 1, true),
+          }
+        else
+          {
+            n_splits,
+            Map.put(new_beams_map, beam, true),
+          }
+        end
+      end)
+    count_splits_on_row(grid, y + 1, max_y, Map.keys(new_beams_map), count + n_splits)
+  end
+
+  # return a Map of the splitters on row `y`
+  # - key: `x` (column of splitter)
+  # - value: `y` (row)
+  defp splitters_on_row(grid, y) do
+    Grid.keys(grid)
+    |> Enum.filter(fn {ky, _kx} -> ky == y end)
+    |> Enum.map(fn {ky, kx} -> {kx, ky} end)
+    |> Enum.into(%{})
+  end
 
   @doc """
   Parse arguments and call puzzle part methods.
@@ -24,7 +67,7 @@ defmodule Split do
   """
   def part1(input_path) do
     parse_input_file(input_path)
-    nil  # TODO
+    |> count_splits()
     |> IO.inspect(label: "Part 1 answer is")
   end
 
--- advent-2025/day-07/split/test/split_test.exs
+++ advent-2025/day-07/split/test/split_test.exs
@@ -1,4 +1,50 @@
 defmodule SplitTest do
   use ExUnit.Case
   doctest Split
+
+  alias Decor.Grid
+
+  describe "puzzle example" do
+    setup do
+      [
+        grid: %Grid{
+          # (NB empty row/column is trimmed off the end)
+          size: %{y: 15, x: 14},
+          squares: %{
+                {2, 7} => ?^,
+                {4, 6} => ?^,
+                {4, 8} => ?^,
+                {6, 5} => ?^,
+                {6, 7} => ?^,
+                {6, 9} => ?^,
+                {8, 4} => ?^,
+                {8, 6} => ?^,
+                {8, 10} => ?^,
+                {10, 3} => ?^,
+                {10, 5} => ?^,
+                {10, 9} => ?^,
+                {10, 11} => ?^,
+                {12, 2} => ?^,
+                {12, 6} => ?^,
+                {12, 12} => ?^,
+                {14, 1} => ?^,
+                {14, 3} => ?^,
+                {14, 5} => ?^,
+                {14, 7} => ?^,
+                {14, 9} => ?^,
+                {14, 13} => ?^,
+          },
+          meta: %{start: {0, 7}},
+        },
+        exp_split_count: 21,
+      ]
+    end
+
+    test "produces correct split count", fixture do
+      act_split_count =
+        fixture.grid
+        |> Split.count_splits()
+      assert act_split_count == fixture.exp_split_count
+    end
+  end
 end
