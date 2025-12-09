03be652184f71dec00c12be881a794a1269b17d4
Brent J. Nordquist <brent@nordist.net>
2025-12-09 06:35:24 -0600

    aoc-2025 09-p1: Correct answer

--- advent-2025/day-09/tile/lib/tile.ex
+++ advent-2025/day-09/tile/lib/tile.ex
@@ -5,6 +5,41 @@ defmodule Tile do
 
   import Tile.Parser
   import Decor.CLI
+  alias Decor.Grid
+
+  @doc """
+  Find the pair of red tiles that enclose the greatest area.
+  """
+  def max_area_tiles(grid) do
+    grid
+    |> Grid.keys()
+    |> Enum.sort()
+    |> tile_areas([])
+    |> Enum.sort(fn a, b -> elem(a, 0) > elem(b, 0) end)
+    |> Enum.map(fn {_dist, a, b} -> {a, b} end)
+    |> List.first()
+  end
+
+  defp tile_areas([next_tile, rem_tile], acc) do
+    dist = {tile_area(next_tile, rem_tile), next_tile, rem_tile}
+    [dist | acc]
+  end
+  defp tile_areas([next_tile | rem_tiles], acc) do
+    acc =
+      rem_tiles
+      |> Enum.reduce(acc, fn rem_tile, acc ->
+        dist = {tile_area(next_tile, rem_tile), next_tile, rem_tile}
+        [dist | acc]
+      end)
+    tile_areas(rem_tiles, acc)
+  end
+
+  @doc """
+  Find the area enclosed by two red tile positions.
+  """
+  def tile_area({y1, x1}, {y2, x2}) do
+    (abs(y1 - y2) + 1) * (abs(x1 - x2) + 1)
+  end
 
   @doc """
   Parse arguments and call puzzle part methods.
@@ -24,7 +59,8 @@ defmodule Tile do
   """
   def part1(input_path) do
     parse_input_file(input_path)
-    nil  # TODO
+    |> max_area_tiles()
+    |> then(fn {a, b} -> tile_area(a, b) end)
     |> IO.inspect(label: "Part 1 answer is")
   end
 
--- advent-2025/day-09/tile/test/tile_test.exs
+++ advent-2025/day-09/tile/test/tile_test.exs
@@ -1,4 +1,31 @@
 defmodule TileTest do
   use ExUnit.Case
   doctest Tile
+
+  import Tile
+  alias Decor.Grid
+
+  describe "puzzle example" do
+    setup do
+      [
+        grid: %Grid{
+          size: %{y: 8, x: 12},
+          squares: %{
+            {1, 7} => ?#, {1, 11} => ?#,
+            {3, 2} => ?#, {3, 7} => ?#,
+            {5, 2} => ?#, {5, 9} => ?#,
+            {7, 9} => ?#, {7, 11} => ?#,
+          },
+        },
+        exp_area: 50,
+      ]
+    end
+
+    test "finds largest area", fixture do
+      act_area = fixture.grid
+                 |> max_area_tiles()
+                 |> then(fn {a, b} -> tile_area(a, b) end)
+      assert act_area == fixture.exp_area
+    end
+  end
 end
